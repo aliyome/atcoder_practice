a26d10052eb8493ffde27d23baafb1a55c818b44
aliyome <aliyome@gmail.com>
2023-04-28 23:21:26 +0900

    typical90 003

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -6,49 +6,55 @@ fn main() {
         ab: [[usize; 2]; n as usize - 1]
     };
 
-    // 辺の情報を格納する配列 edges[from][to] = bool
-    let mut edges = vec![vec![false; n + 1]; n + 1];
-    for edge in ab {
-        let from = edge[0];
-        let to = edge[1];
-        // 木なので双方向に辺を張る
-        edges[from][to] = true;
-        edges[to][from] = true;
+    // edges に隣接リストを作る
+    let mut edges = vec![vec![]; n];
+    for e in ab {
+        edges[e[0] - 1].push(e[1] - 1);
+        edges[e[1] - 1].push(e[0] - 1);
     }
 
-    // 木の直径を求める
-    // 頂点1から最も遠い頂点を求める
-    let mut dist = vec![0; n + 1];
-    let mut dist2 = vec![0; n + 1];
-    dfs(&mut edges.clone(), 1, &mut dist, n, 1);
-    let mut max = 0;
-    let mut max_idx = 0;
-    for i in 1..n + 1 {
-        if dist[i] > max {
-            max = dist[i];
-            max_idx = i;
+    // dist に各頂点の深さを入れる
+    let mut dist = vec![0; n];
+
+    // 0 番目の頂点から dfs をする
+    dfs(&edges, 0, n, &mut dist);
+
+    // 0 番目の頂点から最も遠い頂点を探す
+    let mut max_dist = 0;
+    let mut max_dist_idx = 0;
+    for i in 0..n {
+        if dist[i] > max_dist {
+            max_dist = dist[i];
+            max_dist_idx = i;
         }
     }
-    // println!("{:?}", dist);
-    // 最も遠い頂点からもう一度最も遠い頂点を求める
-    dfs(&mut edges.clone(), max_idx, &mut dist2, n, 1);
-    max = 0;
-    for i in 1..n + 1 {
-        if dist2[i] > max {
-            max = dist2[i];
+
+    // dist に各頂点の深さを入れる
+    let mut dist = vec![0; n];
+
+    // 最も遠い頂点から dfs をする
+    dfs(&edges, max_dist_idx, n, &mut dist);
+
+    // 最も遠い頂点から最も遠い頂点を探す
+    let mut max_dist = 0;
+    let mut max_dist_idx = 0;
+    for i in 0..n {
+        if dist[i] > max_dist {
+            max_dist = dist[i];
+            max_dist_idx = i;
         }
     }
-    // println!("{:?}", dist2);
-    println!("{}", max + 1);
+
+    // 答えは最も遠い頂点から最も遠い頂点の距離 + 1
+    println!("{}", max_dist + 1);
 }
 
-fn dfs(edges: &mut Vec<Vec<bool>>, cur: usize, dist: &mut Vec<usize>, n: usize, d: usize) {
-    for i in 1..n + 1 {
-        if edges[cur][i] {
-            edges[cur][i] = false;
-            edges[i][cur] = false;
-            dist[i] += d;
-            dfs(edges, i, dist, n, d + 1);
+fn dfs(edges: &Vec<Vec<usize>>, curr: usize, prev: usize, dist: &mut Vec<usize>) {
+    for next in edges[curr].iter() {
+        if *next == prev {
+            continue;
         }
+        dist[*next] = dist[curr] + 1;
+        dfs(edges, *next, curr, dist);
     }
 }
