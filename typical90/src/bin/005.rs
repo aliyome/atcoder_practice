a9ffd0dd761aef83023ed12bebcbf42779ada9d0
aliyome <aliyome@gmail.com>
2023-04-29 15:20:31 +0900

    wip typical90 005 tle

--- typical90/src/bin/005.rs
+++ typical90/src/bin/005.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        b: usize,
+        k: usize,
+        c: [usize; k]
+    };
+
+    const MOD: usize = 1_000_000_007;
+    let mut dp = vec![vec![0; b]; n + 1];
+    dp[0][0] = 1;
+
+    for i in 0..n {
+        for j in 0..b {
+            for &x in &c {
+                dp[i + 1][(j * 10 + x) % b] += dp[i][j];
+                dp[i + 1][(j * 10 + x) % b] %= MOD;
+            }
+        }
+    }
+    println!("{}", dp[n][0]);
 }
