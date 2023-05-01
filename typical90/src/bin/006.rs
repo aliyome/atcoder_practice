00c60de30ef2b8c4ec842e276aaeb1a49b8a83c2
aliyome <aliyome@gmail.com>
2023-05-01 18:11:57 +0900

    typical90 006 tle

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        s: Chars,
+    }
+
+    // 全探索で解く
+    let mut ans: Vec<char> = vec![];
+    let mut idx = 0;
+    for kk in 0..k {
+        for c in 0..26 {
+            for nn in idx..n - k + kk {
+                let c = (c as u8 + b'a') as char;
+                if s[nn] == c {
+                    ans.push(c);
+                    idx = nn;
+                    break;
+                }
+            }
+            if ans.len() > kk {
+                break;
+            }
+        }
+    }
+    println!("{}", ans.iter().collect::<String>());
 }
