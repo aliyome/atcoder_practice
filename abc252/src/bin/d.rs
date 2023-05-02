8381ef0bdd604f407573d5fc9736088269537ff6
aliyome <aliyome@gmail.com>
2023-05-02 11:30:42 +0900

    abc252 d

--- abc252/src/bin/d.rs
+++ abc252/src/bin/d.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
@@ -6,27 +8,25 @@ fn main() {
         a: [usize; n],
     }
 
-    let mut ans = 0;
+    // Aiの値毎にグループ分けして数を数える
+    let mut counts = HashMap::new();
+    for e in a {
+        *counts.entry(e).or_insert(0) += 1usize;
+    }
+    let counts = counts.values().collect::<Vec<_>>();
 
-    // 全探索
-    for i in 1..=n - 2 {
-        for j in i..=n - 1 {
-            for k in j..n {
-                println!("{} {} {}", i, j, k);
-                if is_ok(i - 1, j - 1, k - 1, &a) {
-                    ans += 1;
-                }
+    // dp[i][j] = i番目までのグループを使ってj個の数列を作る場合の数
+    let mut dp = vec![vec![0 as usize; 4]; 2 * 10usize.pow(5) + 1];
+    dp[0][0] = 1;
+    for j in 0..=3 {
+        for i in 0..counts.len() {
+            if j == 0 {
+                dp[i + 1][j] = 1;
+            } else {
+                dp[i + 1][j] = dp[i][j] + dp[i][j - 1] * counts[i];
             }
         }
     }
 
-    println!("{}", ans);
-}
-
-fn is_ok(i: usize, j: usize, k: usize, a: &[usize]) -> bool {
-    if a[i] == a[j] || a[i] == a[k] || a[j] == a[k] {
-        println!("same {} {} {}", a[i], a[j], a[k]);
-        return false;
-    }
-    true
+    println!("{}", dp[counts.len()][3]);
 }
