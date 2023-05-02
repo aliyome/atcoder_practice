59f4e34cd9171e7ea18f3360db352ea79fb32908
aliyome <aliyome@gmail.com>
2023-05-02 12:34:49 +0900

    typical90 006 full search TLE

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,7 +1,5 @@
 use proconio::input;
 use proconio::marker::Chars;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 
 fn main() {
     input! {
@@ -10,23 +8,35 @@ fn main() {
         s: Chars,
     }
 
-    // 全探索で解く
-    let mut ans: Vec<char> = vec![];
+    // 各文字の出現位置を記録
+    let mut pos = vec![vec![false; 10usize.pow(5) + 1]; 26];
+    for i in 0..n {
+        let c = s[i] as usize - 'a' as usize;
+        pos[c][i] = true;
+    }
+
+    // 全探索
+    // k文字作れるまで
     let mut idx = 0;
-    for kk in 0..k {
-        for c in 0..26 {
-            for nn in idx..n - k + kk {
-                let c = (c as u8 + b'a') as char;
-                if s[nn] == c {
-                    ans.push(c);
-                    idx = nn;
-                    break;
+    let mut ans = vec![];
+    for j in 0..k {
+        // a から順番に
+        'outer: for c in 0..26 {
+            // その文字の出現位置を順番に見ていく
+            for i in idx..n {
+                if !pos[c][i] {
+                    continue;
                 }
-            }
-            if ans.len() > kk {
-                break;
+                // 出現位置が後ろすぎる場合はスキップ
+                if i + k - j > n {
+                    continue;
+                }
+                idx = i + 1;
+                ans.push((c as u8 + 'a' as u8) as char);
+                break 'outer;
             }
         }
     }
+
     println!("{}", ans.iter().collect::<String>());
 }
