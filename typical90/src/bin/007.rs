acb928dfcf3982cd9840ef23c01b7c7ffc50a290
aliyome <aliyome@gmail.com>
2023-05-02 20:18:39 +0900

    typical90 007

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+        q: usize,
+        b: [i64; q],
+    }
+
+    // 二分探索するためにaをソートして番兵を追加する
+    let mut a = a;
+    a.sort();
+
+    // 二分探索の結果を出力する
+    for b in b {
+        println!("{}", binary_search(&a, b));
+    }
+}
+
+fn binary_search(a: &Vec<i64>, x: i64) -> i64 {
+    let mut ok = 0;
+    let mut ng = a.len() - 1;
+
+    while ng - ok > 1 {
+        let mid = (ok + ng) / 2;
+        if a[mid] <= x {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    let diff1 = (a[ok] - x).abs();
+    let diff2 = (a[ng] - x).abs();
+    if diff1 < diff2 {
+        diff1
+    } else {
+        diff2
+    }
 }
