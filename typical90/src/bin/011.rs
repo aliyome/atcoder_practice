ccf5a90d2c23987e0d40223f73d710e77bd9497b
aliyome <aliyome@gmail.com>
2023-05-03 18:22:05 +0900

    typical90 011 1/3

--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -1,3 +1,30 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        dcs: [(usize, usize, usize); n]
+    }
+
+    //
+    // 全探索 O(N!) 2点
+    //
+    let mut max = 0;
+    // 全組み合わせを列挙
+    for indices in (0..n).permutations(n) {
+        let mut sum = 0;
+        let mut day = 0;
+        for i in indices {
+            let (d, c, s) = dcs[i];
+            day += c;
+            if day > d {
+                break;
+            }
+            sum += s;
+        }
+        max = max.max(sum);
+    }
+
+    println!("{}", max);
 }
