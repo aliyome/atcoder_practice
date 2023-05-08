a7a14c1c8cea1d1e6eab91b0de952da13adc425b
aliyome <aliyome@gmail.com>
2023-05-08 12:02:59 +0900

    abc137 d

--- abc137/src/bin/d.rs
+++ abc137/src/bin/d.rs
@@ -1,9 +1,32 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        mut ab: [(usize, usize); n]
     };
 
-    println!("{}", n);
+    let mut jobs = vec![vec![]; m + 1];
+    for (a, b) in ab {
+        if a <= m {
+            jobs[a].push(b);
+        }
+    }
+
+    let mut heap = BinaryHeap::new();
+    let mut ans = 0;
+
+    for i in 1..=m {
+        for &b in &jobs[i] {
+            heap.push(b);
+        }
+        if let Some(b) = heap.pop() {
+            ans += b;
+        }
+    }
+
+    println!("{}", ans);
 }
