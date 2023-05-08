18ffa69345e9b163afa41ed921ae2b0bd4f375ab
aliyome <aliyome@gmail.com>
2023-05-08 17:29:17 +0900

    typical90 014

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+        mut b: [i64; n],
+    }
+
+    a.sort();
+    b.sort();
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += (a[i] - b[i]).abs();
+    }
+
+    println!("{}", ans);
 }
