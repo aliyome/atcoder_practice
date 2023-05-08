06f8db5f1630ae0cad10e7177d20f111e1d2ac23
aliyome <aliyome@gmail.com>
2023-05-08 13:33:53 +0900

    abc292 d union find

--- abc292/src/bin/d.rs
+++ abc292/src/bin/d.rs
@@ -1,25 +1,6 @@
+use std::collections::HashSet;
+
 use proconio::input;
-use std::collections::VecDeque;
-
-fn dfs(v: usize, graph: &Vec<Vec<usize>>, visited: &mut Vec<bool>) -> (usize, usize) {
-    let mut stack = VecDeque::new();
-    stack.push_back(v);
-    visited[v] = true;
-    let mut vertices = 1;
-    let mut edges = 0;
-
-    while let Some(current) = stack.pop_back() {
-        for &next in &graph[current] {
-            edges += 1;
-            if !visited[next] {
-                visited[next] = true;
-                vertices += 1;
-                stack.push_back(next);
-            }
-        }
-    }
-    (vertices, edges)
-}
 
 fn main() {
     input! {
@@ -28,24 +9,83 @@ fn main() {
         uv: [(usize, usize); m],
     }
 
-    let mut graph = vec![Vec::new(); n + 1];
-    for (u, v) in uv {
-        graph[u].push(v);
-        graph[v].push(u);
+    // 連結成分の統合
+    let mut uf = UnionFind::new(n + 1);
+    for (u, v) in &uv {
+        uf.unite(*u, *v);
+    }
+    // 連結成分の数を数える
+    let mut roots = HashSet::new();
+    for i in 1..=n {
+        let root = uf.find(i);
+        roots.insert(root);
+    }
+    // 連結成分ごとの辺数を数える
+    for root_v in roots {
+        let vert_count = uf.size[root_v];
+        let mut edge_count = 0;
+        for (u, v) in &uv {
+            if uf.same(root_v, *u) {
+                edge_count += 1;
+            }
+        }
+        if edge_count != vert_count {
+            println!("No");
+            return;
+        }
     }
 
-    let mut visited = vec![false; n + 1];
-    let mut result = "Yes";
+    println!("Yes");
+}
 
-    for i in 1..=n {
-        if !visited[i] {
-            let (vertices, edges) = dfs(i, &graph, &mut visited);
-            if 2 * vertices != edges {
-                result = "No";
-                break;
+pub struct UnionFind {
+    parent: Vec<usize>,
+    rank: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl UnionFind {
+    // O(N)
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: (0..n).collect(),
+            rank: vec![0; n],
+            size: vec![1; n],
+        }
+    }
+
+    // O(α(N)) ≒ O(1)
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] == x {
+            x
+        } else {
+            let p = self.find(self.parent[x]);
+            self.parent[x] = p;
+            p
+        }
+    }
+
+    // O(α(N)) ≒ O(1)
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.find(x);
+        let y = self.find(y);
+        if x == y {
+            return;
+        }
+        if self.rank[x] < self.rank[y] {
+            self.parent[x] = y;
+            self.size[y] += self.size[x];
+        } else {
+            self.parent[y] = x;
+            self.size[x] += self.size[y];
+            if self.rank[x] == self.rank[y] {
+                self.rank[x] += 1;
             }
         }
     }
 
-    println!("{}", result);
+    // O(α(N)) ≒ O(1)
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
 }
