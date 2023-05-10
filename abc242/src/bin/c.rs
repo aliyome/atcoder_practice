26f9be6bceb5256b109521cf5f6c246d4d25ba69
aliyome <aliyome@gmail.com>
2023-05-10 14:50:27 +0900

    abc242 c

--- abc242/src/bin/c.rs
+++ abc242/src/bin/c.rs
@@ -1,44 +1,36 @@
 use proconio::input;
 
-// TODO: TLE
-
-const MOD: usize = 998244353;
+const MOD: i64 = 998244353;
 
 fn main() {
     input! {
         n: usize,
     };
 
-    let mut dp = vec![vec![0; 11]; n + 1];
-    for i in 1..=9 {
-        dp[1][i] = 1;
+    // dp[j][i] = i - 1 桁の数で、j が最後になるもので条件を満たす数
+    let mut dp = vec![vec![0; n + 1]; 11];
+
+    // 1 桁の数は 1 から 9 までそれぞれが条件を満たす
+    for j in 1..=9 {
+        dp[j][0] = 1;
     }
-    for i in 2..=n {
+
+    // 2桁目以降はDPで求める
+    for i in 1..=n {
         for j in 1..=9 {
-            dp[i][j] += dp[i - 1][j - 1];
-            dp[i][j] += dp[i - 1][j];
-            dp[i][j] += dp[i - 1][j + 1];
-            dp[i][j] %= MOD
+            // 1桁前の数が j とその前後の数 の個数を足す
+            dp[j][i] += dp[j - 1][i - 1];
+            dp[j][i] += dp[j][i - 1];
+            dp[j][i] += dp[j + 1][i - 1];
+            dp[j][i] %= MOD;
         }
     }
 
     let mut ans = 0;
-    for i in 1..=9 {
-        ans += dp[n][i];
+    for j in 1..=9 {
+        ans += dp[j][n - 1];
+        ans %= MOD;
     }
 
     println!("{}", ans);
-}
-
-fn f(left: usize, last: i64) -> usize {
-    if left == 0 {
-        return 1;
-    }
-    if last - 1 <= 0 {
-        return f(left - 1, last) + f(left - 1, last + 1);
-    }
-    if last + 1 >= 0 {
-        return f(left - 1, last - 1) + f(left - 1, last);
-    }
-    f(left - 1, last - 1) + f(left - 1, last) + f(left - 1, last + 1)
 }
