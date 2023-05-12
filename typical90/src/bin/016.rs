431ac53a6bd9ac30f17c3b0fa81630b3a5c274e1
aliyome <aliyome@gmail.com>
2023-05-12 13:13:02 +0900

    typical90 016 tle

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        (a, b, c): (usize, usize, usize)
+    }
+
+    // 全探索 TLE
+    let mut min = 10usize.pow(4);
+    for i in 0..10usize.pow(4) {
+        if i * a > n {
+            break;
+        }
+        for j in 0..10usize.pow(4) {
+            if i + j > 9999 || i * a + j * b > n {
+                break;
+            }
+            for k in 0..10usize.pow(4) {
+                if i + j + k > 9999 || i * a + j * b + k * c > n {
+                    break;
+                }
+                if a * i + b * j + c * k == n {
+                    min = min.min(i + j + k);
+                }
+            }
+        }
+    }
+
+    println!("{}", min);
 }
