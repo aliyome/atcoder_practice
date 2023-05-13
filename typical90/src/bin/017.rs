a5e47a300e207cefba0def093c3c4d208eeed76c
aliyome <aliyome@gmail.com>
2023-05-13 11:22:42 +0900

    typical90 017 tle

--- typical90/src/bin/017.rs
+++ typical90/src/bin/017.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (n, m): (usize, usize),
+        lr: [(usize, usize); m]
+    }
+
+    // 全探索 O(M^2) TLE
+    // N,M <= 10^3
+    let mut ans = 0;
+    for a in 0..m {
+        let (la, ra) = lr[a];
+        for b in 0..m {
+            let (lb, rb) = lr[b];
+            if a == b {
+                continue;
+            }
+            if lb < la && rb < ra && la < rb {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
