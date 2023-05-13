ce62fba4902f1c585299d593326f7b13376c53e0
aliyome <aliyome@gmail.com>
2023-05-13 21:13:07 +0900

    abc301 b

--- abc301/src/bin/b.rs
+++ abc301/src/bin/b.rs
@@ -3,7 +3,29 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        a: [usize; n]
     };
 
-    println!("{}", n);
+    let mut ans = vec![];
+    ans.push(a[0]);
+    for i in 1..n {
+        let last = *ans.last().unwrap();
+        if last < a[i] {
+            for j in last + 1..=a[i] {
+                ans.push(j);
+            }
+        } else if last > a[i] {
+            for j in (a[i]..=last - 1).rev() {
+                ans.push(j);
+            }
+        }
+    }
+
+    let joined = ans
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    println!("{}", joined);
 }
