f0b1db7978680442fe5dabe217a2c74c77f74881
aliyome <aliyome@gmail.com>
2023-05-15 21:07:37 +0900

    abc163 c

--- abc163/src/bin/c.rs
+++ abc163/src/bin/c.rs
@@ -3,7 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        a: [usize; n - 1]
     };
 
-    println!("{}", n);
+    let mut ans = vec![0; n + 1];
+    for i in 0..n - 1 {
+        ans[a[i]] += 1;
+    }
+
+    for i in 1..=n {
+        println!("{}", ans[i]);
+    }
 }
