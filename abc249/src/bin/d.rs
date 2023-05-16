0f07688eae735c6ff1908628b5b969952ba93e5b
aliyome <aliyome@gmail.com>
2023-05-16 23:58:29 +0900

    abc249 d

--- abc249/src/bin/d.rs
+++ abc249/src/bin/d.rs
@@ -3,7 +3,22 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        a: [usize; n]
     };
 
-    println!("{}", n);
+    const M: usize = 200001;
+    let mut d = vec![0; M + 1];
+    for i in 0..n {
+        d[a[i]] += 1;
+    }
+
+    let mut ans = 0usize;
+    for a in 1..=M {
+        for b in 1..=M / a {
+            let c = a * b;
+            ans += d[a] * d[b] * d[c];
+        }
+    }
+
+    println!("{}", ans);
 }
