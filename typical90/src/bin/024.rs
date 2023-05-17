ee4d1a0442a1cfeceb1c5753b370c9312046d32d
aliyome <aliyome@gmail.com>
2023-05-17 19:13:11 +0900

    typical90 024

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (n, k): (usize, usize),
+        a: [usize; n],
+        b: [usize; n],
+    }
+
+    let mut diff = 0;
+    for i in 0..n {
+        diff += (a[i] as isize - b[i] as isize).abs();
+    }
+    if k < diff as usize {
+        println!("No");
+        return;
+    }
+    if (diff - k as isize) % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
