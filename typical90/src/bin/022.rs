8b1497d4709466cdedc89f8ba975a671e6fe4615
aliyome <aliyome@gmail.com>
2023-05-17 17:16:52 +0900

    typical90 022 gcd

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize
+    }
+
+    let x = gcd(gcd(a, b), c);
+    let ans = a / x + b / x + c / x - 3;
+    println!("{}", ans);
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }
