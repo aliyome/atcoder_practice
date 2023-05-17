ceb66da54edd897ed42fb8834cc4c68c61301c24
aliyome <aliyome@gmail.com>
2023-05-17 12:12:10 +0900

    typical90 020 ac

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
+// 典型90問 020 Log Inequality
 fn main() {
-    todo!();
+    input! {
+        a: u64,
+        b: u64,
+        c: u64,
+    }
+
+    if a < c.pow(b as u32) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
