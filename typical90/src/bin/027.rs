ce3574ffccd99ab817a340f6ec8e0145c1bd514f
aliyome <aliyome@gmail.com>
2023-05-18 12:42:54 +0900

    typical90 027

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let mut set = HashSet::new();
+    for i in 0..n {
+        if !set.contains(s[i].as_str()) {
+            println!("{}", i + 1);
+        }
+        set.insert(s[i].as_str());
+    }
 }
