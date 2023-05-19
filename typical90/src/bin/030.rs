c5f9f5cead529b4cbfe1b87d62fa9d46addf49dd
aliyome <aliyome@gmail.com>
2023-05-19 12:49:52 +0900

    typical90 030

--- typical90/src/bin/030.rs
+++ typical90/src/bin/030.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
+// 解答見る
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize
+    }
+
+    let mut cnt = vec![0; n + 1];
+    for i in 2..=n {
+        if cnt[i] >= 1 {
+            continue;
+        }
+        for j in (i..=n).step_by(i) {
+            cnt[j] += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for i in 1..=n {
+        if cnt[i] >= k {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
