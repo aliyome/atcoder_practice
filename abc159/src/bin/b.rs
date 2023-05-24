e10479d2c2f32aff3ada90852cc01990419c25ab
aliyome <aliyome@gmail.com>
2023-05-24 21:11:21 +0900

    abc159 b

--- abc159/src/bin/b.rs
+++ abc159/src/bin/b.rs
@@ -1,9 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     };
 
-    println!("{}", n);
+    let s = s;
+    let mut t = s.clone();
+    t.reverse();
+
+    for i in 0..s.len() {
+        if s[i] != t[i] {
+            println!("No");
+            return;
+        }
+    }
+    if s[0..s.len() / 2].iter().collect::<Vec<_>>()
+        != s[s.len() / 2 + 1..].iter().collect::<Vec<_>>()
+    {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
 }
