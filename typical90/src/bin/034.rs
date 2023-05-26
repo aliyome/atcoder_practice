888cc7c668506b5a4f344f4db6fc5bc1ed2f23d5
aliyome <aliyome@gmail.com>
2023-05-26 13:50:28 +0900

    typical90 034 shakutori

--- typical90/src/bin/034.rs
+++ typical90/src/bin/034.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
+    }
+
+    let mut count = HashMap::new();
+    let mut ans = 0;
+    let mut l = 0;
+    for r in 0..n {
+        *count.entry(a[r]).or_insert(0) += 1;
+
+        // k 種類以内の場合
+        if count.len() <= k {
+            ans = ans.max(r - l + 1);
+            continue;
+        }
+
+        // k 種類を超えた場合
+        // 一番古いものを削除して、文字種が変わるまで l を進める
+        while count.len() > k {
+            count.entry(a[l]).and_modify(|e| *e -= 1);
+            if count[&a[l]] == 0 {
+                count.remove(&a[l]);
+            }
+            l += 1;
+        }
+        ans = ans.max(r - l + 1);
+    }
+
+    println!("{}", ans);
 }
