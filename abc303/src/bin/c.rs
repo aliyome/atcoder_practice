4a531af3d18dcfb207ede53fe81647bd89253c53
aliyome <aliyome@gmail.com>
2023-05-27 21:17:19 +0900

    abc303 c

--- abc303/src/bin/c.rs
+++ abc303/src/bin/c.rs
@@ -1,9 +1,38 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
         n: usize,
-    };
+        m: usize,
+        mut h: i64,
+        k: i64,
+        s: String,
+        items: [(i64, i64); m],
+    }
 
-    println!("{}", n);
+    let mut items_set = HashSet::new();
+    for item in items {
+        items_set.insert(item);
+    }
+
+    let mut position = (0, 0);
+    for direction in s.chars() {
+        h -= 1;
+        match direction {
+            'R' => position.0 += 1,
+            'L' => position.0 -= 1,
+            'U' => position.1 += 1,
+            'D' => position.1 -= 1,
+            _ => unreachable!(),
+        }
+        if h < 0 {
+            println!("No");
+            return;
+        }
+        if items_set.contains(&position) && h < k {
+            h = k;
+        }
+    }
+    println!("Yes");
 }
