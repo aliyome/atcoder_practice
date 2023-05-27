e81565e2674e0f68ff456ea99ed2e973ba29dde7
aliyome <aliyome@gmail.com>
2023-05-27 21:12:32 +0900

    abc303 b

--- abc303/src/bin/b.rs
+++ abc303/src/bin/b.rs
@@ -1,9 +1,29 @@
 use proconio::input;
+use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
-    };
+        m: usize,
+        a: [[Usize1; n]; m],
+    }
 
-    println!("{}", n);
+    let mut good = vec![vec![false; n]; n];
+    for i in 0..m {
+        for j in 0..n - 1 {
+            good[a[i][j]][a[i][j + 1]] = true;
+            good[a[i][j + 1]][a[i][j]] = true;
+        }
+    }
+
+    let mut count = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            if !good[i][j] {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count);
 }
