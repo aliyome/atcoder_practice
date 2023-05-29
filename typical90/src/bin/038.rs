66661ad73ef63b713916edf36daa93d6b0a108ee
aliyome <aliyome@gmail.com>
2023-05-29 14:46:29 +0900

    typical90 038 式変形

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize
+    }
+
+    let r = b / gcd(a, b);
+
+    if r > 10usize.pow(18) / a {
+        println!("Large");
+    } else {
+        println!("{}", r * a);
+    }
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }
