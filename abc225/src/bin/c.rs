5cfab1ad3032c58253443e3da5d44571710fd51c
aliyome <aliyome@gmail.com>
2023-05-29 21:49:18 +0900

    abc225 c cheat AC

--- abc225/src/bin/c.rs
+++ abc225/src/bin/c.rs
@@ -1,9 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        b: [[Usize1; m]; n]
     };
 
-    println!("{}", n);
+    for i in 0..n {
+        for j in 0..m {
+            if b[i][j] != b[0][0] + (i * 7) + j || b[0][0] / 7 != b[0][m - 1] / 7 {
+                println!("No");
+                return;
+            }
+        }
+    }
+    println!("Yes");
 }
