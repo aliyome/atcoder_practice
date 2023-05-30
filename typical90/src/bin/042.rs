ed025f9317898847d060fcdf733c1d03aac0b720
aliyome <aliyome@gmail.com>
2023-05-30 21:01:19 +0900

    typical90 042 解答AC mod 9の性質

--- typical90/src/bin/042.rs
+++ typical90/src/bin/042.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
+const MOD: usize = 1_000_000_007;
+
 fn main() {
-    todo!();
+    input! {
+        k: usize
+    }
+
+    // (N mod 9) = (N の各桁の和 mod 9)
+    if k % 9 != 0 {
+        println!("0");
+        return;
+    }
+
+    let mut dp = vec![0; k + 1];
+    dp[0] = 1;
+    for i in 1..=k {
+        let b = i.min(9);
+        for j in 1..=b {
+            dp[i] += dp[i - j];
+            dp[i] %= MOD;
+        }
+    }
+
+    println!("{:?}", dp[k]);
 }
