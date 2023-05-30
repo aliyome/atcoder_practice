b9a7b4871e3f920a04cb31f2d15339ef6bf92074
aliyome <aliyome@gmail.com>
2023-05-30 21:37:33 +0900

    abc300 再実装

--- abc300/src/bin/b.rs
+++ abc300/src/bin/b.rs
@@ -8,16 +8,50 @@ fn main() {
         b: [Chars; h]
     };
 
-    for s in 0..h {
-        for t in 0..w {
-            let mut a_shifted = a.clone();
-            for _ in 0..s {
-                a_shifted = shift_vertical(a_shifted);
+    let tate_shift = |a: Vec<Vec<char>>, n: usize| {
+        let mut a = a.clone();
+        for _ in 0..n {
+            let first_row = a[0].clone();
+            for i in 0..h - 1 {
+                a[i] = a[i + 1].clone();
             }
-            for _ in 0..t {
-                a_shifted = shift_horizontal(a_shifted);
+            a[h - 1] = first_row;
+        }
+        a
+    };
+
+    let yoko_shift = |a: Vec<Vec<char>>, n: usize| {
+        let mut a = a.clone();
+        for _ in 0..n {
+            for i in 0..h {
+                let first = a[i][0].clone();
+                for j in 0..w - 1 {
+                    a[i][j] = a[i][j + 1];
+                }
+                a[i][w - 1] = first;
             }
-            if a_shifted == b {
+        }
+        a
+    };
+
+    // 未操作
+    if a == b {
+        println!("Yes");
+        return;
+    }
+
+    // 縦方向にs回シフト
+    let mut a = a.clone();
+    for _s in 0..=h {
+        a = tate_shift(a, 1);
+        if a == b {
+            println!("Yes");
+            return;
+        }
+        // 横方向にt回シフト
+        for _t in 0..=w {
+            a = yoko_shift(a, 1);
+            if a == b {
                 println!("Yes");
                 return;
             }
@@ -25,25 +59,4 @@ fn main() {
     }
 
     println!("No");
-}
-
-fn shift_vertical(a: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let h = a.len();
-    let mut shifted = a.clone();
-    for i in 0..h {
-        shifted[i] = a[(i + 1) % h].clone();
-    }
-    shifted
-}
-
-fn shift_horizontal(a: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let h = a.len();
-    let w = a[0].len();
-    let mut shifted = a.clone();
-    for i in 0..h {
-        for j in 0..w {
-            shifted[i][j] = a[i][(j + 1) % w];
-        }
-    }
-    shifted
 }
