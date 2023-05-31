4746b1d34f3e0008b2e6ffa2efddb160177503d7
aliyome <aliyome@gmail.com>
2023-05-31 21:33:16 +0900

    abc247 d deque

--- abc247/src/bin/d.rs
+++ abc247/src/bin/d.rs
@@ -1,9 +1,44 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        q: usize,
     };
 
-    println!("{}", n);
+    let mut deq = VecDeque::new();
+
+    for _ in 0..q {
+        input! {
+            t: usize
+        }
+
+        if t == 1 {
+            input! {
+                x: usize,
+                c: usize
+            }
+            deq.push_back((x, c));
+        } else {
+            input! {
+                mut c: usize
+            }
+
+            let mut ans = 0;
+            while let Some((xx, cc)) = deq.pop_front() {
+                if c <= cc {
+                    ans += xx * c;
+                    println!("{}", ans);
+                    if c > 0 {
+                        deq.push_front((xx, cc - c));
+                    }
+                    break;
+                } else {
+                    ans += xx * cc;
+                    c -= cc;
+                }
+            }
+        }
+    }
 }
