15f4233d99c796346217ed20ec5d32c6bfa2cb66
aliyome <aliyome@gmail.com>
2023-05-31 21:23:39 +0900

    abc120 c

--- abc120/src/bin/c.rs
+++ abc120/src/bin/c.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     };
 
-    println!("{}", n);
+    let mut ans = 0;
+    let mut seq = HashMap::new();
+    *seq.entry(s[0]).or_insert(0) += 1;
+    for i in 1..s.len() {
+        *seq.entry(s[i]).or_insert(0) += 1;
+        if (s[i] == '0' && *seq.entry('1').or_default() > 0)
+            || (s[i] == '1' && *seq.entry('0').or_default() > 0)
+        {
+            ans += 2;
+            *seq.entry('0').or_default() -= 1;
+            *seq.entry('1').or_default() -= 1;
+        }
+    }
+    println!("{}", ans);
 }
