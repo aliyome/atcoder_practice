5b63d1e1cbabcb08d6857391bc71222f0745af4b
aliyome <aliyome@gmail.com>
2023-06-02 14:35:57 +0900

    typical90 048

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (n, k): (usize, usize),
+        ab: [(usize, usize); n]
+    }
+
+    let mut v = vec![];
+    for i in 0..n {
+        v.push(ab[i].1);
+        v.push(ab[i].0 - ab[i].1);
+    }
+
+    v.sort();
+
+    let mut ans = 0;
+    for _ in 0..k {
+        ans += v.pop().unwrap();
+    }
+
+    println!("{}", ans);
 }
