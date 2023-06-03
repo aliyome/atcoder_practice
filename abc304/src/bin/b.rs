b458d7fc8cdad3ee33343c6bbafd12199904bfc7
aliyome <aliyome@gmail.com>
2023-06-03 21:06:46 +0900

    abc304 b

--- abc304/src/bin/b.rs
+++ abc304/src/bin/b.rs
@@ -5,5 +5,25 @@ fn main() {
         n: usize,
     };
 
-    println!("{}", n);
+    if n <= 10usize.pow(3) - 1 {
+        println!("{}", n);
+    } else if n <= 10usize.pow(4) - 1 {
+        let x = n / 10;
+        println!("{}", x * 10)
+    } else if n <= 10usize.pow(5) - 1 {
+        let x = n / 100;
+        println!("{}", x * 100)
+    } else if n <= 10usize.pow(6) - 1 {
+        let x = n / 1000;
+        println!("{}", x * 1000)
+    } else if n <= 10usize.pow(7) - 1 {
+        let x = n / 10000;
+        println!("{}", x * 10000)
+    } else if n <= 10usize.pow(8) - 1 {
+        let x = n / 100000;
+        println!("{}", x * 100000)
+    } else if n <= 10usize.pow(9) - 1 {
+        let x = n / 1000000;
+        println!("{}", x * 1000000)
+    }
 }
