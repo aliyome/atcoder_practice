5cc336a463dedccfc5d5d07d2663c32fd9e676b5
aliyome <aliyome@gmail.com>
2023-06-04 14:28:11 +0900

    up

--- abc304/src/bin/d.rs
+++ abc304/src/bin/d.rs
@@ -6,7 +6,7 @@ fn main() {
     input! {
         _w: usize, _h: usize,
         n: usize,
-        pq: [(usize, usize); n],
+        mut pq: [(usize, usize); n],
         a: usize,
         xa: [usize; a],
         b: usize,
@@ -14,21 +14,19 @@ fn main() {
     }
 
     // O(N)
-    let total_piece = (a + 1) * (b + 1);
-    let mut berry_map = HashMap::new();
-    for (p, q) in pq {
+    let mut pieces = HashMap::new();
+    for &(p, q) in &pq {
         // 二分探索 O(logA + logB)
         let x = xa.binary_search(&p).unwrap_err();
         let y = yb.binary_search(&q).unwrap_err();
-        *berry_map.entry((x, y)).or_insert(0usize) += 1;
+        // println!("p: {}, q: {}, x: {}, y: {}", p, q, x, y);
+        *pieces.entry((x, y)).or_insert(0usize) += 1;
     }
 
-    let max = *berry_map.values().max().unwrap();
-    let mut min = *berry_map.values().min().unwrap();
-
-    if berry_map.len() < total_piece {
+    let max = *pieces.values().max().unwrap();
+    let mut min = *pieces.values().min().unwrap();
+    if (a + 1) * (b + 1) > pieces.keys().len() {
         min = 0;
     }
-
     println!("{} {}", min, max);
 }
