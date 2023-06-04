8cc9ab883e2348a61e7243cf211caf9428f6e338
aliyome <aliyome@gmail.com>
2023-06-04 21:20:04 +0900

    abc159 a

--- abc159/src/bin/a.rs
+++ abc159/src/bin/a.rs
@@ -3,7 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        m: usize,
     };
 
-    println!("{}", n);
+    let mut ans = 0;
+    if n > 0 {
+        ans += n * (n - 1) / 2;
+    }
+    if m > 0 {
+        ans += m * (m - 1) / 2;
+    }
+
+    println!("{}", ans);
 }
