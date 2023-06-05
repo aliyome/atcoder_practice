cb443436e663d90faeec4125c1bd2e2fc2f357c6
aliyome <aliyome@gmail.com>
2023-06-05 17:03:13 +0900

    tessoku a06

--- tessoku/tessoku-book/src/bin/a06.rs
+++ tessoku/tessoku-book/src/bin/a06.rs
@@ -3,7 +3,20 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      q: usize,
+      a: [isize; n],
+      lr: [(usize, usize); q],
     }
 
-    println!("{}", n);
+    // 1日目からの累積和を計算
+    let mut acc = vec![0; n + 1];
+    for i in 0..n {
+        acc[i + 1] += acc[i] + a[i];
+    }
+
+    for (l, r) in lr {
+        // l日目からr日目までの合計を計算
+        let ans = acc[r] - acc[l - 1];
+        println!("{}", ans);
+    }
 }
