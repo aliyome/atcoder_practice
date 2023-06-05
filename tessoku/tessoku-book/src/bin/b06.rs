4dea8b6d05d23e1e338ccafcf9f8734eeedab32c
aliyome <aliyome@gmail.com>
2023-06-05 17:09:16 +0900

    tessoku b06

--- tessoku/tessoku-book/src/bin/b06.rs
+++ tessoku/tessoku-book/src/bin/b06.rs
@@ -3,7 +3,27 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: [isize; n],
+      q: usize,
+      lr: [(usize, usize); q]
     }
 
-    println!("{}", n);
+    let mut ok = vec![0; n + 1];
+    let mut ng = vec![0; n + 1];
+    for i in 1..=n {
+        ok[i] = ok[i - 1] + if a[i - 1] == 1 { 1 } else { 0 };
+        ng[i] = ng[i - 1] + if a[i - 1] == 0 { 1 } else { 0 };
+    }
+
+    for (l, r) in lr {
+        let ok = ok[r] - ok[l - 1];
+        let ng = ng[r] - ng[l - 1];
+        if ok > ng {
+            println!("win");
+        } else if ok < ng {
+            println!("lose");
+        } else {
+            println!("draw");
+        }
+    }
 }
