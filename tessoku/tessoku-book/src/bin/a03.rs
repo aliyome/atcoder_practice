3587e83bc397d5d3fbf3e8c32a67f581ef59bae2
aliyome <aliyome@gmail.com>
2023-06-05 14:32:57 +0900

    tessoku a03

--- tessoku/tessoku-book/src/bin/a03.rs
+++ tessoku/tessoku-book/src/bin/a03.rs
@@ -3,7 +3,19 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      k: usize,
+      p: [usize; n],
+      q: [usize; n]
     }
 
-    println!("{}", n);
+    for &r in &p {
+        for &b in &q {
+            if r + b == k {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
