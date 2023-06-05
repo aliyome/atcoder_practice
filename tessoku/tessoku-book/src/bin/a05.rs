b2f0dd5a31b6f2ca4e52f6660364e4bd081a070b
aliyome <aliyome@gmail.com>
2023-06-05 16:50:28 +0900

    tessoku a05

--- tessoku/tessoku-book/src/bin/a05.rs
+++ tessoku/tessoku-book/src/bin/a05.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      n: isize, // 1 <= n <= 3000
+      k: isize // 3 <= k <= 9000
     }
 
-    println!("{}", n);
+    // 全探索 O(n^2)
+    let mut ans = 0;
+    for r in 1..=n {
+        for b in 1..=n {
+            let w = k - (r + b);
+            if w < 1 || n < w {
+                continue;
+            }
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
