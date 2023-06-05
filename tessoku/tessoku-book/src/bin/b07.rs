07a490096a3cd3ea8e8f29e6667a330c58fb9200
aliyome <aliyome@gmail.com>
2023-06-05 17:23:43 +0900

    tessoku b07

--- tessoku/tessoku-book/src/bin/b07.rs
+++ tessoku/tessoku-book/src/bin/b07.rs
@@ -2,8 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      t: usize, // 5x10^5
+      n: usize, // 5x10^5
+      lr: [(usize, usize); n]
     }
 
-    println!("{}", n);
+    let mut counts = vec![0; t + 1];
+    for &(l, r) in &lr {
+        for i in l..r {
+            counts[i] += 1;
+        }
+        for i in r..t {
+            counts[i] += 0;
+        }
+    }
+    for i in 0..t {
+        println!("{}", counts[i]);
+    }
 }
