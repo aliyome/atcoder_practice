20c2556aedceab1e4e58128e6d9f224eaa3591c6
aliyome <aliyome@gmail.com>
2023-06-05 17:16:49 +0900

    tessoku a07

--- tessoku/tessoku-book/src/bin/a07.rs
+++ tessoku/tessoku-book/src/bin/a07.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      d: usize, // <= 10^6
+      n: usize, // <= 10^6
+      lr: [(usize, usize); n]
     }
 
-    println!("{}", n);
+    let mut sum = vec![0; d + 1];
+    for &(l, r) in &lr {
+        for i in l..=r {
+            sum[i] += 1;
+        }
+    }
+
+    for d in 1..=d {
+        println!("{}", sum[d]);
+    }
 }
