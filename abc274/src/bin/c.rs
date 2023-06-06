55ce8d3a526c0a71caaa4fac178ca5e10d68efe1
aliyome <aliyome@gmail.com>
2023-06-06 11:14:39 +0900

    abc274 c tle

--- abc274/src/bin/c.rs
+++ abc274/src/bin/c.rs
@@ -1,9 +1,37 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        a: [usize; n]
     };
 
-    println!("{}", n);
+    let mut rev = HashMap::new();
+
+    // O(N)
+    for i in 0..n {
+        let a = a[i];
+        let next_1 = (i + 1) * 2;
+        let next_2 = (i + 1) * 2 + 1;
+
+        rev.insert(next_1, a);
+        rev.insert(next_2, a);
+    }
+
+    // O(N^2)
+    for k in 1..=2 * n + 1 {
+        if !rev.contains_key(&k) {
+            println!("0");
+            continue;
+        }
+        let mut count = 0;
+        let mut child = k;
+        while let Some(parent) = rev.get(&child) {
+            count += 1;
+            child = *parent;
+        }
+        println!("{}", count);
+    }
 }
