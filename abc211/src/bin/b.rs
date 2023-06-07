b7978dff67f76a60756ef0ea973ce1f593491dca
aliyome <aliyome@gmail.com>
2023-06-07 21:02:34 +0900

    abc211 b

--- abc211/src/bin/b.rs
+++ abc211/src/bin/b.rs
@@ -1,9 +1,24 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s1: Chars,
+        s2: Chars,
+        s3: Chars,
+        s4: Chars,
     };
 
-    println!("{}", n);
+    let mut set = HashSet::new();
+    set.insert(s1);
+    set.insert(s2);
+    set.insert(s3);
+    set.insert(s4);
+
+    if set.len() == 4 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
