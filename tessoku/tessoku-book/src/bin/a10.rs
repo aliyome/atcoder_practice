381ca0ab891508374d3e471229c04b187bb13c36
aliyome <aliyome@gmail.com>
2023-06-08 12:31:52 +0900

    tessoku a10

--- tessoku/tessoku-book/src/bin/a10.rs
+++ tessoku/tessoku-book/src/bin/a10.rs
@@ -3,7 +3,24 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: [usize; n],
+      d: usize,
+      lr: [(usize, usize); d],
     }
 
-    println!("{}", n);
+    let mut lefts = a.clone();
+    let mut rights = a.clone();
+    rights.reverse();
+    for i in 1..n {
+        lefts[i] = lefts[i].max(lefts[i - 1]);
+        rights[i] = rights[i].max(rights[i - 1]);
+    }
+
+    rights.reverse();
+
+    for (l, r) in lr {
+        let left = lefts[l - 2];
+        let right = rights[r];
+        println!("{}", left.max(right));
+    }
 }
