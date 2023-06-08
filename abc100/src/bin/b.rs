39db26d08d0360d89929b532427d29e4537de10e
aliyome <aliyome@gmail.com>
2023-06-08 21:17:02 +0900

    abc100 b

--- abc100/src/bin/b.rs
+++ abc100/src/bin/b.rs
@@ -2,8 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
+        d: usize,
         n: usize,
     };
 
-    println!("{}", n);
+    if d == 0 {
+        println!("{}", n);
+    } else {
+        println!("{}", 100usize.pow(d as u32) * n);
+    }
 }
