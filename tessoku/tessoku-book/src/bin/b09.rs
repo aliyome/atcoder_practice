c32d4379f25abd3934129c3e621584c2fa26fec0
aliyome <aliyome@gmail.com>
2023-06-08 12:13:34 +0900

    tessoku b09 2-imos

--- tessoku/tessoku-book/src/bin/b09.rs
+++ tessoku/tessoku-book/src/bin/b09.rs
@@ -3,7 +3,38 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      abcd: [(usize, usize, usize, usize); n]
     }
 
-    println!("{}", n);
+    let mut map = vec![vec![0; 1502]; 1502];
+    for (a, b, c, d) in abcd {
+        map[a][b] += 1;
+        map[c][d] += 1;
+        map[a][d] -= 1;
+        map[c][b] -= 1;
+    }
+    // H方向
+    for i in 1..=1500 {
+        for j in 1..=1500 {
+            map[i][j] += map[i - 1][j];
+        }
+    }
+    // W方向
+    for i in 1..=1500 {
+        for j in 1..=1500 {
+            map[i][j] += map[i][j - 1];
+        }
+    }
+
+    // 面積のカウント
+    let mut cnt = 0;
+    for i in 1..=1500 {
+        for j in 1..=1500 {
+            if map[i][j] > 0 {
+                cnt += 1;
+            }
+        }
+    }
+
+    println!("{}", cnt);
 }
