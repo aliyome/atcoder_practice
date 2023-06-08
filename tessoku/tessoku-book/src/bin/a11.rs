1cac30365be9dd5bb879ddb0eb811eed4a3a1bba
aliyome <aliyome@gmail.com>
2023-06-08 13:13:57 +0900

    tessoku a11 binary_search

--- tessoku/tessoku-book/src/bin/a11.rs
+++ tessoku/tessoku-book/src/bin/a11.rs
@@ -3,7 +3,22 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      x: usize,
+      a: [usize; n]
     }
 
-    println!("{}", n);
+    let mut ng = -1isize;
+    let mut ok = n as isize - 1;
+    let is_ok = |mid, key| key <= a[mid];
+
+    while (ok - ng).abs() > 1 {
+        let mid = (ng + ok) / 2;
+        if is_ok(mid as usize, x) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", ok + 1);
 }
