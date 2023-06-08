5655e9d32bed78de87143bace6be8632aba3c09b
aliyome <aliyome@gmail.com>
2023-06-08 14:41:08 +0900

    tessoku b11 binary_search meguru

--- tessoku/tessoku-book/src/bin/b11.rs
+++ tessoku/tessoku-book/src/bin/b11.rs
@@ -10,17 +10,19 @@ fn main() {
 
     a.sort();
 
+    let is_ok = |index: usize, key: usize| a[index] >= key;
+
     for x in x {
-        let mut low = 0;
-        let mut high = n;
-        while low != high {
-            let mid = (low + high) / 2;
-            if a[mid] < x {
-                low = mid + 1;
+        let mut left = -1;
+        let mut right = n as isize;
+        while right - left > 1 {
+            let mid = (left + right) / 2;
+            if is_ok(mid as usize, x) {
+                right = mid;
             } else {
-                high = mid;
+                left = mid;
             }
         }
-        println!("{}", low);
+        println!("{}", right);
     }
 }
