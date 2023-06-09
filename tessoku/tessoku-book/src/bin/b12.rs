8a1b495f0b2dc798722778e6a5fbf456a7154ee5
aliyome <aliyome@gmail.com>
2023-06-09 13:40:04 +0900

    tessoku b12 binary_search

--- tessoku/tessoku-book/src/bin/b12.rs
+++ tessoku/tessoku-book/src/bin/b12.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      n: f64,
     }
 
-    println!("{}", n);
+    let f = |x: f64| x * x * x + x;
+    let mut ng = 0.0;
+    let mut ok = 100_000.0;
+    while ok - ng > 0.001 {
+        let mid = (ok + ng) / 2.0;
+        if n <= f(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", ok);
 }
