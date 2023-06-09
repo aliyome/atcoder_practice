622a63f145e18da73a0cd768a1a21ac46fbad13b
aliyome <aliyome@gmail.com>
2023-06-09 14:13:16 +0900

    tessoku a13 shakutori

--- tessoku/tessoku-book/src/bin/a13.rs
+++ tessoku/tessoku-book/src/bin/a13.rs
@@ -3,7 +3,28 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      k: usize,
+      a: [usize; n]
     }
 
-    println!("{}", n);
+    let mut r = vec![0; n];
+    for i in 0..n {
+        // スタート地点
+        if i == 0 {
+            r[i] = 0;
+        } else {
+            r[i] = r[i - 1];
+        }
+        // ゴール地点
+        while r[i] < n - 1 && a[r[i] + 1] - a[i] <= k {
+            r[i] += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += r[i] - i;
+    }
+
+    println!("{}", ans);
 }
