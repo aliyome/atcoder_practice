cfceed198deb6fc0ec0b818f346062678d57c6c3
aliyome <aliyome@gmail.com>
2023-06-09 13:25:38 +0900

    tessoku a12 binary_search

--- tessoku/tessoku-book/src/bin/a12.rs
+++ tessoku/tessoku-book/src/bin/a12.rs
@@ -2,8 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+        n: usize,
+        k: usize,
+        a: [usize; n]
     }
-
-    println!("{}", n);
+    // 答えを二分探索する
+    let mut ng = 0;
+    let mut ok = 10usize.pow(9);
+    let total = |mid| {
+        let mut sum = 0;
+        for i in 0..n {
+            sum += mid / a[i];
+        }
+        sum
+    };
+    while ok - ng > 1 {
+        let mid = (ok + ng) / 2;
+        if k <= total(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    println!("{}", ok);
 }
