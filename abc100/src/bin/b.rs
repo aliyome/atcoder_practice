df726b6d6ffa432cac38ae5e2e8564212b226375
aliyome <aliyome@gmail.com>
2023-06-09 13:13:04 +0900

    abc100 b

--- abc100/src/bin/b.rs
+++ abc100/src/bin/b.rs
@@ -5,10 +5,25 @@ fn main() {
         d: usize,
         n: usize,
     };
+    // 100で割り切れる -> x % 100 == 0
+    // ちょうどD回割り切れる -> x % 100 == 0; x /= 100; がD回続く
 
-    if d == 0 {
-        println!("{}", n);
+    let mut count = 0;
+    for x in 1.. {
+        if f(x) == d {
+            count += 1;
+        }
+        if count == n {
+            println!("{}", x);
+            return;
+        }
+    }
+}
+
+fn f(x: usize) -> usize {
+    if x % 100 != 0 {
+        return 0;
     } else {
-        println!("{}", 100usize.pow(d as u32) * n);
+        return f(x / 100) + 1;
     }
 }
