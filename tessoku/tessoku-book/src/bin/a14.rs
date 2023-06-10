e7a893adb0c09718b0af92a3bc2d29f70e513c7b
aliyome <aliyome@gmail.com>
2023-06-10 12:41:00 +0900

    tessoku a14 acだけど最適ではない…

--- tessoku/tessoku-book/src/bin/a14.rs
+++ tessoku/tessoku-book/src/bin/a14.rs
@@ -1,9 +1,53 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
       n: usize,
+      k: isize,
+      mut a: [isize; n],
+      mut b: [isize; n],
+      mut c: [isize; n],
+      mut d: [isize; n],
     }
 
-    println!("{}", n);
+    a.sort();
+    b.sort();
+    c.sort();
+    d.sort();
+
+    a.reverse();
+    b.reverse();
+    c.reverse();
+    d.reverse();
+
+    // O(N^3) -> O(10^12) TLE
+    let mut set_d = HashSet::new();
+    for &d in &d {
+        set_d.insert(d);
+    }
+
+    for &a in &a {
+        if a >= k {
+            continue;
+        }
+        for &b in &b {
+            if a + b >= k {
+                continue;
+            }
+            for &c in &c {
+                if a + b + c >= k {
+                    continue;
+                }
+                let d = k - (a + b + c);
+                if set_d.contains(&d) {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("No");
 }
