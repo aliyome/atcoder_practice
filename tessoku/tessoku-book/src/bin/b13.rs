bea85d0245a29efe682d5445f24b365f96572fdb
aliyome <aliyome@gmail.com>
2023-06-10 11:40:20 +0900

    tessoku b13 ac

--- tessoku/tessoku-book/src/bin/b13.rs
+++ tessoku/tessoku-book/src/bin/b13.rs
@@ -4,38 +4,36 @@ fn main() {
     input! {
       n: usize,
       k: usize,
-      a: [usize; n]
+      aa: [usize; n]
     }
+    // 1-index
+    let mut a = vec![0usize; n + 1];
+    for i in 1..=n {
+        a[i] = aa[i - 1];
+    }
+    // println!("{:?}", a);
 
     // 累積和
-    let mut sum = a.clone();
-    for i in 1..n {
-        sum[i] += sum[i - 1];
+    let mut sum = vec![0usize; n + 1];
+    for i in 1..=n {
+        sum[i] += sum[i - 1] + a[i];
     }
+    // println!("{:?}", sum);
 
     // しゃくとり法
-    let total = |l, r| {
-        if l == 0 {
-            sum[r]
-        } else {
-            sum[r] - sum[l - 1]
-        }
-    };
-    let mut r = vec![0; n];
-    for l in 0..n {
-        if l == 0 {
-            r[l] = 0;
-        } else {
-            r[l] = r[l - 1];
-        }
-        while r[l] < n - 1 && total(l, r[l] + 1) <= k {
+    let total = |l, r| sum[r] - sum[l - 1];
+    let mut r = vec![0; n + 1];
+    for l in 1..=n {
+        r[l] = r[l - 1];
+        while r[l] < n && total(l, r[l] + 1) <= k {
             r[l] += 1;
         }
     }
+    // println!("{:?}", r);
 
     // 出力
     let mut ans = 0;
-    for l in 0..n {
+    for l in 1..=n {
         ans += r[l] - l + 1
     }
 
