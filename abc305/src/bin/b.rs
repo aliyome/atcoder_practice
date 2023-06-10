1b1f2d95b6097ecdcb5322bcbdbbe7c56911dc3f
aliyome <aliyome@gmail.com>
2023-06-10 21:11:28 +0900

    abc305 b

--- abc305/src/bin/b.rs
+++ abc305/src/bin/b.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        p: char,
+        q: char
     };
 
-    println!("{}", n);
+    let dists = vec![0, 3, 1, 4, 1, 5, 9];
+    let mut sum = dists.clone();
+    for i in 1..dists.len() {
+        sum[i] += sum[i - 1];
+    }
+
+    let p = p as usize - 'A' as usize;
+    let q = q as usize - 'A' as usize;
+
+    if p < q {
+        println!("{}", sum[q] - sum[p]);
+    } else {
+        println!("{}", sum[p] - sum[q]);
+    }
 }
