a54028c431e4922a2492422da376b07e46e036c2
aliyome <aliyome@gmail.com>
2023-06-10 14:21:42 +0900

    tessoku a15

--- tessoku/tessoku-book/src/bin/a15.rs
+++ tessoku/tessoku-book/src/bin/a15.rs
@@ -3,7 +3,35 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: [usize; n]
     }
 
-    println!("{}", n);
+    let mut b: Vec<(usize, usize)> = a.clone().into_iter().enumerate().collect();
+    b.sort_by(|x, y| x.1.cmp(&y.1));
+    let mut c = vec![];
+    let mut prev = 0;
+    for i in 0..n {
+        let (index, val) = b[i];
+        if i == 0 {
+            c.push((index, 1));
+            prev = 1;
+            continue;
+        }
+        if val == b[i - 1].1 {
+            c.push((index, prev));
+        } else {
+            c.push((index, prev + 1));
+            prev += 1;
+        }
+    }
+
+    c.sort_by(|x, y| x.0.cmp(&y.0));
+
+    println!(
+        "{}",
+        c.iter()
+            .map(|x| x.1.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
