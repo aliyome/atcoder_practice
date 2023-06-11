2b829afadc0a9cfa7488d15f2c03c8e313792982
aliyome <aliyome@gmail.com>
2023-06-11 12:20:56 +0900

    tessoku b16 dp

--- tessoku/tessoku-book/src/bin/b16.rs
+++ tessoku/tessoku-book/src/bin/b16.rs
@@ -3,7 +3,16 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      h: [isize; n]
     }
 
-    println!("{}", n);
+    let mut dp = vec![100000000; n];
+    dp[0] = 0;
+    dp[1] = (h[1] - h[0]).abs();
+    for i in 2..n {
+        dp[i] = dp[i].min(dp[i - 1] + (h[i] - h[i - 1]).abs());
+        dp[i] = dp[i].min(dp[i - 2] + (h[i] - h[i - 2]).abs());
+    }
+
+    println!("{}", dp[n - 1]);
 }
