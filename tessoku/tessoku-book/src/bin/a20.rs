755b891a0c0c3d8462c6254f2258cccf02bb6c41
aliyome <aliyome@gmail.com>
2023-06-12 12:12:27 +0900

    tessoku a20 lcs

--- tessoku/tessoku-book/src/bin/a20.rs
+++ tessoku/tessoku-book/src/bin/a20.rs
@@ -1,9 +1,37 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-      n: usize,
+      _s: Chars,
+      _t: Chars
     }
 
-    println!("{}", n);
+    // 1-indexed
+    let mut s = vec!['@'];
+    s.extend(_s);
+    let mut t = vec!['@'];
+    t.extend(_t);
+
+    // dp[i][j] := s の i 文字目までと t の j 文字目までの LCS の長さ
+    let mut dp = vec![vec![0; t.len()]; s.len()];
+
+    for i in 1..s.len() {
+        for j in 1..t.len() {
+            // i - 1 引き継ぎ
+            dp[i][j] = dp[i - 1][j];
+            // j - 1 引き継ぎ
+            dp[i][j] = dp[i][j].max(dp[i][j - 1]);
+            // 連結成分追加
+            if s[i] == t[j] {
+                dp[i][j] = dp[i][j].max(dp[i - 1][j - 1] + 1);
+            }
+        }
+    }
+
+    let mut max = 0;
+    for j in 1..t.len() {
+        max = max.max(dp[s.len() - 1][j]);
+    }
+
+    println!("{}", max);
 }
