d14a8fb9b2241c21d147e0205f76c6a9adff20e5
aliyome <aliyome@gmail.com>
2023-06-14 21:01:45 +0900

    abc237 a

--- abc237/src/bin/a.rs
+++ abc237/src/bin/a.rs
@@ -2,8 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        n: isize,
     };
 
-    println!("{}", n);
+    if -2isize.pow(31) <= n && n < 2isize.pow(31) - 1 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
