1611bba42c946f3cbb1eafd643682356d55f2b41
aliyome <aliyome@gmail.com>
2023-06-14 14:22:21 +0900

    tessoku a22 貰う配るDP

--- tessoku/tessoku-book/src/bin/a22.rs
+++ tessoku/tessoku-book/src/bin/a22.rs
@@ -3,10 +3,11 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
-      mut a: [usize; n - 1], // a[i] に進み、スコア100をえる
-      mut b: [usize; n - 1], // b[i] に進み、スコア150をえる
+      a: [usize; n - 1], // a[i] に進み、スコア100をえる
+      b: [usize; n - 1], // b[i] に進み、スコア150をえる
     }
 
+    // もらうDP
     // edges[i] := iマス目が遷移先のマスリスト
     let mut edges_a = vec![vec![]; n + 1];
     let mut edges_b = vec![vec![]; n + 1];
@@ -16,7 +17,7 @@ fn main() {
     }
 
     // dp[i] := iマス目にいるときの最大スコア
-    let mut dp = vec![0; n + 1];
+    let mut dp = vec![-10isize.pow(8); n + 1];
     dp[1] = 0;
     for i in 2..=n {
         // iマス目には edges_a[i], edges_b[i] からしか遷移できない
@@ -29,4 +30,19 @@ fn main() {
     }
 
     println!("{}", dp[n]);
+
+    // // 1-indexed
+    // a.insert(0, 0);
+    // b.insert(0, 0);
+
+    // // 配るDP
+    // let mut dp = vec![-10isize.pow(8); n + 1];
+    // dp[0] = 0;
+    // dp[1] = 0;
+    // for i in 1..n {
+    //     dp[a[i]] = dp[a[i]].max(dp[i] + 100);
+    //     dp[b[i]] = dp[b[i]].max(dp[i] + 150);
+    // }
+
+    // println!("{}", dp[n]);
 }
