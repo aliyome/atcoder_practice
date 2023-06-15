56473236998e50ba76ba7b29a06741e5f0661e53
aliyome <aliyome@gmail.com>
2023-06-15 21:19:02 +0900

    abc115 c

--- abc115/src/bin/c.rs
+++ abc115/src/bin/c.rs
@@ -3,7 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        k: usize,
+        mut h: [isize; n]
     };
 
-    println!("{}", n);
+    h.sort();
+
+    let mut ans = std::isize::MAX;
+    for l in 0..=n - k {
+        let r = l + k - 1;
+        ans = ans.min(h[r] - h[l]);
+    }
+
+    println!("{}", ans);
 }
