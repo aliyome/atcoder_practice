eeeb43d737c0998a4b836d21a529756b7bc5765b
aliyome <aliyome@gmail.com>
2023-06-16 21:01:56 +0900

    abc176 a

--- abc176/src/bin/a.rs
+++ abc176/src/bin/a.rs
@@ -3,7 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        x: usize,
+        t: usize,
     };
 
-    println!("{}", n);
+    if n % x == 0 {
+        println!("{}", n / x * t);
+    } else {
+        println!("{}", (n / x + 1) * t);
+    }
 }
