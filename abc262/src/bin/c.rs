206107690f407a1e3ed6d2cb69ac049a7c968e71
aliyome <aliyome@gmail.com>
2023-06-16 21:09:08 +0900

    abc262 c tle

--- abc262/src/bin/c.rs
+++ abc262/src/bin/c.rs
@@ -3,7 +3,19 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        mut a: [usize; n]
     };
 
-    println!("{}", n);
+    a.insert(0, 0);
+
+    let mut ans = 0;
+    for i in 1..n {
+        for j in i + 1..=n {
+            if a[i].min(a[j]) == i && a[i].max(a[j]) == j {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
