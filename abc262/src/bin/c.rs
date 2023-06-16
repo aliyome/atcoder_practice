69b39d4ec7185dc0aa962822eb6e99140983e4d3
aliyome <aliyome@gmail.com>
2023-06-16 22:01:37 +0900

    abc262 c 等差数列

--- abc262/src/bin/c.rs
+++ abc262/src/bin/c.rs
@@ -8,14 +8,22 @@ fn main() {
 
     a.insert(0, 0);
 
-    let mut ans = 0;
-    for i in 1..n {
-        for j in i + 1..=n {
-            if a[i].min(a[j]) == i && a[i].max(a[j]) == j {
-                ans += 1;
-            }
+    let mut ans: usize = 0;
+    // case1: ai = i && aj = j && i < j となる個数
+    let mut count = 0;
+    for i in 1..=n {
+        if a[i] == i {
+            count += 1;
         }
     }
+    ans += count * (count - 1) / 2;
 
+    // case2: ai = j && aj = i && i < j となる個数
+    for i in 1..n {
+        let j = a[i];
+        if i < j && a[j] == i {
+            ans += 1;
+        }
+    }
     println!("{}", ans);
 }
