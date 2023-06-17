2f4fae44517857946b78ad4ff99451d3e0374532
aliyome <aliyome@gmail.com>
2023-06-17 21:13:05 +0900

    abc306 c

--- abc306/src/bin/c.rs
+++ abc306/src/bin/c.rs
@@ -1,9 +1,27 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        a: [usize; n * 3]
     };
 
-    println!("{}", n);
+    let mut map = HashMap::new();
+
+    for i in 0..n * 3 {
+        map.entry(a[i]).or_insert(vec![]).push(i);
+    }
+
+    let mut list = vec![];
+    for (k, v) in map {
+        list.push((k, v[1]));
+    }
+
+    list.sort_by(|a, b| a.1.cmp(&b.1));
+
+    for (k, _) in list {
+        print!("{} ", k);
+    }
 }
