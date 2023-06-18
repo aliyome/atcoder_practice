a7f89d28cc2d226cd57a9c83637df861a2485113
aliyome <aliyome@gmail.com>
2023-06-18 13:43:57 +0900

    tessoku a24 tle

--- tessoku/tessoku-book/src/bin/a24.rs
+++ tessoku/tessoku-book/src/bin/a24.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      n: usize, // <= 10^5
+      a: [usize; n] // a[i] <= 5*10^5
     }
 
-    println!("{}", n);
+    // dp[i] := ai が最後の要素となる最長の増加部分列の長さ
+    let mut dp = vec![0usize; n];
+    for i in 0..n {
+        dp[i] = 1;
+        for j in 0..i {
+            if a[j] < a[i] {
+                dp[i] = dp[i].max(dp[j] + 1);
+            }
+        }
+    }
+
+    println!("{:?}", dp[n - 1]);
 }
