612898ba597c2c0d81edf1cf9c4f2fd05dfa1389
aliyome <aliyome@gmail.com>
2023-06-19 16:49:10 +0900

    arc162 WA

---
+++ arc162/src/bin/a.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        t: usize,
+    };
+
+    for _ in 0..t {
+        input! {
+            n: usize,
+            p: [usize; n]
+        };
+    }
+}
