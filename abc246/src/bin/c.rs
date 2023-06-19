60e9132430b405a3c4e5693cdec0496a48f67324
aliyome <aliyome@gmail.com>
2023-06-19 21:41:45 +0900

    abc246 c

--- abc246/src/bin/c.rs
+++ abc246/src/bin/c.rs
@@ -3,7 +3,38 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        mut k: isize,
+        x: isize,
+        mut a: [isize; n]
     };
 
-    println!("{}", n);
+    for i in 0..n {
+        let cnt = a[i] / x;
+        if cnt <= k {
+            a[i] -= cnt * x;
+            k -= cnt;
+        } else {
+            a[i] -= k * x;
+            k = 0;
+        }
+        if k == 0 {
+            break;
+        }
+    }
+
+    if k > 0 {
+        a.sort();
+        a.reverse();
+        for i in 0..n {
+            if a[i] > 0 {
+                a[i] = 0;
+                k -= 1;
+            }
+            if k == 0 {
+                break;
+            }
+        }
+    }
+
+    println!("{}", a.iter().sum::<isize>());
 }
