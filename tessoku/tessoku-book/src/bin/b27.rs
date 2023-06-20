78c8c79b7cf0c7340ef4f91ec43604ae802d3312
aliyome <aliyome@gmail.com>
2023-06-20 13:18:04 +0900

    tessoku b27 lcm

--- tessoku/tessoku-book/src/bin/b27.rs
+++ tessoku/tessoku-book/src/bin/b27.rs
@@ -2,8 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      a: usize,
+      b: usize
     }
 
-    println!("{}", n);
+    println!("{}", lcm(a, b));
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if a == 0 {
+        return b;
+    }
+    if b == 0 {
+        return a;
+    }
+
+    if a < b {
+        gcd(a, b % a)
+    } else {
+        gcd(a % b, b)
+    }
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
 }
