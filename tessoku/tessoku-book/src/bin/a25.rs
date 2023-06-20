454ff813ad19c48028368ffd41ba1c0ce1c4f5f7
aliyome <aliyome@gmail.com>
2023-06-20 12:11:39 +0900

    tessoku a25 dp

--- tessoku/tessoku-book/src/bin/a25.rs
+++ tessoku/tessoku-book/src/bin/a25.rs
@@ -1,9 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-      n: usize,
+      h: usize,
+      w: usize,
+      c: [Chars; h]
     }
 
-    println!("{}", n);
+    let mut dp = vec![vec![0usize; w + 1]; h + 1];
+    dp[1][1] = 1;
+    for x in 1..=h {
+        for y in 1..=w {
+            if x == 1 && y == 1 {
+                continue;
+            }
+            let ix = x - 1;
+            let iy = y - 1;
+            if c[ix][iy] == '#' {
+                continue;
+            }
+            dp[x][y] = dp[x - 1][y] + dp[x][y - 1];
+        }
+    }
+
+    println!("{}", dp[h][w]);
 }
