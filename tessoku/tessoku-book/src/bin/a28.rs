dc5cfee50fea5459f4fc221cb855a7f25613119c
aliyome <aliyome@gmail.com>
2023-06-20 13:26:56 +0900

    tessoku a28 mod

--- tessoku/tessoku-book/src/bin/a28.rs
+++ tessoku/tessoku-book/src/bin/a28.rs
@@ -3,7 +3,25 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      ta: [(char, usize); n],
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..n {
+        let (t, a) = ta[i];
+        match t {
+            '+' => ans += a,
+            '-' => ans -= a,
+            '*' => ans *= a,
+            _ => unreachable!(),
+        }
+
+        ans %= 10000;
+        println!("{}", ans);
+
+        // ans -= a で負の数にならないようにする
+        if ans < 10000 {
+            ans += 10000;
+        }
+    }
 }
