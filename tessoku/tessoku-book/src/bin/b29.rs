f128b11dfdf86c14415ab25267eef97c4c4f16cd
aliyome <aliyome@gmail.com>
2023-06-20 14:10:18 +0900

    tessoku b29 繰り返し二乗法

--- tessoku/tessoku-book/src/bin/b29.rs
+++ tessoku/tessoku-book/src/bin/b29.rs
@@ -1,9 +1,34 @@
 use proconio::input;
 
+const MOD: usize = 1000000007;
+
 fn main() {
     input! {
-      n: usize,
+      a: usize,
+      b: usize,
     }
 
-    println!("{}", n);
+    let mut ex = vec![(1, a)];
+    while let Some(&(i, prev)) = ex.last() {
+        if i * 2 > b {
+            break;
+        }
+        let mut next = prev * prev;
+        next %= MOD;
+        ex.push((i * 2, next));
+    }
+
+    let mut b = b;
+    let mut ans = 1;
+    while b > 0 {
+        let &(i, prev) = ex.last().unwrap();
+        if i <= b {
+            b -= i;
+            ans *= prev;
+            ans %= MOD;
+        }
+        ex.pop();
+    }
+
+    println!("{}", ans);
 }
