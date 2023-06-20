7a59c87b2249cf4d10ac75147081b04ec9dd2c43
aliyome <aliyome@gmail.com>
2023-06-20 13:12:26 +0900

    tessoku a27 gcd

--- tessoku/tessoku-book/src/bin/a27.rs
+++ tessoku/tessoku-book/src/bin/a27.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      a: usize,
+      b: usize,
     }
 
-    println!("{}", n);
+    println!("{}", gcd(a, b));
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if a == 0 {
+        return b;
+    } else if b == 0 {
+        return a;
+    }
+    if a > b {
+        return gcd(a % b, b);
+    } else {
+        return gcd(a, b % a);
+    }
 }
