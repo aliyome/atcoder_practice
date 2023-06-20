e5f7abde364d3f79057e701ed6d848d512b3a09e
aliyome <aliyome@gmail.com>
2023-06-20 13:34:12 +0900

    tessoku b28 fib

--- tessoku/tessoku-book/src/bin/b28.rs
+++ tessoku/tessoku-book/src/bin/b28.rs
@@ -5,5 +5,14 @@ fn main() {
       n: usize,
     }
 
-    println!("{}", n);
+    let mut fib = vec![0; n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+    fib[2] = 1;
+    for i in 3..=n {
+        fib[i] = fib[i - 1] + fib[i - 2];
+        fib[i] %= 10usize.pow(9) + 7;
+    }
+
+    println!("{}", fib[n]);
 }
