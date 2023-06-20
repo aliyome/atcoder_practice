69b0c8d19b331eb5ea0178eaa926ca138722e3db
aliyome <aliyome@gmail.com>
2023-06-20 13:04:54 +0900

    tessoku b26 prime numbers

--- tessoku/tessoku-book/src/bin/b26.rs
+++ tessoku/tessoku-book/src/bin/b26.rs
@@ -5,5 +5,30 @@ fn main() {
       n: usize,
     }
 
-    println!("{}", n);
+    let is_primes = sieve(n);
+    for i in 2..=n {
+        if is_primes[i] {
+            println!("{}", i);
+        }
+    }
+}
+
+fn sieve(n: usize) -> Vec<bool> {
+    let mut is_primes = vec![true; n + 1];
+    is_primes[0] = false;
+    is_primes[1] = false;
+
+    for i in 2..=(n as f64).sqrt() as usize {
+        if !is_primes[i] {
+            continue;
+        }
+
+        let mut j = i + i;
+        while j <= n {
+            is_primes[j] = false;
+            j += i;
+        }
+    }
+
+    is_primes
 }
