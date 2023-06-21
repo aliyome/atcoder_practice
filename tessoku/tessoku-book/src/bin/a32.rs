4cbcd1fe2e49fdd56fed1448bf166551b38df050
aliyome <aliyome@gmail.com>
2023-06-21 14:20:13 +0900

    tessoku a32 grundy?

--- tessoku/tessoku-book/src/bin/a32.rs
+++ tessoku/tessoku-book/src/bin/a32.rs
@@ -3,7 +3,31 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: usize,
+      b: usize
     }
 
-    println!("{}", n);
+    // 石が x になった時、その手番の人が負け
+    let x = a.min(b) - 1;
+    // dp[i] := 石が i になった時、その手番の人が勝つかどうか
+    let mut dp = vec![false; n + 1];
+    for i in 0..=x {
+        dp[i] = false;
+    }
+
+    for i in x + 1..=n {
+        let a_win = a <= i && dp[i - a];
+        let b_win = b <= i && dp[i - b];
+        dp[i] = !(a_win && b_win);
+        // println!(
+        //     "i: {}, a_win: {}, b_win: {}, win?: {}",
+        //     i, a_win, b_win, dp[i]
+        // );
+    }
+
+    if dp[n] {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
