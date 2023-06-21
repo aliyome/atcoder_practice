3a93121ebecf59bd85e93720bfc1001f14b82271
aliyome <aliyome@gmail.com>
2023-06-21 14:38:14 +0900

    tessoku b32 grundy 2

--- tessoku/tessoku-book/src/bin/b32.rs
+++ tessoku/tessoku-book/src/bin/b32.rs
@@ -3,7 +3,27 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      k: usize,
+      a: [usize; k]
     }
 
-    println!("{}", n);
+    let mut dp = vec![false; n + 1];
+    // 手番 i が
+    for i in 0..=n {
+        let mut win = false;
+        for &a in &a {
+            // 手番 i で a 個取ったときに相手を負けにできるなら、勝ち
+            if a <= i && !dp[i - a] {
+                win = true;
+                break;
+            }
+        }
+        dp[i] = win;
+    }
+
+    if dp[n] {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
