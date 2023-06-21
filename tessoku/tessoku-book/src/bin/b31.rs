437567817cd09432a347897fba00b30b8f1ffbc0
aliyome <aliyome@gmail.com>
2023-06-21 13:19:49 +0900

    tessoku b31

--- tessoku/tessoku-book/src/bin/b31.rs
+++ tessoku/tessoku-book/src/bin/b31.rs
@@ -5,5 +5,17 @@ fn main() {
       n: usize,
     }
 
-    println!("{}", n);
+    let a = n / 3;
+    let b = n / 5;
+    let c = n / 7;
+
+    let ab = n / (3 * 5);
+    let bc = n / (5 * 7);
+    let ca = n / (7 * 3);
+
+    let abc = n / (3 * 5 * 7);
+
+    let ans = a + b + c - (ab + bc + ca) + abc;
+
+    println!("{}", ans);
 }
