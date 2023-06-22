3d3ab7ea47092ede48954caac4019b4e6286ff6c
aliyome <aliyome@gmail.com>
2023-06-22 14:43:22 +0900

    tessoku b33 nim

--- tessoku/tessoku-book/src/bin/b33.rs
+++ tessoku/tessoku-book/src/bin/b33.rs
@@ -3,7 +3,24 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      _h: usize,
+      _w: usize,
+      ab: [(usize, usize); n]
     }
 
-    println!("{}", n);
+    // Nim の亜種
+    let mut can_move_count = vec![];
+    for i in 0..n {
+        let (a, b) = ab[i];
+        can_move_count.push(a - 1);
+        can_move_count.push(b - 1);
+    }
+
+    let xor = can_move_count.iter().fold(0, |acc, x| acc ^ x);
+
+    if xor == 0 {
+        println!("Second");
+    } else {
+        println!("First");
+    }
 }
