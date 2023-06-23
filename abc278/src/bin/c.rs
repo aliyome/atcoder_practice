431b417eb7bc3b72f82b1b0e9e2bb7e196b011d2
aliyome <aliyome@gmail.com>
2023-06-23 21:13:59 +0900

    abc278 c

--- abc278/src/bin/c.rs
+++ abc278/src/bin/c.rs
@@ -1,9 +1,32 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        n: usize, // <=10^9
+        q: usize, // <=2x10^5
+        tab: [(usize, usize, usize); q]
     };
 
-    println!("{}", n);
+    let mut set = HashSet::new();
+
+    for &(t, a, b) in &tab {
+        match t {
+            1 => {
+                set.insert((a, b));
+            }
+            2 => {
+                set.remove(&(a, b));
+            }
+            3 => {
+                if set.contains(&(a, b)) && set.contains(&(b, a)) {
+                    println!("Yes");
+                } else {
+                    println!("No");
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
 }
