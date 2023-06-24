d1210329b0fda1fe66812f561149b5cb6a46c784
aliyome <aliyome@gmail.com>
2023-06-24 21:08:10 +0900

    abc307 b

--- abc307/src/bin/b.rs
+++ abc307/src/bin/b.rs
@@ -1,9 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
+        s: [Chars; n]
     };
 
-    println!("{}", n);
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            let mut a = s[i].clone();
+            a.extend(s[j].clone());
+
+            let mut is_ok = true;
+            for k in 0..a.len() {
+                if a[k] == a[a.len() - 1 - k] {
+                    continue;
+                } else {
+                    is_ok = false;
+                    break;
+                }
+            }
+            if is_ok {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
