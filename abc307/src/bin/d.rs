9c0ccb9737b6cdaa31fbc11571716f8d340dd211
aliyome <aliyome@gmail.com>
2023-06-24 22:23:22 +0900

    abc307 d

--- abc307/src/bin/d.rs
+++ abc307/src/bin/d.rs
@@ -1,9 +1,34 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-    };
+        _n: usize,
+        s: Chars,
+    }
 
-    println!("{}", n);
+    let mut stack = vec![Vec::new()];
+    for c in s {
+        match c {
+            '(' => {
+                stack.push(Vec::new());
+                let n = stack.len();
+                stack[n - 1].push(c);
+            }
+            ')' => {
+                let n = stack.len();
+                if stack.len() == 1 {
+                    stack[n - 1].push(c);
+                } else {
+                    stack.pop();
+                }
+            }
+            _ => {
+                let n = stack.len();
+                stack[n - 1].push(c);
+            }
+        }
+    }
+
+    let ans: String = stack.into_iter().flatten().collect();
+    println!("{}", ans);
 }
