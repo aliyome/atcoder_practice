5dbc6276b08b0f0993105740fa049f83c4c0cb21
aliyome <aliyome@gmail.com>
2023-06-28 13:42:37 +0900

    tessoku a17

--- tessoku/tessoku-book/src/bin/a37.rs
+++ tessoku/tessoku-book/src/bin/a37.rs
@@ -2,8 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      (n, m, b): (usize, usize, usize),
+      a: [usize; n],
+      c: [usize; m],
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    ans += b * n * m;
+    ans += a.iter().sum::<usize>() * m;
+    ans += c.iter().sum::<usize>() * n;
+    println!("{}", ans);
 }
