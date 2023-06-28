28118620de03d48c7e2f30b2d76e63f134a0000a
aliyome <aliyome@gmail.com>
2023-06-28 11:45:38 +0900

    tessoku a34 grundy

--- tessoku/tessoku-book/src/bin/a34.rs
+++ tessoku/tessoku-book/src/bin/a34.rs
@@ -3,7 +3,46 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      x: usize,
+      y: usize,
+      a:[usize; n]
     }
 
-    println!("{}", n);
+    // Grundy数
+    let mut grundy = vec![std::usize::MAX; 100001];
+    grundy[0] = 0;
+
+    // i: 石の数
+    for i in 1..=100000 {
+        // 到達可能なgrundy数を集める
+        let mut gg = vec![];
+        if i >= x {
+            gg.push(grundy[i - x]);
+        }
+        if i >= y {
+            gg.push(grundy[i - y]);
+        }
+
+        // grundy数
+        for k in 0..=100000 {
+            if gg.contains(&k) {
+                continue;
+            } else {
+                grundy[i] = k;
+                break;
+            }
+        }
+    }
+
+    // 山iのgrundy数をXORする
+    let mut ans = 0;
+    for i in 0..n {
+        ans ^= grundy[a[i]];
+    }
+
+    if ans != 0 {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
