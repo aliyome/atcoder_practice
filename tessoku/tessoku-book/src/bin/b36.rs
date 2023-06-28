ecd727be78454e2dbba73ab2cc2fe4cb65b053a2
aliyome <aliyome@gmail.com>
2023-06-28 13:32:43 +0900

    tessoku b36

--- tessoku/tessoku-book/src/bin/b36.rs
+++ tessoku/tessoku-book/src/bin/b36.rs
@@ -1,9 +1,17 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
       n: usize,
+      k: isize,
+      s: Chars
     }
 
-    println!("{}", n);
+    let on_count = s.iter().filter(|&c| *c == '1').count();
+
+    if (k - on_count as isize).abs() % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
