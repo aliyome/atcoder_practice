5c62342e10059d3561168db7d95e77e98feb3c4c
aliyome <aliyome@gmail.com>
2023-06-28 12:18:59 +0900

    tessoku b34 grundy

--- tessoku/tessoku-book/src/bin/b34.rs
+++ tessoku/tessoku-book/src/bin/b34.rs
@@ -2,8 +2,32 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      n: usize, // <= 100000
+      _x: usize, // = 2
+      _y: usize, // = 3
+      a: [usize; n] // <= 10^18
     }
 
-    println!("{}", n);
+    // Grundy数
+    let mut ans = 0;
+    for i in 0..n {
+        match a[i] % 5 {
+            0 | 1 => {
+                ans ^= 0;
+            }
+            2 | 3 => {
+                ans ^= 1;
+            }
+            4 => {
+                ans ^= 2;
+            }
+            _ => {}
+        }
+    }
+
+    if ans != 0 {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
