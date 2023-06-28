974537d7ab37e1ab9a1c8c51bb3bf43760ed9748
aliyome <aliyome@gmail.com>
2023-06-28 13:24:53 +0900

    tessoku a36

--- tessoku/tessoku-book/src/bin/a36.rs
+++ tessoku/tessoku-book/src/bin/a36.rs
@@ -3,7 +3,13 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      k: usize,
     }
 
-    println!("{}", n);
+    let move_count = n * 2 - 2;
+    if move_count <= k && k % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
