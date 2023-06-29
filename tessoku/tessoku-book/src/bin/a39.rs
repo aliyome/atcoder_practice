ac845a7f8fe3ec0c5d32e0a7c7aa2183c5ff7ff6
aliyome <aliyome@gmail.com>
2023-06-29 10:49:49 +0900

    tessoku a39

--- tessoku/tessoku-book/src/bin/a39.rs
+++ tessoku/tessoku-book/src/bin/a39.rs
@@ -3,7 +3,19 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      mut lr: [(usize, usize); n ]
     }
 
-    println!("{}", n);
+    lr.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let mut count = 0;
+    let mut curr = 0;
+    for &(l, r) in &lr {
+        if curr <= l {
+            curr = r;
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
 }
