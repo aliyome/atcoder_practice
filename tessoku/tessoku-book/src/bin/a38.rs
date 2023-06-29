1e919b71b1af38f8aafdcfde2fb05ee7576ad78e
aliyome <aliyome@gmail.com>
2023-06-29 10:11:40 +0900

    tessoku a38

--- tessoku/tessoku-book/src/bin/a38.rs
+++ tessoku/tessoku-book/src/bin/a38.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
+      d: usize,
       n: usize,
+      lrh: [(usize, usize, usize); n],
     }
 
-    println!("{}", n);
+    let mut days = vec![24; d + 1];
+    for &(l, r, h) in &lrh {
+        for i in l..=r {
+            days[i] = days[i].min(h);
+        }
+    }
+
+    let mut ans = 0;
+    for i in 1..=d {
+        ans += days[i];
+    }
+
+    println!("{}", ans);
 }
