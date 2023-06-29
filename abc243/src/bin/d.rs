b01306026892021810ecfc1c7c8070afb9d7c379
aliyome <aliyome@gmail.com>
2023-06-29 09:26:18 +0900

    abc243 d

--- abc243/src/bin/d.rs
+++ abc243/src/bin/d.rs
@@ -7,7 +7,24 @@ fn main() {
         s: Chars
     };
 
-    for c in s {
+    // 高速化
+    let mut t = vec![];
+    for &c in &s {
+        match c {
+            'U' => {
+                if t.last() == Some(&'L') || t.last() == Some(&'R') {
+                    t.pop();
+                } else {
+                    t.push(c);
+                }
+            }
+            'L' | 'R' => t.push(c),
+            _ => unreachable!(),
+        }
+    }
+
+    // 愚直に計算
+    for c in t {
         match c {
             'U' => x /= 2,
             'L' => x *= 2,
