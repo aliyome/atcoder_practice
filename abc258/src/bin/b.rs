56f297406ec7244a532f60d52c4e4106d2be6d4f
aliyome <aliyome@gmail.com>
2023-06-29 21:34:26 +0900

    abc258 b

--- abc258/src/bin/b.rs
+++ abc258/src/bin/b.rs
@@ -12,12 +12,38 @@ fn main() {
         let mut list = vec![];
         let mut i = i;
         let mut j = j;
-        for _ in 0..n {
-            j = (j + 1) % n;
-            i = (i + 1) % n;
-            list.push(a[i][j]);
+
+        let v = [
+            (0isize, 1isize),
+            (1, 0),
+            (0, -1),
+            (-1, 0),
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1),
+        ];
+
+        for &(di, dj) in v.iter() {
+            let mut str = vec![];
+            for _ in 0..n {
+                let mut jj = j as isize + dj;
+                let mut ii = i as isize + di;
+                if jj < 0 {
+                    jj += n as isize;
+                }
+                if ii < 0 {
+                    ii += n as isize;
+                }
+                j = jj as usize % n;
+                i = ii as usize % n;
+                str.push(a[i][j]);
+            }
+            list.push(str.iter().collect::<String>());
         }
-        list.iter().collect::<String>()
+        list.sort();
+        list.reverse();
+        list[0].clone()
     };
 
     for i in 0..n {
