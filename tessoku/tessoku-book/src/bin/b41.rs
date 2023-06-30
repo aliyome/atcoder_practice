786134e3dc3fb4fdf2a7e53c336773807fa698f9
aliyome <aliyome@gmail.com>
2023-06-30 13:49:16 +0900

    tessoku b41

--- tessoku/tessoku-book/src/bin/b41.rs
+++ tessoku/tessoku-book/src/bin/b41.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      x: usize,
+      y: usize,
     }
 
-    println!("{}", n);
+    let mut ans = vec![];
+    ans.push((x, y));
+
+    while let Some(&(x, y)) = ans.last() {
+        if (x, y) == (1, 1) {
+            break;
+        }
+        if x > y {
+            ans.push((x - y, y));
+        } else {
+            ans.push((x, y - x));
+        }
+    }
+
+    println!("{}", ans.len() - 1);
+    for &(x, y) in ans.iter().rev().skip(1) {
+        println!("{} {}", x, y);
+    }
 }
