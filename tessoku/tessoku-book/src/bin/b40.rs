15cba10a9afe4df3e743381c382b34e5b601ba3f
aliyome <aliyome@gmail.com>
2023-06-30 13:35:37 +0900

    tessoku b40

--- tessoku/tessoku-book/src/bin/b40.rs
+++ tessoku/tessoku-book/src/bin/b40.rs
@@ -3,7 +3,17 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: [usize; n]
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            if (a[i] + a[j]) % 100 == 0 {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
