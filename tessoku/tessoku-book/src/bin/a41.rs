16ea198a1182f0de03d1118647987a6f93d65cae
aliyome <aliyome@gmail.com>
2023-06-30 13:15:52 +0900

    tessoku a41

--- tessoku/tessoku-book/src/bin/a41.rs
+++ tessoku/tessoku-book/src/bin/a41.rs
@@ -1,9 +1,36 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
       n: usize,
+      s: Chars
     }
 
-    println!("{}", n);
+    let mut t = vec![' '; n];
+    let mut l = 0;
+    let mut r = n - 1;
+
+    while l < r {
+        for i in 0..3 {
+            t[l + i] = s[l];
+        }
+        l += 1;
+        // println!("{:?}", t);
+        if s == t {
+            println!("Yes");
+            return;
+        }
+
+        for i in 0..3 {
+            t[r - i] = s[r];
+        }
+        r -= 1;
+        // println!("{:?}", t);
+        if s == t {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
