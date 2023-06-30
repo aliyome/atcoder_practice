9444253d3b5cbb2abf0775dd7e53a795a661a00b
aliyome <aliyome@gmail.com>
2023-06-30 14:36:54 +0900

    tessoku a42 fix-range

--- tessoku/tessoku-book/src/bin/a42.rs
+++ tessoku/tessoku-book/src/bin/a42.rs
@@ -3,7 +3,21 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      k: isize,
+      mut ab: [(isize, isize); n]
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    for a_min in 0..=100 - k {
+        let a_max = a_min + k;
+        for b_min in 0..=100 - k {
+            let b_max = b_min + k;
+            let count = ab
+                .iter()
+                .filter(|(a, b)| a_min <= *a && *a <= a_max && b_min <= *b && *b <= b_max)
+                .count();
+            ans = ans.max(count);
+        }
+    }
+    println!("{}", ans);
 }
