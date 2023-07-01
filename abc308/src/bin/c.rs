4074e2eb171b20a25cb2c6bd9a17a3f11f8b7c33
aliyome <aliyome@gmail.com>
2023-07-01 21:11:45 +0900

    abc308 c

--- abc308/src/bin/c.rs
+++ abc308/src/bin/c.rs
@@ -3,7 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        ab: [(usize, usize); n]
     };
 
-    println!("{}", n);
+    let mut list = vec![];
+    for i in 0..n {
+        let rate = ab[i].0 as f64 / (ab[i].0 as f64 + ab[i].1 as f64);
+        list.push((rate, i + 1));
+    }
+
+    list.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap().then(a.1.cmp(&b.1)));
+
+    for i in 0..n {
+        print!("{} ", list[i].1);
+    }
 }
