5b0b90a9a4155c1b4b0bea76a9ec1c9b1825f83b
aliyome <aliyome@gmail.com>
2023-07-03 14:22:42 +0900

    tessoku b44

--- tessoku/tessoku-book/src/bin/b44.rs
+++ tessoku/tessoku-book/src/bin/b44.rs
@@ -3,7 +3,23 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: [[usize; n]; n],
+      q: usize,
+      queries: [(usize, usize, usize); q]
     }
 
-    println!("{}", n);
+    let mut rows = vec![0; n];
+    for i in 0..n {
+        rows[i] = i;
+    }
+
+    for &(t, x, y) in &queries {
+        if t == 1 {
+            let tmp = rows[x - 1];
+            rows[x - 1] = rows[y - 1];
+            rows[y - 1] = tmp;
+        } else {
+            println!("{}", a[rows[x - 1]][y - 1]);
+        }
+    }
 }
