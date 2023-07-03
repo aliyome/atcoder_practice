83c2ccecff97e2475db06b91a3ab1f04f82ab714
aliyome <aliyome@gmail.com>
2023-07-03 13:39:32 +0900

    tessoku a43

--- tessoku/tessoku-book/src/bin/a43.rs
+++ tessoku/tessoku-book/src/bin/a43.rs
@@ -3,7 +3,18 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      l: usize,
+      ab: [(usize, char); n]
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    for &(a, b) in &ab {
+        if b == 'E' {
+            ans = ans.max(l - a);
+        } else {
+            ans = ans.max(a);
+        }
+    }
+
+    println!("{}", ans);
 }
