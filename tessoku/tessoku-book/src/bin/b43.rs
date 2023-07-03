4498a51efeaa56c57b7b0677d8659c79976be5f6
aliyome <aliyome@gmail.com>
2023-07-03 13:46:29 +0900

    tessoku b43

--- tessoku/tessoku-book/src/bin/b43.rs
+++ tessoku/tessoku-book/src/bin/b43.rs
@@ -3,7 +3,19 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      m: usize,
+      mut a: [usize; m]
     }
 
-    println!("{}", n);
+    // 1-indexed
+    a.insert(0, 0);
+
+    let mut wrong = vec![0; n + 1];
+    for &a in &a {
+        wrong[a] += 1;
+    }
+
+    for i in 1..=n {
+        println!("{}", m - wrong[i]);
+    }
 }
