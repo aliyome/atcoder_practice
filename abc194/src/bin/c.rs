4b7c74a9285122c89c0de1785d3a90b231b4760e
aliyome <aliyome@gmail.com>
2023-07-04 21:41:39 +0900

    abc194 c

--- abc194/src/bin/c.rs
+++ abc194/src/bin/c.rs
@@ -3,24 +3,24 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut a: [isize; n]
+        mut a: [i64; n]
     };
 
     // sums[i][j] := i番目以降にjが出現する回数 (jはa[i]に200の下駄を履かせたもの)
-    let mut sums = vec![vec![0; 400]; n + 1];
+    let mut sums = vec![vec![0i64; 401]; n + 1];
     for i in 0..n {
         let idx = n - i - 1;
         sums[idx] = sums[idx + 1].clone();
         sums[idx][(a[idx] + 200) as usize] += 1;
     }
 
-    let mut ans = 0;
+    let mut ans = 0i64;
     for i in 0..n {
-        for j in 0..400 {
+        for j in 0..=400 {
             if sums[i][j] == 0 {
                 continue;
             }
-            ans += ((a[i] + 200) - j as isize).pow(2) * sums[i][j];
+            ans += ((a[i] + 200) - j as i64).pow(2) * sums[i][j];
         }
     }
 
