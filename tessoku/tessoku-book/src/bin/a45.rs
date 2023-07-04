7f489b322874118096d0452e7cf34ed681391e25
aliyome <aliyome@gmail.com>
2023-07-04 13:13:03 +0900

    tessoku a45

--- tessoku/tessoku-book/src/bin/a45.rs
+++ tessoku/tessoku-book/src/bin/a45.rs
@@ -1,9 +1,35 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
       n: usize,
+      c: char,
+      a: Chars
     }
 
-    println!("{}", n);
+    let mut score = 0;
+    for &a in &a {
+        if a == 'W' {
+            score += 0;
+        } else if a == 'B' {
+            score += 1;
+        } else {
+            score += 2;
+        }
+    }
+
+    let mut res = 'W';
+    if score % 3 == 0 {
+        res = 'W';
+    } else if score % 3 == 1 {
+        res = 'B';
+    } else {
+        res = 'R';
+    }
+
+    if c == res {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
