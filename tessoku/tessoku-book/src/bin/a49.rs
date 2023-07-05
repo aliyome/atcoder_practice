6ac5664feb8386e8d055ed22f750d7e5b4cff452
aliyome <aliyome@gmail.com>
2023-07-05 13:24:59 +0900

    tessoku a49 beam

--- tessoku/tessoku-book/src/bin/a49.rs
+++ tessoku/tessoku-book/src/bin/a49.rs
@@ -6,9 +6,6 @@ fn main() {
       pqr: [(usize, usize, usize); t]
     }
 
-    let mut x = vec![0isize; 21];
-    x[0] = 1;
-
     let calc_score = |x: &Vec<isize>| x.iter().skip(1).filter(|&x| *x == 0).count();
 
     let op_a = |x: &mut Vec<isize>, p: usize, q: usize, r: usize| {
@@ -22,24 +19,71 @@ fn main() {
         x[r] -= 1;
     };
 
-    let mut ans = vec![];
-    for &(p, q, r) in &pqr {
-        let mut xa = x.clone();
-        let mut xb = x.clone();
-        op_a(&mut xa, p, q, r);
-        op_b(&mut xb, p, q, r);
-        let a_score = calc_score(&xa);
-        let b_score = calc_score(&xb);
-        if a_score > b_score {
-            x = xa;
-            ans.push('A');
-        } else {
-            x = xb;
-            ans.push('B');
+    // ビームサーチ
+    #[derive(Debug, Clone)]
+    struct Beam {
+        score: usize,
+        x: Vec<isize>,
+        ans: Vec<char>,
+    }
+    let beam_width = 10000;
+    let mut beam = vec![
+        vec![
+            Beam {
+                x: vec![0isize; 21],
+                score: 0,
+                ans: vec![]
+            };
+            beam_width
+        ];
+        t + 1
+    ];
+    // x[0]は使わない (1-indexed)
+    for i in 0..=t {
+        for j in 0..beam_width {
+            beam[i][j].x[0] = 1;
         }
     }
 
-    for c in ans {
-        println!("{}", c);
+    for i in 0..t {
+        let (p, q, r) = pqr[i];
+        let mut candidates = vec![];
+        for j in 0..beam_width {
+            let b = &beam[i][j];
+            let mut xa = b.x.clone();
+            let mut xb = b.x.clone();
+            op_a(&mut xa, p, q, r);
+            op_b(&mut xb, p, q, r);
+            let a_score = calc_score(&xa);
+            let b_score = calc_score(&xb);
+            let mut ans_a = b.ans.clone();
+            let mut ans_b = b.ans.clone();
+            ans_a.push('A');
+            ans_b.push('B');
+            candidates.push(Beam {
+                x: xa,
+                score: a_score,
+                ans: ans_a,
+            });
+            candidates.push(Beam {
+                x: xb,
+                score: b_score,
+                ans: ans_b,
+            });
+        }
+        candidates.sort_by_key(|b| b.score);
+        for (j, b) in candidates.iter().rev().take(beam_width).enumerate() {
+            beam[i + 1][j] = b.clone();
+        }
+    }
+
+    let mut max_j = 0;
+    for j in 0..beam_width {
+        if beam[t][j].score > beam[t][max_j].score {
+            max_j = j;
+        }
+    }
+    for i in 0..t {
+        println!("{}", beam[t][max_j].ans[i]);
     }
 }
