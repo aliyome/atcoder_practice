a6fe0ef173f6db70482df3a521b1ab17d18ccc14
aliyome <aliyome@gmail.com>
2023-07-06 14:15:14 +0900

    tessoku b51 stack

--- tessoku/tessoku-book/src/bin/b51.rs
+++ tessoku/tessoku-book/src/bin/b51.rs
@@ -1,9 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-      n: usize,
+      s: Chars,
     }
 
-    println!("{}", n);
+    let mut stack = vec![];
+    for i in 0..s.len() {
+        if s[i] == '(' {
+            stack.push(i);
+        } else if s[i] == ')' {
+            if let Some(l) = stack.pop() {
+                println!("{} {}", l + 1, i + 1);
+            }
+        }
+    }
 }
