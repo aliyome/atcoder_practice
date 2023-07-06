62434c2237be0094f2a124b7b1387594efe52cf9
aliyome <aliyome@gmail.com>
2023-07-06 14:19:08 +0900

    tessoku a52 queue

--- tessoku/tessoku-book/src/bin/a52.rs
+++ tessoku/tessoku-book/src/bin/a52.rs
@@ -1,9 +1,33 @@
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      q: usize,
     }
 
-    println!("{}", n);
+    let mut queue = VecDeque::new();
+    for _ in 0..q {
+        input! {
+          t: usize
+        }
+        match t {
+            1 => {
+                input! {
+                  s: String
+                }
+                queue.push_back(s);
+            }
+            2 => {
+                if let Some(s) = queue.front() {
+                    println!("{}", s);
+                }
+            }
+            3 => {
+                queue.pop_front();
+            }
+            _ => unreachable!(),
+        }
+    }
 }
