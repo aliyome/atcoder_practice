83ddb5d8da98cc2b311bfcb4bfc63406af1a610a
aliyome <aliyome@gmail.com>
2023-07-06 14:33:14 +0900

    tessoku a53 heap

--- tessoku/tessoku-book/src/bin/a53.rs
+++ tessoku/tessoku-book/src/bin/a53.rs
@@ -1,9 +1,34 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      q: usize,
     }
 
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+
+    for _ in 0..q {
+        input! {
+          t: usize
+        }
+        match t {
+            1 => {
+                input! {
+                  x: isize
+                }
+                heap.push(Reverse(x));
+            }
+            2 => {
+                if let Some(x) = heap.peek() {
+                    println!("{}", x.0);
+                }
+            }
+            3 => {
+                heap.pop();
+            }
+            _ => unreachable!(),
+        }
+    }
 }
