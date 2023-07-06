fbe3c16a26dfffc2daa3f31ed0fedde2b8c1933d
aliyome <aliyome@gmail.com>
2023-07-06 14:29:39 +0900

    tessoku b52 queue

--- tessoku/tessoku-book/src/bin/b52.rs
+++ tessoku/tessoku-book/src/bin/b52.rs
@@ -1,9 +1,27 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-      n: usize,
+      n: isize,
+      x: isize,
+      mut a: Chars
     }
 
-    println!("{}", n);
+    let mut q = VecDeque::new();
+    q.push_back(x - 1);
+    a[x as usize - 1] = '@';
+    while let Some(c) = q.pop_front() {
+        if c - 1 >= 0 && a[c as usize - 1] == '.' {
+            a[c as usize - 1] = '@';
+            q.push_back(c - 1);
+        }
+        if c + 1 < n && a[c as usize + 1] == '.' {
+            a[c as usize + 1] = '@';
+            q.push_back(c + 1);
+        }
+    }
+
+    println!("{}", a.iter().collect::<String>());
 }
