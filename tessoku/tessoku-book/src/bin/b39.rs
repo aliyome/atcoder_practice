52dc61a39e9435a9d3c29d5642edff29e5c4d367
aliyome <aliyome@gmail.com>
2023-07-08 13:13:57 +0900

    tessoku b39 BinaryHeap

--- tessoku/tessoku-book/src/bin/b39.rs
+++ tessoku/tessoku-book/src/bin/b39.rs
@@ -1,9 +1,40 @@
+use std::collections::{BinaryHeap, VecDeque};
+
 use proconio::input;
 
 fn main() {
     input! {
       n: usize,
+      d: usize,
+      mut xy: [(usize, usize); n]
     }
 
-    println!("{}", n);
+    // 着手可能日付でAscソート
+    xy.sort_by(|a, b| a.0.cmp(&b.0).then(b.1.cmp(&a.1)));
+    let mut deq = VecDeque::new();
+    for (x, y) in xy {
+        deq.push_back((x, y));
+    }
+
+    // 優先度付きキュー
+    let mut heap = BinaryHeap::new();
+    let mut ans = 0;
+    for day in 1..=d {
+        // 着手可能日付の仕事を優先度付きキューに追加
+        while let Some((x, y)) = deq.pop_front() {
+            if x <= day {
+                heap.push(y);
+            } else {
+                deq.push_front((x, y));
+                break;
+            }
+        }
+
+        // 仕事を優先度付きキューから取り出す
+        if let Some(y) = heap.pop() {
+            ans += y;
+        }
+    }
+
+    println!("{}", ans);
 }
