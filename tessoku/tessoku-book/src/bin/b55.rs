de705d42483c498b3284abec3ffd65e09d38a9d2
aliyome <aliyome@gmail.com>
2023-07-08 13:42:51 +0900

    tessoku b55 BTreeSet, next, next_back

--- tessoku/tessoku-book/src/bin/b55.rs
+++ tessoku/tessoku-book/src/bin/b55.rs
@@ -1,9 +1,27 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      q: usize,
+      tx: [(usize, isize); q]
     }
 
-    println!("{}", n);
+    let mut set = BTreeSet::new();
+    for i in 0..q {
+        let (t, x) = tx[i];
+        if t == 1 {
+            set.insert(x);
+        } else {
+            let inf = 10isize.pow(12);
+            let c1 = *set.range(x..).next().unwrap_or(&inf);
+            let c2 = *set.range(..x).next_back().unwrap_or(&inf);
+            if c1 == inf && c2 == inf {
+                println!("-1");
+            } else {
+                println!("{}", (c1 - x).abs().min((c2 - x).abs()));
+            }
+        }
+    }
 }
