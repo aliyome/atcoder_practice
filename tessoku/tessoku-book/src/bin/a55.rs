9797c5d853592b88d57b3eaa8d5fb3ccbf5f5c19
aliyome <aliyome@gmail.com>
2023-07-08 13:29:59 +0900

    tessoku a55 BTreeSet::range

--- tessoku/tessoku-book/src/bin/a55.rs
+++ tessoku/tessoku-book/src/bin/a55.rs
@@ -1,9 +1,42 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      q: usize,
     }
 
-    println!("{}", n);
+    let mut set = BTreeSet::new();
+
+    for _ in 0..q {
+        input! {
+          t: usize
+        }
+        match t {
+            1 => {
+                input! {
+                  x: usize
+                }
+                set.insert(x);
+            }
+            2 => {
+                input! {
+                  x: usize
+                }
+                set.remove(&x);
+            }
+            3 => {
+                input! {
+                  x: usize
+                }
+                if let Some(&y) = set.range(x..).next() {
+                    println!("{}", y);
+                } else {
+                    println!("-1");
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
 }
