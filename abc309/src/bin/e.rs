c91458328ad56adad67ee1e31d17fe623c102895
aliyome <aliyome@gmail.com>
2023-07-09 14:17:51 +0900

    abc309 e DP

--- abc309/src/bin/e.rs
+++ abc309/src/bin/e.rs
@@ -5,7 +5,7 @@ fn main() {
         n: usize, // <= 3 x 10^5
         m: usize, // <= 3 x 10^5
         mut p: [usize; n-1],
-        xy: [(usize, usize); m]
+        mut xy: [(usize, isize); m]
     };
 
     // p[i] := iの親
@@ -18,24 +18,41 @@ fn main() {
         c[p[i]].push(i);
     }
 
-    // O(MN) -> TLE
-    // x := 加入者
-    // y := 補償対象の世代数
-    let mut ans = vec![false; n + 1];
-    for &(x, y) in &xy {
-        ans[x] = true;
+    // // O(MN) -> TLE
+    // // x := 加入者
+    // // y := 補償対象の世代数
+    // let mut ans = vec![false; n + 1];
+    // for &(x, y) in &xy {
+    //     ans[x] = true;
 
-        let mut stack = vec![(x, y)];
-        while let Some((x, y)) = stack.pop() {
-            ans[x] = true;
-            for &i in &c[x] {
-                if y == 0 {
-                    break;
-                }
-                stack.push((i, y - 1));
-            }
+    //     let mut stack = vec![(x, y)];
+    //     while let Some((x, y)) = stack.pop() {
+    //         ans[x] = true;
+    //         for &i in &c[x] {
+    //             if y == 0 {
+    //                 break;
+    //             }
+    //             stack.push((i, y - 1));
+    //         }
+    //     }
+    // }
+
+    // println!("{}", ans.iter().filter(|&&x| x).count());
+
+    // dp[v] := 人vを含む補償対象数の世代数の最大値
+    let mut dp = vec![-1isize; n + 1];
+    // 初期値
+    for &(x, y) in &xy {
+        dp[x] = dp[x].max(y);
+    }
+    // 全ての人について
+    for v in 1..=n {
+        let y = dp[v];
+        for &u in &c[v] {
+            // 親の補償対象数の世代数の最大値を子に引き継ぐ
+            dp[u] = dp[u].max(y - 1);
         }
     }
 
-    println!("{}", ans.iter().filter(|&&x| x).count());
+    println!("{}", dp.iter().filter(|&&x| x >= 0).count());
 }
