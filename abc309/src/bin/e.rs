b55d9c757ec2c8317f389641bad1483ab3c611db
aliyome <aliyome@gmail.com>
2023-07-09 13:06:07 +0900

    309 tle

--- abc309/src/bin/e.rs
+++ abc309/src/bin/e.rs
@@ -2,7 +2,40 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        n: usize, // <= 3 x 10^5
+        m: usize, // <= 3 x 10^5
+        mut p: [usize; n-1],
+        xy: [(usize, usize); m]
     };
 
+    // p[i] := iの親
+    p.insert(0, 0);
+    p.insert(0, 0);
+
+    // c[i] := iの子リスト
+    let mut c = vec![vec![]; n + 1];
+    for i in 1..=n {
+        c[p[i]].push(i);
+    }
+
+    // O(MN) -> TLE
+    // x := 加入者
+    // y := 補償対象の世代数
+    let mut ans = vec![false; n + 1];
+    for &(x, y) in &xy {
+        ans[x] = true;
+
+        let mut stack = vec![(x, y)];
+        while let Some((x, y)) = stack.pop() {
+            ans[x] = true;
+            for &i in &c[x] {
+                if y == 0 {
+                    break;
+                }
+                stack.push((i, y - 1));
+            }
+        }
+    }
+
+    println!("{}", ans.iter().filter(|&&x| x).count());
 }
