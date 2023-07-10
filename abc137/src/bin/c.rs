cf91a9246f85e959fd0647655ab70354f6264d12
aliyome <aliyome@gmail.com>
2023-07-10 22:43:09 +0900

    abc137 c

--- abc137/src/bin/c.rs
+++ abc137/src/bin/c.rs
@@ -1,9 +1,26 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
+        s: [Chars; n]
     };
 
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 0..n {
+        let mut x = s[i].clone();
+        x.sort();
+        *map.entry(x).or_insert(0usize) += 1;
+    }
+
+    let mut ans = 0;
+    for (_, v) in map {
+        if v > 1 {
+            ans += v * (v - 1) / 2;
+        }
+    }
+
+    println!("{}", ans);
 }
