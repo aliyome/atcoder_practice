cd1f25d9fff6b20ee8ba5ebebd61cb60143ec830
aliyome <aliyome@gmail.com>
2023-07-11 21:13:25 +0900

    abc079 c

--- abc079/src/bin/c.rs
+++ abc079/src/bin/c.rs
@@ -1,9 +1,38 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        abcd: Chars,
     };
 
-    println!("{}", n);
+    let abcd = abcd
+        .iter()
+        .map(|&x| x.to_digit(10).unwrap() as isize)
+        .collect_vec();
+    let calc = |x, y, op| {
+        if op == '+' {
+            x + y
+        } else {
+            x - y
+        }
+    };
+    for op1 in ['+', '-'] {
+        for op2 in ['+', '-'] {
+            for op3 in ['+', '-'] {
+                if calc(
+                    calc(calc(abcd[0], abcd[1], op1), abcd[2], op2),
+                    abcd[3],
+                    op3,
+                ) == 7
+                {
+                    println!(
+                        "{}{}{}{}{}{}{}=7",
+                        abcd[0], op1, abcd[1], op2, abcd[2], op3, abcd[3]
+                    );
+                    return;
+                }
+            }
+        }
+    }
 }
