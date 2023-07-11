f71bb5914047b59308475d1bada83444a0e71052
aliyome <aliyome@gmail.com>
2023-07-11 21:05:05 +0900

    abc191 a

--- abc191/src/bin/a.rs
+++ abc191/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        v: usize,
+        t: usize,
+        s: usize,
+        d: usize
     };
 
+    if v * t <= d && d <= v * s {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }
