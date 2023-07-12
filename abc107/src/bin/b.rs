0d53c2ff68a7c803a19b29e6b54f8f4ac6c3eb29
aliyome <aliyome@gmail.com>
2023-07-12 21:35:14 +0900

    abc107 b

--- abc107/src/bin/b.rs
+++ abc107/src/bin/b.rs
@@ -1,9 +1,48 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        h: usize,
+        w: usize,
+        a: [Chars; h]
     };
 
-    println!("{}", n);
+    let mut skip_i = vec![];
+    let mut skip_j = vec![];
+    for i in 0..h {
+        let mut skip = true;
+        for j in 0..w {
+            if a[i][j] == '#' {
+                skip = false;
+                break;
+            }
+        }
+        if skip {
+            skip_i.push(i);
+        }
+    }
+    for j in 0..w {
+        let mut skip = true;
+        for i in 0..h {
+            if a[i][j] == '#' {
+                skip = false;
+                break;
+            }
+        }
+        if skip {
+            skip_j.push(j);
+        }
+    }
+    for i in 0..h {
+        if skip_i.contains(&i) {
+            continue;
+        }
+        for j in 0..w {
+            if skip_j.contains(&j) {
+                continue;
+            }
+            print!("{}", a[i][j]);
+        }
+        println!();
+    }
 }
