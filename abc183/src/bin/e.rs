d0839653caedcac1b8ee6bb412fa3b98aebebfdd
aliyome <aliyome@gmail.com>
2023-07-13 19:09:57 +0900

    abc183 解答ac

--- abc183/src/bin/e.rs
+++ abc183/src/bin/e.rs
@@ -14,35 +14,33 @@ fn main() {
     let mut dp = vec![vec![0; w + 1]; h + 1];
     dp[0][0] = 1;
 
+    let mut x = vec![vec![0; w + 1]; h + 1];
+    let mut y = vec![vec![0; w + 1]; h + 1];
+    let mut z = vec![vec![0; w + 1]; h + 1];
     for i in 0..h {
         for j in 0..w {
+            if i == 0 && j == 0 {
+                continue;
+            }
+            if s[i][j] == '#' {
+                continue;
+            }
             // 右
-            for k in 1..=2000 {
-                if j + k < w && s[i][j + k] == '.' {
-                    dp[i][j + k] += dp[i][j];
-                    dp[i][j + k] %= MOD;
-                } else {
-                    break;
-                }
+            if 0 < j {
+                x[i][j] = x[i][j - 1] + dp[i][j - 1];
+                x[i][j] %= MOD;
             }
             // 下
-            for k in 1..=2000 {
-                if i + k < h && s[i + k][j] == '.' {
-                    dp[i + k][j] += dp[i][j];
-                    dp[i + k][j] %= MOD;
-                } else {
-                    break;
-                }
+            if 0 < i {
+                y[i][j] = y[i - 1][j] + dp[i - 1][j];
+                y[i][j] %= MOD;
             }
             // 右下
-            for k in 1..=2000 {
-                if i + k < h && j + k < w && s[i + k][j + k] == '.' {
-                    dp[i + k][j + k] += dp[i][j];
-                    dp[i + k][j + k] %= MOD;
-                } else {
-                    break;
-                }
+            if 0 < i && 0 < j {
+                z[i][j] = z[i - 1][j - 1] + dp[i - 1][j - 1];
+                z[i][j] %= MOD;
             }
+            dp[i][j] += x[i][j] + y[i][j] + z[i][j];
         }
     }
 
