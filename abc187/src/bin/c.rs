cf15ffb6b88c3e820ee6136680fa40612bce6580
aliyome <aliyome@gmail.com>
2023-07-14 21:13:20 +0900

    abc187 c RE

--- abc187/src/bin/c.rs
+++ abc187/src/bin/c.rs
@@ -5,29 +5,26 @@ use proconio::{input, marker::Chars};
 fn main() {
     input! {
         n: usize,
+        s: [Chars; n]
     }
 
-    let mut hash_without_1 = HashSet::new();
-    let mut hash_with_1 = HashSet::new();
-
-    for _ in 0..n {
-        input! {
-            s: Chars,
-        };
-        if s[0] != '!' {
-            let s = s.iter().collect::<String>();
-            hash_without_1.insert(s.clone());
-            if hash_with_1.contains(&s) {
-                println!("{}", s);
+    let mut set_raw = HashSet::new();
+    let mut set_ex = HashSet::new();
+    for s in s.iter() {
+        if s[0] == '!' {
+            let x = s[1..].iter().collect::<String>();
+            if set_raw.contains(&x) {
+                println!("{}", x);
                 return;
             }
+            set_ex.insert(x);
         } else {
-            let s = s.iter().skip(1).collect::<String>();
-            hash_with_1.insert(s.clone());
-            if hash_without_1.contains(&s) {
-                println!("{}", s);
+            let x = s[0..].iter().collect::<String>();
+            if set_ex.contains(&x) {
+                println!("{}", x);
                 return;
             }
+            set_raw.insert(x);
         }
     }
 
