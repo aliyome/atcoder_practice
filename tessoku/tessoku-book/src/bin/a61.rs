8807d56d8aacd7861ca02560b91098ca38400a3c
aliyome <aliyome@gmail.com>
2023-07-14 13:21:22 +0900

    tessoku a61 adjacent

--- tessoku/tessoku-book/src/bin/a61.rs
+++ tessoku/tessoku-book/src/bin/a61.rs
@@ -3,7 +3,26 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      m: usize,
+      ab: [(usize, usize); m]
     }
 
-    println!("{}", n);
+    let mut graph = vec![vec![]; n + 1];
+    for &(a, b) in &ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    for i in 1..=n {
+        graph[i].sort();
+        println!(
+            "{}: {{{}}}",
+            i,
+            graph[i]
+                .iter()
+                .map(|&x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
+    }
 }
