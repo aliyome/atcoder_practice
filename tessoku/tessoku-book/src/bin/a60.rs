236c4f2ec90253fec1d306b7e46b2a59c47927ce
aliyome <aliyome@gmail.com>
2023-07-14 12:35:11 +0900

    tessoku a60 tle

--- tessoku/tessoku-book/src/bin/a60.rs
+++ tessoku/tessoku-book/src/bin/a60.rs
@@ -3,7 +3,20 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      mut a: [usize; n]
     }
 
-    println!("{}", n);
+    a.insert(0, 0);
+
+    // O(N^2) TLE
+    for i in 1..=n {
+        let mut d = -1isize;
+        for j in (0..i).rev() {
+            if a[i] < a[j] {
+                d = j as isize;
+                break;
+            }
+        }
+        print!("{} ", d);
+    }
 }
