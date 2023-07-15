0125e9ce9df0b8b63b7d2b985781760366aa442e
aliyome <aliyome@gmail.com>
2023-07-15 22:31:07 +0900

    abc310 e tle

--- abc310/src/bin/d.rs
+++ abc310/src/bin/d.rs
@@ -3,6 +3,47 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        t: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    }
 
+    // 相性の悪い組み合わせをビットマスクで表す
+    let mut bad = vec![];
+    for &(a, b) in &ab {
+        let mut mask = 0;
+        mask |= 1 << (a - 1);
+        mask |= 1 << (b - 1);
+        bad.push(mask);
+    }
+
+    // 全組み合わせをビットマスクで表す
+    let mut team = vec![];
+    for i in 1..=1 << n {
+        let mut mask = 0;
+        for j in 0..n {
+            if (i >> j) & 1 == 1 {
+                mask |= 1 << j;
+            }
+        }
+        team.push(mask);
+    }
+
+    for i in 0..t {
+        for j in 0..(1 << n) {
+            if dp[i][j] > 0 {
+                for k in 0..n {
+                    if (j >> k) & 1 == 0 && (j & bad[k]) == 0 {
+                        dp[i + 1][j | (1 << k)] += dp[i][j];
+                    }
+                }
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for &x in &dp[t] {
+        ans += x;
+    }
+    println!("{}", ans);
 }
--- abc310/src/bin/e.rs
+++ abc310/src/bin/e.rs
@@ -1,8 +1,31 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
         n: usize,
-    };
+        mut s: Chars,
+    }
 
+    let s: Vec<_> = s
+        .into_iter()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect();
+
+    let nand = |a: usize, b: usize| !(a & b) & 1;
+
+    let mut ans = 0;
+    for i in 0..n {
+        let mut tmp = std::usize::MAX;
+        for j in i..n {
+            if i == j {
+                tmp = s[i];
+            } else {
+                tmp = nand(tmp, s[j]);
+            }
+            ans += tmp;
+        }
+    }
+
+    println!("{}", ans);
 }
