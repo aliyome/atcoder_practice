d030a44ba92aa9cb746ca0b77eb0ea3a665cee52
aliyome <aliyome@gmail.com>
2023-07-15 21:31:47 +0900

    abc310 c

--- abc310/src/bin/c.rs
+++ abc310/src/bin/c.rs
@@ -1,8 +1,19 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
         n: usize,
-    };
+        s: [String; n],
+    }
 
+    let mut set = HashSet::new();
+    for s in s {
+        let rev = s.chars().rev().collect::<String>();
+        if !set.contains(&s) && !set.contains(&rev) {
+            set.insert(s);
+        }
+    }
+
+    println!("{}", set.len());
 }
