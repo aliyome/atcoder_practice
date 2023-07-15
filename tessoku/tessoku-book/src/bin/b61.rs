8335f73a24faf496d5b8f17e03a82c4a7c46d7ca
aliyome <aliyome@gmail.com>
2023-07-15 14:15:13 +0900

    tessoku b61

--- tessoku/tessoku-book/src/bin/b61.rs
+++ tessoku/tessoku-book/src/bin/b61.rs
@@ -3,7 +3,24 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      m: usize,
+      ab: [(usize, usize); m]
     }
 
-    println!("{}", n);
+    let mut graph = vec![vec![]; n + 1];
+    for &(a, b) in &ab {
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let mut max_i = 0;
+    let mut max = 0;
+    for i in 1..=n {
+        if graph[i].len() > max {
+            max = graph[i].len();
+            max_i = i;
+        }
+    }
+
+    println!("{}", max_i);
 }
