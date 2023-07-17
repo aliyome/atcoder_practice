1a2bf084b973ce8fc7623dd3fd3ddff70b60214a
aliyome <aliyome@gmail.com>
2023-07-17 21:20:17 +0900

    abc185 f SegTree

--- abc185/src/bin/f.rs
+++ abc185/src/bin/f.rs
@@ -1,48 +1,5 @@
 use proconio::input;
 
-struct SegTree {
-    size: usize,
-    nodes: Vec<usize>,
-}
-
-impl SegTree {
-    fn new(n: usize, a: Vec<usize>) -> Self {
-        let size = n.next_power_of_two();
-        let mut nodes = vec![0; 2 * size - 1];
-        nodes[size - 1..size - 1 + n].clone_from_slice(&a);
-        for i in (0..size - 1).rev() {
-            nodes[i] = nodes[2 * i + 1] ^ nodes[2 * i + 2];
-        }
-        Self { size, nodes }
-    }
-
-    fn update(&mut self, i: usize, x: usize) {
-        let mut i = i + self.size - 1;
-        self.nodes[i] = x;
-        while i > 0 {
-            i = (i - 1) / 2;
-            self.nodes[i] = self.nodes[2 * i + 1] ^ self.nodes[2 * i + 2];
-        }
-    }
-
-    fn query(&self, a: usize, b: usize) -> usize {
-        self.execute_query(a, b, 0, 0, self.size)
-    }
-
-    fn execute_query(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> usize {
-        if r <= a || b <= l {
-            return 0;
-        }
-        if a <= l && r <= b {
-            return self.nodes[k];
-        }
-        let mid = (l + r) / 2;
-        let vl = self.execute_query(a, b, 2 * k + 1, l, mid);
-        let vr = self.execute_query(a, b, 2 * k + 2, mid, r);
-        vl ^ vr
-    }
-}
-
 fn main() {
     input! {
         n: usize,
@@ -51,14 +8,98 @@ fn main() {
         txy: [(usize, usize, usize); q],
     };
 
-    let mut seg_tree = SegTree::new(n, a);
+    let mut tree = SegmentTree::new(n + 1, |x, y| x ^ y);
+    for i in 0..n {
+        tree.update(i + 1, a[i]);
+    }
 
-    for (t, x, y) in txy {
-        if t == 1 {
-            let val = seg_tree.query(x - 1, x) ^ y;
-            seg_tree.update(x - 1, val);
-        } else {
-            println!("{}", seg_tree.query(x - 1, y));
+    let replace = |x: usize, y: usize, tree: &mut SegmentTree| {
+        let before = tree.get(x);
+        let next = before ^ y;
+        tree.update(x, next);
+    };
+
+    let display = |x: usize, y: usize, tree: &mut SegmentTree| {
+        let ans = tree.query(x, y + 1);
+        println!("{}", ans);
+    };
+
+    for &(t, x, y) in &txy {
+        match t {
+            1 => {
+                replace(x, y, &mut tree);
+            }
+            2 => {
+                display(x, y, &mut tree);
+            }
+            _ => unreachable!(),
         }
+    }
+}
+
+struct SegmentTree {
+    size: usize,
+    node: Vec<usize>,
+    monoid: fn(usize, usize) -> usize,
+}
+
+impl SegmentTree {
+    fn new(n: usize, monoid: fn(usize, usize) -> usize) -> Self {
+        // ノードの数は 2 の指数倍に正規化する
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+
+        Self {
+            size,
+            // ルートが 1 の完全二分木なので、ノード数は 2n - 1
+            // node[0] は使わない
+            node: vec![0; size * 2],
+            monoid,
+        }
+    }
+
+    // pos 番目の値を x に更新する
+    fn update(&mut self, pos: usize, x: usize) {
+        // 葉のノードの位置
+        let mut pos = pos + self.size - 1;
+        self.node[pos] = x;
+        // 親に上って更新していく
+        while pos > 0 {
+            pos /= 2;
+            self.node[pos] = (self.monoid)(self.node[pos * 2], self.node[pos * 2 + 1]);
+        }
+    }
+
+    // [l, r) の区間に対するクエリを実行する
+    fn query(&self, l: usize, r: usize) -> usize {
+        self._query(l, r, 1, self.size + 1, 1)
+    }
+
+    // [l, r) の区間に対するクエリを実行する
+    // u は現在見ているノードの位置
+    // [a, b) は u の担当区間
+    // u が [l, r) に完全に含まれるまで分解していく
+    fn _query(&self, l: usize, r: usize, a: usize, b: usize, u: usize) -> usize {
+        // ありえない区間なら 0 を返す
+        // MEMO: 0 以外を返したい場合は実装を修正する。例えば、区間の最小値を求める場合は usize::MAX を返す。
+        if r <= a || b <= l {
+            return 0;
+        }
+        // u の担当区間がクエリ区間に完全に含まれるなら、u の値を返す
+        if l <= a && b <= r {
+            return self.node[u];
+        }
+        // そうでないなら、左右の子に対して再帰的にクエリを実行する
+        let m = (a + b) / 2;
+        let vl = self._query(l, r, a, m, u * 2);
+        let vr = self._query(l, r, m, b, u * 2 + 1);
+        // 左右の子の値をマージして返す
+        (self.monoid)(vl, vr)
+    }
+
+    fn get(&self, pos: usize) -> usize {
+        self.node[pos + self.size - 1]
     }
 }
