ec43c64fdd37af386e958d2da46fa32bcf4bfc0e
aliyome <aliyome@gmail.com>
2023-07-17 21:09:22 +0900

    abc070 c

--- abc070/src/bin/c.rs
+++ abc070/src/bin/c.rs
@@ -3,6 +3,25 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        t: [u128; n]
     };
 
+    let mut ans = t[0];
+    for i in 1..n {
+        ans = lcm(ans, t[i]);
+    }
+
+    println!("{}", ans);
+}
+
+fn gcd(a: u128, b: u128) -> u128 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u128, b: u128) -> u128 {
+    a * b / gcd(a, b)
 }
