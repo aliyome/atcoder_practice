0034984a6570755d28a20c4e0577122e1061e3d3
aliyome <aliyome@gmail.com>
2023-07-18 14:36:38 +0900

    tessoku a66 uf

--- tessoku/tessoku-book/src/bin/a66.rs
+++ tessoku/tessoku-book/src/bin/a66.rs
@@ -3,7 +3,72 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      q: usize,
+      tuv: [(usize, usize, usize); q]
     }
 
-    println!("{}", n);
+    let mut uf = UnionFind::new(n + 1);
+    for &(t, u, v) in &tuv {
+        if t == 1 {
+            uf.unite(u, v);
+        } else {
+            if uf.same(u, v) {
+                println!("Yes");
+            } else {
+                println!("No");
+            }
+        }
+    }
+}
+
+pub struct UnionFind {
+    parent: Vec<usize>,
+    rank: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl UnionFind {
+    // O(N)
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: (0..n).collect(),
+            rank: vec![0; n],
+            size: vec![1; n],
+        }
+    }
+
+    // O(α(N)) ≒ O(1)
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] == x {
+            x
+        } else {
+            let p = self.find(self.parent[x]);
+            self.parent[x] = p;
+            p
+        }
+    }
+
+    // O(α(N)) ≒ O(1)
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.find(x);
+        let y = self.find(y);
+        if x == y {
+            return;
+        }
+        if self.rank[x] < self.rank[y] {
+            self.parent[x] = y;
+            self.size[y] += self.size[x];
+        } else {
+            self.parent[y] = x;
+            self.size[x] += self.size[y];
+            if self.rank[x] == self.rank[y] {
+                self.rank[x] += 1;
+            }
+        }
+    }
+
+    // O(α(N)) ≒ O(1)
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
 }
