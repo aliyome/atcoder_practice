47fe27841ebbfc5140e796304c34b57fab467939
aliyome <aliyome@gmail.com>
2023-07-21 21:29:51 +0900

    abc179 c

--- abc179/src/bin/c.rs
+++ abc179/src/bin/c.rs
@@ -5,5 +5,18 @@ fn main() {
         n: usize,
     };
 
-    println!("{}", n);
+    let mut ans = vec![];
+    for a in 1..=n {
+        for b in 1..=n {
+            if a * b > n {
+                break;
+            }
+            let c = n as isize - a as isize * b as isize;
+            if c > 0 {
+                ans.push((a, b, c as usize));
+            }
+        }
+    }
+
+    println!("{}", ans.len());
 }
