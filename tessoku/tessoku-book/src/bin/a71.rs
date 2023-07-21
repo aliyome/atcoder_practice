45344a8919b29282ae96c70d1217d804e846043e
aliyome <aliyome@gmail.com>
2023-07-21 14:51:31 +0900

    tessoku a71

--- tessoku/tessoku-book/src/bin/a71.rs
+++ tessoku/tessoku-book/src/bin/a71.rs
@@ -3,7 +3,18 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      mut a: [usize; n],
+      mut b: [usize; n]
     }
 
-    println!("{}", n);
+    a.sort();
+    b.sort();
+    b.reverse();
+
+    let mut cost = 0;
+    for i in 0..n {
+        cost += a[i] * b[i];
+    }
+
+    println!("{}", cost);
 }
