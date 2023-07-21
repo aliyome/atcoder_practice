7fcab8cbab28e06360fb13ee5d73a69ce0c6947d
aliyome <aliyome@gmail.com>
2023-07-21 21:53:29 +0900

    abc309 d re

--- abc309/src/bin/d.rs
+++ abc309/src/bin/d.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 use proconio::input;
 
@@ -10,49 +10,53 @@ fn main() {
         ab: [(usize, usize); m]
     };
 
-    // 頂点1を含むグラフg1と、頂点N1+N2を含むグラフg2を作る
-    let mut g1 = vec![vec![]; n1 + 1];
-    let mut g2 = vec![vec![]; n1 + n2 + 1];
+    let mut graph = vec![vec![]; n1 + n2 + 1];
     for &(a, b) in &ab {
-        if a <= n1 {
-            g1[a].push(b);
-            g1[b].push(a);
-        } else {
-            g2[a].push(b);
-            g2[b].push(a);
-        }
+        graph[a].push(b);
+        graph[b].push(a);
     }
 
-    // 頂点1から各頂点までの距離をBFSで求める
-    let mut dist1 = vec![None; n1 + 1];
-    let mut q = VecDeque::new();
-    let mut max = 0;
-    q.push_back((1, 0));
-    while let Some((v, d)) = q.pop_front() {
-        if dist1[v].is_some() {
+    let mut dist = vec![std::usize::MAX; n1 + n2 + 1];
+    let mut heap = BinaryHeap::new();
+    dist[1] = 0;
+    heap.push((Reverse(0), 1));
+    while let Some((Reverse(d), v)) = heap.pop() {
+        if dist[v] < d {
             continue;
         }
-        dist1[v] = Some(d);
-        max = max.max(d);
-        for &u in &g1[v] {
-            q.push_back((u, d + 1));
+        for &u in &graph[v] {
+            if dist[u] > d + 1 {
+                dist[u] = d + 1;
+                heap.push((Reverse(d + 1), u));
+            }
         }
     }
+    let max1 = dist
+        .into_iter()
+        .filter(|&x| x != std::usize::MAX)
+        .max()
+        .unwrap();
 
-    // 頂点N1+N2から各頂点までの距離をBFSで求める
-    let mut dist2 = vec![None; n1 + n2 + 1];
-    let mut max2 = 0;
-    q.push_back((n1 + n2, 0));
-    while let Some((v, d)) = q.pop_front() {
-        if dist2[v].is_some() {
+    let mut dist = vec![std::usize::MAX; n1 + n2 + 1];
+    let mut heap = BinaryHeap::new();
+    dist[n1 + n2] = 0;
+    heap.push((Reverse(0), n1 + n2));
+    while let Some((Reverse(d), v)) = heap.pop() {
+        if dist[v] < d {
             continue;
         }
-        dist2[v] = Some(d);
-        for &u in &g2[v] {
-            max2 = max2.max(d);
-            q.push_back((u, d + 1));
+        for &u in &graph[v] {
+            if dist[u] > d + 1 {
+                dist[u] = d + 1;
+                heap.push((Reverse(d + 1), u));
+            }
         }
     }
+    let max2 = dist
+        .into_iter()
+        .filter(|&x| x != std::usize::MAX)
+        .max()
+        .unwrap();
 
-    println!("{}", max + max2 + 1);
+    println!("{}", max1 + 1 + max2);
 }
