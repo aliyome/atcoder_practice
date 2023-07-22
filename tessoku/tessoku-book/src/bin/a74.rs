3879594f6ac62d8d4aefa5719004e7f806af779b
aliyome <aliyome@gmail.com>
2023-07-22 14:33:52 +0900

    tessoku a74 転倒数

--- tessoku/tessoku-book/src/bin/a74.rs
+++ tessoku/tessoku-book/src/bin/a74.rs
@@ -3,7 +3,34 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      p: [[usize; n]; n]
     }
 
-    println!("{}", n);
+    let mut row = vec![0; n];
+    let mut col = vec![0; n];
+    for i in 0..n {
+        for j in 0..n {
+            let d = p[i][j];
+            if d != 0 {
+                row[d - 1] = i;
+                col[d - 1] = j
+            }
+        }
+    }
+
+    let mut inversion_count = 0;
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            if row[i] > row[j] {
+                inversion_count += 1;
+            }
+        }
+        for j in i + 1..n {
+            if col[i] > col[j] {
+                inversion_count += 1;
+            }
+        }
+    }
+
+    println!("{}", inversion_count);
 }
