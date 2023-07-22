1c1660b629827d35e4dfd5c6812d43efc82df6f7
aliyome <aliyome@gmail.com>
2023-07-22 21:06:40 +0900

    abc311 b

--- abc311/src/bin/b.rs
+++ abc311/src/bin/b.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
+        d: usize,
+        s: [Chars; n]
     };
 
+    let mut ans = 0;
+    let mut now = 0;
+    for day in 0..d {
+        let mut ok = true;
+        for i in 0..n {
+            if s[i][day] == 'x' {
+                ok = false;
+                break;
+            }
+        }
+        if ok == true {
+            now += 1;
+        } else {
+            now = 0;
+        }
+        ans = ans.max(now);
+    }
+    println!("{}", ans);
 }
