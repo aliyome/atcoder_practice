6e6176b97ce9fbb3d659fe4b00928a61b8b0e28b
aliyome <aliyome@gmail.com>
2023-07-25 21:32:21 +0900

    abc309 c

--- abc309/src/bin/c.rs
+++ abc309/src/bin/c.rs
@@ -5,34 +5,32 @@ use proconio::input;
 fn main() {
     input! {
         n: usize, // <= 3 x 10^5
-        k: i64, // <= 10^9
-        mut ab: [(i64, i64); n] // a,b, <= 10^9
+        k: usize, // <= 10^9
+        mut ab: [(usize, usize); n] // a,b, <= 10^9
     };
 
-    // 全量
-    let mut sum = ab.iter().fold(0, |acc, (_, b)| acc + b);
+    // 同じ日はまとめる
+    let mut abmap = BTreeMap::new();
+    for (a, b) in ab {
+        let entry = abmap.entry(a).or_insert(0);
+        *entry += b;
+    }
 
+    // 初日の数
+    let mut sum = abmap.iter().map(|(_, b)| *b).sum::<usize>();
+
+    // 初日の数がkを超えていたら、初日の数をkにする
     if sum <= k {
         println!("1");
         return;
     }
 
-    // 日毎にまとめる
-    let mut map = BTreeMap::new();
-    for &(a, b) in &ab {
-        *map.entry(a).or_insert(0) += b;
-    }
-
-    // K以下になる日を探す
-    for (a, b) in map {
-        sum -= b;
+    // 減らしていく
+    for (a, b) in abmap.iter() {
+        sum -= *b;
         if sum <= k {
-            println!("{}", a + 1);
+            println!("{}", *a + 1);
             return;
         }
     }
-
-    // ここには来ないはず
-    ab.sort_by(|a, b| a.0.cmp(&b.0));
-    println!("{}", ab[n - 1].0 + 1);
 }
--- tessoku/tessoku-book/src/bin/a76.rs
+++ tessoku/tessoku-book/src/bin/a76.rs
@@ -15,24 +15,49 @@ fn main() {
     x.insert(0, 0);
     x.push(w);
 
-    // dp[i] := i番目の足場に来る方法の数
-    let mut dp = vec![0usize; n + 2];
+    // // dp[i] := i番目の足場に来る方法の数
+    // let mut dp = vec![0usize; n + 2];
+    // dp[0] = 1;
+
+    // // 素朴にDPを実装した場合
+    // // O(N^2) -> TLE
+
+    // // O(NlogN)になるように、二分探索を使う
+    // // 配るDP
+    // for i in 0..=n {
+    //     let xl = x.lower_bound(x[i] + l);
+    //     let xr = x.upper_bound(x[i] + r);
+    //     // この部分のループは単純に足しているだけなので高速化できそう -> 累積和
+    //     for j in xl..xr {
+    //         dp[j] += dp[i];
+    //         dp[j] %= MOD;
+    //     }
+    // }
+
+    // 貰うDP, 累積和
+    let l = l as isize;
+    let r = r as isize;
+    let x = x.into_iter().map(|x| x as isize).collect::<Vec<_>>();
+    let mut dp = vec![0isize; n + 2];
+    let mut sum = vec![0isize; n + 2];
     dp[0] = 1;
+    sum[0] = 1;
 
-    // 素朴にDPを実装した場合
-    // O(N^2) -> TLE
+    for i in 1..=n + 1 {
+        // 範囲外の足場がない場合はスキップ
+        let ir = x.lower_bound(x[i] - l) as isize;
+        let il = x.lower_bound(x[i] - r) as isize;
 
-    // O(NlogN)になるように、二分探索を使う
-    // 配るDP
-    for i in 0..=n {
-        let xl = x.lower_bound(x[i] + l);
-        let xr = x.upper_bound(x[i] + r);
-        // この部分のループは単純に足しているだけなので高速化できそう -> 累積和
-        for j in xl..xr {
-            dp[j] += dp[i];
-            dp[j] %= MOD;
-        }
+        println!("{} {} {}", i, il, ir);
+
+        let sumxl = if x[i] <= r { 0 } else { sum[il - 1] };
+
+        dp[i] = (sum[ir] - sumxl) % MOD as isize;
+        sum[i] = (sum[i - 1] + dp[i]) % MOD as isize;
     }
+
+    println!("{:?}", dp);
+    println!("{:?}", sum);
 
     println!("{}", dp[n + 1]);
 }
