9a5a9e5ca6cf9b98a75e905fd5e5fdf0ff79fb8b
aliyome <aliyome@gmail.com>
2023-07-25 21:08:41 +0900

    abc309 b

--- abc309/src/bin/b.rs
+++ abc309/src/bin/b.rs
@@ -7,21 +7,16 @@ fn main() {
     };
 
     let mut b = a.clone();
-
-    // 1行目
     for j in 1..n {
         b[0][j] = a[0][j - 1];
     }
-    // n-1列目
     for i in 1..n {
         b[i][n - 1] = a[i - 1][n - 1];
     }
-    // n-1行目
-    for j in (0..n - 1).rev() {
+    for j in 0..n - 1 {
         b[n - 1][j] = a[n - 1][j + 1];
     }
-    // 0列目
-    for i in (0..n - 1).rev() {
+    for i in 0..n - 1 {
         b[i][0] = a[i + 1][0];
     }
 
@@ -30,6 +25,5 @@ fn main() {
             print!("{}", b[i][j]);
         }
         println!();
-        // println!("{:?}", b[i]);
     }
 }
