5f5863d6639f5acdeff64f77a229d69b14ab124b
aliyome <aliyome@gmail.com>
2023-07-26 21:03:18 +0900

    abc219 a

--- abc219/src/bin/a.rs
+++ abc219/src/bin/a.rs
@@ -2,8 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        x: usize,
     };
+    for y in [40, 70, 90] {
+        if x < y {
+            println!("{}", y - x);
+            return;
+        }
+    }
 
-    println!("{}", n);
+    println!("expert");
 }
