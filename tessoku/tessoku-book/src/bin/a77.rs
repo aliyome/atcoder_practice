0ccdcb0c452a78e4d487fed83a55d52ca62650ce
aliyome <aliyome@gmail.com>
2023-07-27 14:17:15 +0900

    tessoku a77 binary_search

--- tessoku/tessoku-book/src/bin/a77.rs
+++ tessoku/tessoku-book/src/bin/a77.rs
@@ -3,7 +3,37 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      l: usize,
+      k: usize,
+      mut a: [usize; n]
     }
 
-    println!("{}", n);
+    a.insert(0, 0);
+    a.push(l);
+
+    // スコアを二分探索で求める
+    let is_ok = |len| {
+        let mut count = 0;
+        let mut left = 0;
+        for right in 1..a.len() {
+            if a[right] - a[left] >= len {
+                count += 1;
+                left = right;
+            }
+        }
+        count >= k + 1
+    };
+
+    let mut ng = l;
+    let mut ok = 0;
+    while ng - ok > 1 {
+        let mid = (ok + ng) / 2;
+        if is_ok(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", ok);
 }
