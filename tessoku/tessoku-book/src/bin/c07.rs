21a9ed9b39311272d244d1f5618c4c1bb56bf51d
aliyome <aliyome@gmail.com>
2023-07-28 11:52:01 +0900

    tessoku c07 累積和 二分探索

--- tessoku/tessoku-book/src/bin/c07.rs
+++ tessoku/tessoku-book/src/bin/c07.rs
@@ -1,4 +1,5 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
     input! {
@@ -9,17 +10,16 @@ fn main() {
     }
 
     c.sort();
+    c.insert(0, 0);
 
+    let mut acc = vec![0; n + 1];
+    for i in 1..=n {
+        acc[i] = acc[i - 1] + c[i];
+    }
+
+    // O(10^5)
     for &x in &x {
-        let mut sum = 0;
-        let mut count = 0;
-        for &c in &c {
-            sum += c;
-            if sum > x {
-                break;
-            }
-            count += 1;
-        }
-        println!("{}", count);
+        let count = acc.upper_bound(&x);
+        println!("{}", count - 1);
     }
 }
