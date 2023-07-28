1c458f1dacf5cdf4e93df58a796acf21e5b070ab
aliyome <aliyome@gmail.com>
2023-07-28 11:27:30 +0900

    tessoku c04 約数の列挙 √N

--- tessoku/tessoku-book/src/bin/c04.rs
+++ tessoku/tessoku-book/src/bin/c04.rs
@@ -5,5 +5,18 @@ fn main() {
       n: usize,
     }
 
-    println!("{}", n);
+    let mut ans = vec![];
+    let mut i = 1;
+    while i * i <= n {
+        if n % i == 0 {
+            ans.push(i);
+            ans.push(n / i);
+        }
+        i += 1;
+    }
+    ans.sort();
+
+    for e in ans {
+        println!("{}", e);
+    }
 }
