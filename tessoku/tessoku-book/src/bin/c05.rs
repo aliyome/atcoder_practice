c51dcf58fe80a17af96882cd59ec6d563d07c8db
aliyome <aliyome@gmail.com>
2023-07-28 11:36:27 +0900

    tessoku c05 bit全探索

--- tessoku/tessoku-book/src/bin/c05.rs
+++ tessoku/tessoku-book/src/bin/c05.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
@@ -5,5 +7,23 @@ fn main() {
       n: usize,
     }
 
-    println!("{}", n);
+    let mut lucky_list = BTreeSet::new();
+    for i in 0..1 << 10 {
+        let mut lucky = vec![];
+        for j in 0..10 {
+            if i & 1 << j != 0 {
+                lucky.push(7);
+            } else {
+                lucky.push(4);
+            }
+        }
+        let str = lucky
+            .iter()
+            .map(|&x| x.to_string())
+            .collect::<Vec<_>>()
+            .join("");
+        lucky_list.insert(str);
+    }
+
+    println!("{}", lucky_list.iter().skip(n - 1).next().unwrap());
 }
