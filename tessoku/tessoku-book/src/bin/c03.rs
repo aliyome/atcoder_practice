ca0cedffc383ccf5193e8920540aaa413d9aba00
aliyome <aliyome@gmail.com>
2023-07-28 10:45:03 +0900

    tessoku c03

--- tessoku/tessoku-book/src/bin/c03.rs
+++ tessoku/tessoku-book/src/bin/c03.rs
@@ -2,8 +2,26 @@ use proconio::input;
 
 fn main() {
     input! {
-      n: usize,
+      d: usize,
+      x: usize,
+      mut a: [isize; d - 1],
+      q: usize,
+      st: [(usize, usize); q]
     }
 
-    println!("{}", n);
+    let mut price = vec![0; d];
+    price[0] = x as isize;
+    for i in 0..d - 1 {
+        price[i + 1] = price[i] + a[i];
+    }
+
+    for &(s, t) in &st {
+        if price[s - 1] < price[t - 1] {
+            println!("{}", t);
+        } else if price[s - 1] > price[t - 1] {
+            println!("{}", s);
+        } else {
+            println!("Same");
+        }
+    }
 }
