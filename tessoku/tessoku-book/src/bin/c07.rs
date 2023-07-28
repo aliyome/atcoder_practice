8519b12f5a43707b4914f8e8c3228cecbc24675e
aliyome <aliyome@gmail.com>
2023-07-28 11:46:22 +0900

    tessoku c07 貪欲

--- tessoku/tessoku-book/src/bin/c07.rs
+++ tessoku/tessoku-book/src/bin/c07.rs
@@ -3,7 +3,23 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      mut c: [usize; n],
+      q: usize,
+      x: [usize; q]
     }
 
-    println!("{}", n);
+    c.sort();
+
+    for &x in &x {
+        let mut sum = 0;
+        let mut count = 0;
+        for &c in &c {
+            sum += c;
+            if sum > x {
+                break;
+            }
+            count += 1;
+        }
+        println!("{}", count);
+    }
 }
