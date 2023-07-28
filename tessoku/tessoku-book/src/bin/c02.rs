e99c419850583a8d1b67f3b46103ace5217b122e
aliyome <aliyome@gmail.com>
2023-07-28 10:37:58 +0900

    tessoku c02

--- tessoku/tessoku-book/src/bin/c02.rs
+++ tessoku/tessoku-book/src/bin/c02.rs
@@ -3,7 +3,19 @@ use proconio::input;
 fn main() {
     input! {
       n: usize,
+      a: [usize; n]
     }
 
-    println!("{}", n);
+    let mut ans = 0;
+    // O(N^2) 100^2 = 10000
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            ans = ans.max(a[i] + a[j]);
+        }
+    }
+
+    println!("{}", ans);
 }
