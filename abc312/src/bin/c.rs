238ff6616c8e6a3e76e490c129050e6b8c551cf4
aliyome <aliyome@gmail.com>
2023-07-29 21:30:01 +0900

    abc312 c binary_search

--- abc312/src/bin/c.rs
+++ abc312/src/bin/c.rs
@@ -1,8 +1,36 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        a: [usize; n],
+        b: [usize; m]
     };
 
+    let mut sell = BTreeSet::new();
+    let mut buy = BTreeSet::new();
+    for i in 0..n {
+        sell.insert((a[i], i as isize));
+    }
+    for i in 0..m {
+        buy.insert((b[i], i as isize));
+    }
+
+    let mut ng = 0;
+    let mut ok = 1_000_000_000;
+    while ok - ng > 1 {
+        let mid = (ng + ok) / 2;
+        let sell_cnt = sell.range(..=(mid, 1_000_000_000)).count();
+        let buy_cnt = buy.range((mid, -1)..).count();
+        if buy_cnt <= sell_cnt {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+
+    println!("{}", ok);
 }
