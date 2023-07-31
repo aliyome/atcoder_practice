3cfe2b4d1f6a19c0e7f060c9973a3a8be890ce43
aliyome <aliyome@gmail.com>
2023-07-31 21:14:20 +0900

    abc202 c map 片方固定

--- abc202/src/bin/c.rs
+++ abc202/src/bin/c.rs
@@ -1,25 +1,38 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
-        b: [usize; n],
-        c: [usize; n],
+        mut a: [usize; n],
+        mut b: [usize; n],
+        mut c: [usize; n],
     };
 
-    let mut reachable = std::collections::HashMap::new();
-    for j in 0..n {
-        *reachable.entry(b[c[j] - 1]).or_insert(0) += 1;
+    a.insert(0, 0);
+    b.insert(0, 0);
+    c.insert(0, 0);
+
+    let mut map = HashMap::new();
+    for i in 1..=n {
+        let c = c[i];
+        *map.entry(b[c]).or_insert(0) += 1;
     }
 
-    let mut ans: i64 = 0;
-    for i in 0..n {
-        let j = reachable.get(&a[i]);
-        if j.is_some() {
-            ans += j.unwrap();
-            // println!("{} {}", i, j.unwrap());
+    let mut ans = 0usize;
+    for i in 1..=n {
+        let a = a[i];
+        if let Some(b_cnt) = map.get(&a) {
+            ans += b_cnt;
         }
+        // for j in 0..n {
+        //     let c = c[j];
+        //     let b = b[c - 1];
+        //     if a == b {
+        //         ans += 1;
+        //     }
+        // }
     }
 
     println!("{}", ans);
--- tessoku/tessoku-book/src/bin/c13.rs
+++ tessoku/tessoku-book/src/bin/c13.rs
@@ -1,9 +1,162 @@
+use std::collections::HashMap;
+
 use proconio::input;
+
+const MOD: usize = 1000000007;
 
 fn main() {
     input! {
-      n: usize,
+      n: usize, // <= 10^5
+      p: usize, // <= 1000000006
+      a: [usize; n] // a[i] <= 10^18
     }
 
-    println!("{}", n);
+    let a = a.iter().map(|&x| ModInt::new(x)).collect::<Vec<_>>();
+
+    let mut map = HashMap::new();
+    for &a in &a {
+        *map.entry(a.value()).or_insert(0) += 1;
+    }
+
+    let mut ans = 0usize;
+    for &a in &a {
+        if a.value() == 0 {
+            continue;
+        }
+        let b = ModInt::new(p) / a;
+        if let Some(&cnt) = map.get(&b.value()) {
+            if cnt > 1 {
+                ans += cnt - 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
+}
+
+use std::ops;
+
+#[derive(Copy, Clone)]
+pub struct ModInt {
+    value: usize,
+}
+
+impl ModInt {
+    pub fn new(value: usize) -> ModInt {
+        ModInt { value: value % MOD }
+    }
+
+    pub fn value(&self) -> usize {
+        self.value
+    }
+
+    pub fn inverse(&self) -> ModInt {
+        // (a, b) -> (x, y) s.t. a * x + b * y = gcd(a, b)
+        fn extended_gcd(a: isize, b: isize) -> (isize, isize) {
+            if (a, b) == (1, 0) {
+                (1, 0)
+            } else {
+                let (x, y) = extended_gcd(b, a % b);
+                (y, x - (a / b) * y)
+            }
+        }
+
+        let (x, _y) = extended_gcd(self.value() as isize, MOD as isize);
+        ModInt::new((MOD as isize + x) as usize)
+    }
+
+    // a^n
+    pub fn pow(&self, mut n: usize) -> ModInt {
+        let mut res = ModInt::new(1);
+        let mut x = *self;
+        while n > 0 {
+            if n % 2 == 1 {
+                res *= x;
+            }
+            x *= x;
+            n /= 2;
+        }
+
+        res
+    }
+}
+
+impl ops::Add for ModInt {
+    type Output = ModInt;
+    fn add(self, other: Self) -> Self {
+        ModInt::new(self.value + other.value)
+    }
+}
+
+impl ops::Sub for ModInt {
+    type Output = ModInt;
+    fn sub(self, other: Self) -> Self {
+        ModInt::new(MOD + self.value - other.value)
+    }
+}
+
+impl ops::Mul for ModInt {
+    type Output = ModInt;
+    fn mul(self, other: Self) -> Self {
+        ModInt::new(self.value * other.value)
+    }
+}
+
+impl ops::Div for ModInt {
+    type Output = ModInt;
+    fn div(self, other: Self) -> Self {
+        self * other.inverse()
+    }
+}
+
+impl ops::AddAssign for ModInt {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl ops::SubAssign for ModInt {
+    fn sub_assign(&mut self, other: Self) {
+        *self = *self - other;
+    }
+}
+
+impl ops::MulAssign for ModInt {
+    fn mul_assign(&mut self, other: Self) {
+        *self = *self * other;
+    }
+}
+
+impl ops::DivAssign for ModInt {
+    fn div_assign(&mut self, other: Self) {
+        *self = *self / other;
+    }
+}
+
+// n!
+pub fn factorial(n: usize) -> ModInt {
+    (1..=n).fold(ModInt::new(1), |x, y| x * ModInt::new(y))
+}
+
+// nPr (https://ja.wikipedia.org/wiki/%E9%A0%86%E5%88%97#%E9%A0%86%E5%88%97%E3%81%AE%E6%95%B0%E3%81%88%E4%B8%8A%E3%81%92)
+pub fn permutation(n: usize, r: usize) -> ModInt {
+    if n < r {
+        ModInt::new(0)
+    } else {
+        (n - r + 1..=n).fold(ModInt::new(1), |x, y| x * ModInt::new(y))
+    }
+}
+
+// nCr (https://ja.wikipedia.org/wiki/%E7%B5%84%E5%90%88%E3%81%9B_(%E6%95%B0%E5%AD%A6))
+pub fn combination(n: usize, r: usize) -> ModInt {
+    if n < r {
+        ModInt::new(0)
+    } else {
+        permutation(n, r) / factorial(r)
+    }
+}
+
+// nHr (https://ja.wikipedia.org/wiki/%E9%87%8D%E8%A4%87%E7%B5%84%E5%90%88%E3%81%9B#%E9%87%8D%E8%A4%87%E7%B5%84%E5%90%88%E3%81%9B%E3%81%AE%E7%B7%8F%E6%95%B0)
+pub fn homogeneous(n: usize, r: usize) -> ModInt {
+    combination(n + r - 1, r)
 }
