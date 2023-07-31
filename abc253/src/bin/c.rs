74787735eadef823a8ff51043e2df069ecda174c
aliyome <aliyome@gmail.com>
2023-07-31 21:26:36 +0900

    abc253 c map

--- abc253/src/bin/c.rs
+++ abc253/src/bin/c.rs
@@ -1,9 +1,42 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        q: usize,
     };
 
-    println!("{}", n);
+    let mut map = BTreeMap::new();
+
+    for i in 1..=q {
+        input! {
+            t: usize,
+        }
+
+        if t == 1 {
+            input! {
+                x: usize
+            }
+            *map.entry(x).or_insert(0) += 1isize;
+        } else if t == 2 {
+            input! {
+                x: usize,
+                c: usize,
+            }
+            if let Some(v) = map.get_mut(&x) {
+                let new_v = *v - (c as isize);
+                if new_v < 0 {
+                    map.remove(&x);
+                } else {
+                    map.insert(x, new_v);
+                }
+            }
+        } else {
+            let min = map.keys().next().unwrap();
+            let max = map.keys().rev().next().unwrap();
+
+            println!("{}", max - min);
+        }
+    }
 }
