7b552db16263e24bdda4424a088af69bde9e4fc1
aliyome <aliyome@gmail.com>
2023-08-01 21:16:30 +0900

    abc215 b log2

--- abc215/src/bin/b.rs
+++ abc215/src/bin/b.rs
@@ -2,8 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: f64,
+        n: usize,
     };
 
-    println!("{}", n.log2().floor());
+    if n == 1 {
+        println!("0");
+        return;
+    }
+
+    let mut x = 2;
+    for k in 1..=n {
+        x *= 2;
+        if x > n {
+            println!("{}", k);
+            return;
+        }
+    }
 }
