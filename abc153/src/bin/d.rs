eecc4be2727239cc9896df548ed61cacf73686b2
aliyome <aliyome@gmail.com>
2023-08-02 21:04:44 +0900

    abc153 d

--- abc153/src/bin/d.rs
+++ abc153/src/bin/d.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut h: usize,
     };
 
+    let mut ans = 0usize;
+    let mut enemies = 1usize;
+
+    while h > 0 {
+        ans += enemies;
+        h /= 2;
+        enemies *= 2;
+    }
+
+    println!("{}", ans);
 }
