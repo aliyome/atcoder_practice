abc2ff5788e704f3ec6d7027cd51b687cdcbdc2b
aliyome <aliyome@gmail.com>
2023-08-03 19:31:51 +0900

    typical90 078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -1,3 +1,26 @@
+use std::collections::BTreeSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m]
+    }
+
+    let mut edges = vec![BTreeSet::new(); n + 1];
+    for &(a, b) in &ab {
+        edges[a].insert(b);
+        edges[b].insert(a);
+    }
+
+    let mut ans = 0;
+    for i in 1..=n {
+        if edges[i].range(..i).count() == 1 {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
