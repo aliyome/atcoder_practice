add8604cb2a6b9c9f39b97ce853aa43f8c613ce9
aliyome <aliyome@gmail.com>
2023-08-03 14:47:24 +0900

    typical90 055 naive

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: usize,
+        q: usize,
+        a: [usize; n]
+    }
+
+    let mut ans = 0usize;
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                for l in k + 1..n {
+                    for m in l + 1..n {
+                        if ((a[i] % p) * (a[j] % p) * (a[k] % p) * (a[l] % p) * (a[m] % p)) % p == q
+                        {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
