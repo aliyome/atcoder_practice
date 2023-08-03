5a1f2ac77ab6a3c23ae33dd67eb212eaeef98045
aliyome <aliyome@gmail.com>
2023-08-03 14:30:03 +0900

    typical090 033 釈然としない

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -5,11 +5,30 @@ fn main() {
         h: usize,
         w: usize
     }
-    if h == 1 || w == 1 {
-        println!("{}", w * h);
-    } else {
-        let ww = (w + 1) / 2;
-        let hh = (h + 1) / 2;
-        println!("{}", ww * hh);
+
+    let mut count = 0;
+    let mut led = vec![vec![0; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            if led[i][j] == 0 {
+                led[i][j] = 1;
+                count += 1;
+                if h == 1 || w == 1 {
+                    continue;
+                }
+
+                if i + 1 < h {
+                    led[i + 1][j] = 2;
+                }
+                if j + 1 < w {
+                    led[i][j + 1] = 2;
+                }
+                if i + 1 < h && j + 1 < w {
+                    led[i + 1][j + 1] = 2;
+                }
+            }
+        }
     }
+
+    println!("{}", count);
 }
