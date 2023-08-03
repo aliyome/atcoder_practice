29d22edaa5743f9a234b52ab6151b019acf05de6
aliyome <aliyome@gmail.com>
2023-08-04 07:36:19 +0900

    abc276 b

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -1,9 +1,26 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        ab: [(usize, usize); m]
     };
 
-    println!("{}", n);
+    let mut edge = vec![BTreeSet::new(); n + 1];
+    for i in 0..m {
+        let (a, b) = ab[i];
+        edge[a].insert((b, i));
+        edge[b].insert((a, i));
+    }
+
+    for i in 1..=n {
+        print!("{} ", edge[i].len());
+        for j in edge[i].iter() {
+            print!("{} ", j.0);
+        }
+        println!();
+    }
 }
