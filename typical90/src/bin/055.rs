acd0edc57e4effd5ecf7f0c5f34fee131a41a2b7
aliyome <aliyome@gmail.com>
2023-08-03 18:10:15 +0900

    typical90 055 計算量見積もり

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -8,14 +8,23 @@ fn main() {
         a: [usize; n]
     }
 
+    let mul = |a, b| {
+        let a = a % p;
+        let b = b % p;
+        let c = a * b;
+        c % p
+    };
+
     let mut ans = 0usize;
     for i in 0..n {
         for j in i + 1..n {
             for k in j + 1..n {
                 for l in k + 1..n {
                     for m in l + 1..n {
-                        if ((a[i] % p) * (a[j] % p) * (a[k] % p) * (a[l] % p) * (a[m] % p)) % p == q
-                        {
+                        // a*b*c*d*e % p == q の数を数える
+                        // 単純に a*b*c*d*e % p == q としてしまうとオーバーフローするので
+                        // 掛け算のたびに mod する
+                        if mul(mul(mul(mul(a[i], a[j]), a[k]), a[l]), a[m]) == q {
                             ans += 1;
                         }
                     }
