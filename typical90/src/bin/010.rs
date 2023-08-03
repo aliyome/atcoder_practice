b32109cc7a423fbc6837252ccb556122f380b14e
aliyome <aliyome@gmail.com>
2023-08-03 13:33:32 +0900

    typical90 010 累積和

--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -8,38 +8,20 @@ fn main() {
         lr: [(usize, usize); q],
     }
 
-    // // 一番愚直な方法 O(N^2) -> TLE
-    // for (l, r) in &lr {
-    //     let mut sum = (0, 0);
-    //     for (i, (c, p)) in cp.iter().enumerate() {
-    //         if i + 1 < *l || *r < i + 1 {
-    //             continue;
-    //         }
-    //         if *c == 1 {
-    //             sum.0 += *p;
-    //         } else {
-    //             sum.1 += *p;
-    //         }
-    //     }
-    // println!("{} {}", sum.0, sum.1);
-    // }
-
-    // 累積和を使う O(N + Q)
-    let mut sum = vec![vec![0; n + 1]; 2 + 1];
-    // 全生徒の累積和を計算
+    let mut acc = vec![vec![0; n + 1]; 2];
     for i in 0..n {
-        sum[1][i + 1] = sum[1][i];
-        sum[2][i + 1] = sum[2][i];
-        sum[cp[i].0][i + 1] += cp[i].1;
-    }
-    // println!("{:?}", sum);
-    // 指定区間を計算
-    // println!("{:?}", lr);
-    for (l, r) in &lr {
-        let c1 = sum[1][*r] - sum[1][*l - 1];
-        let c2 = sum[2][*r] - sum[2][*l - 1];
-        println!("{} {}", c1, c2);
+        acc[0][i + 1] = acc[0][i];
+        acc[1][i + 1] = acc[1][i];
+
+        let (c, p) = cp[i];
+        let c = c - 1;
+        acc[c][i + 1] += p;
     }
 
-    // セグメントツリーを使う
+    for &(l, r) in &lr {
+        for c in 0..2 {
+            print!("{} ", acc[c][r] - acc[c][l - 1]);
+        }
+        println!();
+    }
 }
