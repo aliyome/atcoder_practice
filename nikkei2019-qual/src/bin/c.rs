01189e27b5fa241485d319b1fe12957ce7887cde
aliyome <aliyome@gmail.com>
2023-08-03 12:44:05 +0900

    nikkei2019-qual c 解答AC

--- nikkei2019-qual/src/bin/c.rs
+++ nikkei2019-qual/src/bin/c.rs
@@ -5,44 +5,22 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        ab: [(usize, usize); n]
+        ab: [(isize, isize); n]
     };
 
-    let mut skip = HashSet::new();
-    let mut turn = 0;
-    let mut takahashi = 0;
-    let mut aoki = 0;
-    for _ in 0..n {
-        let mut min = 10_000_000_000;
-        let mut min_i = 0;
-        for i in 0..n {
-            let (a, b) = ab[i];
-            if skip.contains(&i) {
-                continue;
-            }
-            if turn == 0 {
-                // takahashi
-                if b < min {
-                    min = b;
-                    min_i = i;
-                }
-            } else {
-                // aoki
-                if a < min {
-                    min = a;
-                    min_i = i;
-                }
-            }
-        }
-        if turn == 0 {
-            takahashi += ab[min_i].0;
-        } else {
-            aoki += ab[min_i].1;
-        }
-
-        skip.insert(min_i);
-        turn = 1 - turn;
+    let mut dishes = vec![];
+    for &(a, b) in &ab {
+        dishes.push((a + b, a, b));
     }
+    dishes.sort_by(|a, b| b.0.cmp(&a.0));
 
-    println!("{}", takahashi as isize - aoki as isize);
+    let mut ans = 0isize;
+    for i in 0..n {
+        if i % 2 == 0 {
+            ans += dishes[i].1;
+        } else {
+            ans -= dishes[i].2;
+        }
+    }
+    println!("{}", ans);
 }
