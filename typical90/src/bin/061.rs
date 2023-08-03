1071f5fcd20f1e0e7c5ed37ffbd4cccb0467517b
aliyome <aliyome@gmail.com>
2023-08-03 18:15:45 +0900

    typical90 061 deque

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,3 +1,20 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+        tx: [(usize, usize); q]
+    }
+
+    let mut queue = VecDeque::new();
+    for (t, x) in tx {
+        match t {
+            1 => queue.push_front(x),
+            2 => queue.push_back(x),
+            3 => println!("{}", queue[x - 1]),
+            _ => unreachable!(),
+        }
+    }
 }
