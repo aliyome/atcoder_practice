ecb4008f6b644beb11c90aab9b0d1388d3de7157
aliyome <aliyome@gmail.com>
2023-08-05 07:38:20 +0900

    abc295 b

--- abc295/src/bin/b.rs
+++ abc295/src/bin/b.rs
@@ -7,25 +7,24 @@ fn main() {
         b: [Chars; r]
     };
 
-    let mut ans: Vec<Vec<char>> = b.clone();
-
-    // 全探索
-    let mut bomb = |y: usize, x: usize, k: usize| {
-        for i in 0..r {
-            for j in 0..c {
-                if (i as i32 - y as i32).abs() + (j as i32 - x as i32).abs() <= k as i32 {
+    let mut bomb = vec![];
+    for i in 0..r {
+        for j in 0..c {
+            if b[i][j] == '#' || b[i][j] == '.' {
+                continue;
+            }
+            let k = b[i][j].to_digit(10).unwrap() as isize;
+            bomb.push((i, j, k));
+        }
+    }
+    let mut ans = b.clone();
+    for i in 0..r {
+        for j in 0..c {
+            for &b in &bomb {
+                if (i as isize - b.0 as isize).abs() + (j as isize - b.1 as isize).abs() <= b.2 {
                     ans[i][j] = '.';
                 }
             }
-        }
-    };
-    for i in 0..r {
-        for j in 0..c {
-            if b[i][j] == '.' || b[i][j] == '#' {
-                continue;
-            }
-            let k = b[i][j].to_digit(10).unwrap() as usize;
-            bomb(i, j, k);
         }
     }
 
