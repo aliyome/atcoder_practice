ce96939db929e9ce8dad7458b7826c330c21f489
aliyome <aliyome@gmail.com>
2023-08-04 11:19:32 +0900

    abc144 d math

--- abc144/src/bin/d.rs
+++ abc144/src/bin/d.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        a: f64,
+        b: f64,
+        x: f64,
     };
 
-    println!("{}", n);
+    // 体積
+    let s = x / a;
+
+    // パターン分け
+    if s >= a * b / 2.0 {
+        let h = (2.0 * (a * b - s)) / a;
+        let ans = h.atan2(a) * 180.0 / std::f64::consts::PI;
+        println!("{}", ans);
+    } else {
+        let w = 2.0 * s / b;
+        let ans = b.atan2(w) * 180.0 / std::f64::consts::PI;
+        println!("{}", ans);
+    }
 }
