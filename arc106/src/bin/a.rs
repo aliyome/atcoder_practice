89c4e68cf3c07d338af062f8545ab1700a91070c
aliyome <aliyome@gmail.com>
2023-08-05 07:55:04 +0900

    fix: arc106 a

--- arc106/src/bin/a.rs
+++ arc106/src/bin/a.rs
@@ -8,8 +8,8 @@ fn main() {
     // 3^38 > 10^18
     // 5^26 > 10^18
 
-    for i in 0..38 {
-        for j in 0..26 {
+    for i in 1..=38 {
+        for j in 1..=26 {
             let x = 3_u128.pow(i) + 5_u128.pow(j);
             if x == n as u128 {
                 println!("{} {}", i, j);
