452c5fe5559791ab94a95b2d8da38213fd5de0b6
aliyome <aliyome@gmail.com>
2023-08-05 21:32:36 +0900

    abc313 c

--- abc313/src/bin/c.rs
+++ abc313/src/bin/c.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [usize; n]
-    };
+        a: [i64; n],
+    }
+
+    // 合計値を計算
+    let sum: i64 = a.iter().sum();
+
+    // 平均値（整数部分）を計算
+    let avg = sum / n as i64;
+
+    // 操作回数を初期化
+    let mut ops = 0;
+
+    // 各要素に対して、平均値との差の絶対値を計算し、操作回数に追加
+    for &x in &a {
+        ops += (x - avg).abs();
+    }
+    println!("{}", ops / 2);
 }
