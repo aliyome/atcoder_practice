25d429d9759f24ed64586e25a057fd0701b39f99
aliyome <aliyome@gmail.com>
2023-08-05 21:21:04 +0900

    abc313 b ai

--- abc313/src/bin/b.rs
+++ abc313/src/bin/b.rs
@@ -3,6 +3,34 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
-
+        m: usize,
+        ab: [(usize, usize); m],
+    }
+    let mut dp = vec![vec![false; n]; n];
+    for (a, b) in ab {
+        dp[a - 1][b - 1] = true;
+    }
+    for k in 0..n {
+        for i in 0..n {
+            for j in 0..n {
+                if dp[i][k] && dp[k][j] {
+                    dp[i][j] = true;
+                }
+            }
+        }
+    }
+    let mut strongest = None;
+    for i in 0..n {
+        if (0..n).all(|j| i == j || dp[i][j]) {
+            if strongest.replace(i).is_some() {
+                println!("-1");
+                return;
+            }
+        }
+    }
+    if let Some(i) = strongest {
+        println!("{}", i + 1);
+    } else {
+        println!("-1");
+    }
 }
