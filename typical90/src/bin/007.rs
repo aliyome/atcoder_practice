7c9d0d75aaeb88af2e7d6549a50e6a77988b0991
aliyome <aliyome@gmail.com>
2023-08-06 19:39:49 +0900

    typical90 007 二分探索

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,40 +1,26 @@
+use superslice::Ext;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        a: [i64; n],
+        mut a: [isize; n],
         q: usize,
-        b: [i64; q],
+        b: [isize; q],
     }
 
-    // 二分探索するためにaをソートして番兵を追加する
-    let mut a = a;
     a.sort();
-
-    // 二分探索の結果を出力する
-    for b in b {
-        println!("{}", binary_search(&a, b));
-    }
-}
-
-fn binary_search(a: &Vec<i64>, x: i64) -> i64 {
-    let mut ok = 0;
-    let mut ng = a.len() - 1;
-
-    while ng - ok > 1 {
-        let mid = (ok + ng) / 2;
-        if a[mid] <= x {
-            ok = mid;
-        } else {
-            ng = mid;
+    for &b in &b {
+        let i = a.lower_bound(&b);
+        let mut r = 10_000_000_000isize;
+        let mut l = 10_000_000_000isize;
+        if 0 < i {
+            l = (b - a[i - 1]).abs();
         }
-    }
-    let diff1 = (a[ok] - x).abs();
-    let diff2 = (a[ng] - x).abs();
-    if diff1 < diff2 {
-        diff1
-    } else {
-        diff2
+        if i < n {
+            r = (b - a[i]).abs();
+        }
+        println!("{}", l.min(r));
     }
 }
