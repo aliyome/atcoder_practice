c695ec62419f9552a4edc53f2e527e5c98ec374d
aliyome <aliyome@gmail.com>
2023-08-07 07:36:41 +0900

    abc224 c

--- abc224/src/bin/c.rs
+++ abc224/src/bin/c.rs
@@ -10,14 +10,16 @@ fn main() {
     for i in 0..n - 2 {
         for j in i + 1..n - 1 {
             for k in j + 1..n {
-                let (x1, y1) = xy[i];
-                let (x2, y2) = xy[j];
-                let (x3, y3) = xy[k];
-                if ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)).abs() > 0 {
+                if ((xy[i].0 - xy[k].0) * (xy[j].1 - xy[k].1)
+                    - (xy[j].0 - xy[k].0) * (xy[i].1 - xy[k].1))
+                    .abs()
+                    > 0
+                {
                     ans += 1;
-                }
+                };
             }
         }
     }
+
     println!("{}", ans);
 }
