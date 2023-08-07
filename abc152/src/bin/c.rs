e3c1844f463dda819fcde284af10a053af43e754
aliyome <aliyome@gmail.com>
2023-08-08 07:43:36 +0900

    abc152 c

--- abc152/src/bin/c.rs
+++ abc152/src/bin/c.rs
@@ -3,7 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        p: [usize; n]
     };
 
-    println!("{}", n);
+    let mut min = std::usize::MAX;
+    let mut ans = 0usize;
+    for i in 0..n {
+        min = min.min(p[i]);
+        if p[i] <= min {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
