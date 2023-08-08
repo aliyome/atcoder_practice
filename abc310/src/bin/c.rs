b43abd4a966c24b2a8612313dc85a4a0d454880b
aliyome <aliyome@gmail.com>
2023-08-08 21:14:58 +0900

    abc310 c

--- abc310/src/bin/c.rs
+++ abc310/src/bin/c.rs
@@ -7,13 +7,16 @@ fn main() {
         s: [String; n],
     }
 
+    let mut ans = 0;
     let mut set = HashSet::new();
-    for s in s {
-        let rev = s.chars().rev().collect::<String>();
-        if !set.contains(&s) && !set.contains(&rev) {
-            set.insert(s);
+    for s in s.iter() {
+        if set.contains(s) {
+            continue;
         }
+        set.insert(s.clone());
+        set.insert(s.chars().rev().collect::<String>());
+        ans += 1;
     }
 
-    println!("{}", set.len());
+    println!("{}", ans);
 }
