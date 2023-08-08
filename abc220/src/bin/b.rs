0a7a06ccdcf857153a86914ae3d6f5b3f9c1e351
aliyome <aliyome@gmail.com>
2023-08-08 21:08:34 +0900

    abc220 b K進法

--- abc220/src/bin/b.rs
+++ abc220/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        k: usize,
+        mut a: Chars,
+        mut b: Chars
     };
 
+    a.reverse();
+    b.reverse();
+
+    let mut x = 0;
+    for i in 0..a.len() {
+        x += k.pow(i as u32) * a[i].to_digit(10).unwrap() as usize;
+    }
+
+    let mut y = 0;
+    for i in 0..b.len() {
+        y += k.pow(i as u32) * b[i].to_digit(10).unwrap() as usize;
+    }
+
+    println!("{}", x * y);
 }
