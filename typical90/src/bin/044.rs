1be8d60d5e9886adbdd839626b71cfca8ad58cb9
aliyome <aliyome@gmail.com>
2023-08-09 07:37:01 +0900

    wip

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -1,38 +1,28 @@
-use std::collections::VecDeque;
-
 use proconio::input;
-
-// Deque を使って愚直に実装するだけで AC が出てしまった
 fn main() {
     input! {
-        (n, q): (usize, usize),
+        n: usize,
+        q: usize,
         a: [usize; n],
+        txy: [(usize, usize, usize); q],
     }
 
-    let mut a = VecDeque::from(a);
-
-    for _ in 0..q {
-        input! {
-            t: usize,
-            x: isize,
-            y: isize
-        }
-        match t {
-            1 => {
-                let x = x - 1;
-                let y = y - 1;
-                let tmp = a[x as usize];
-                a[x as usize] = a[y as usize];
-                a[y as usize] = tmp;
-            }
-            2 => {
-                let tmp = a.pop_back().unwrap();
-                a.push_front(tmp);
-            }
-            3 => {
-                println!("{}", a[x as usize - 1]);
-            }
-            _ => unreachable!(),
+    let mut shift = 0;
+    let mut a = a;
+    for (t, x, y) in txy {
+        if t == 1 {
+            // 0-indexed
+            let x = x - 1;
+            let y = y - 1;
+            let x = (x + n - shift) % n;
+            let y = (y + n - shift) % n;
+            a.swap(x, y);
+        } else if t == 2 {
+            shift = (shift + 1) % n;
+        } else {
+            // 0-indexed
+            let x = x - 1;
+            println!("{}", a[(x + shift) % n]);
         }
     }
 }
