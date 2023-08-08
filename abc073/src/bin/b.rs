c8a82e666548da3d264b1684e94bcbcc82534af4
aliyome <aliyome@gmail.com>
2023-08-09 07:36:53 +0900

    abc073 b

--- abc073/src/bin/b.rs
+++ abc073/src/bin/b.rs
@@ -1,9 +1,20 @@
+use im_rc::HashSet;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        lr: [(usize, usize); n]
     };
 
-    println!("{}", n);
+    let mut ans = 0;
+    for i in 1..=100000 {
+        for &(l, r) in &lr {
+            if l <= i && i <= r {
+                ans += 1
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
