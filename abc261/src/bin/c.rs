85148a22c911132b4f35f105ee7245d0774144c7
aliyome <aliyome@gmail.com>
2023-08-10 07:42:21 +0900

    abc261 c

--- abc261/src/bin/c.rs
+++ abc261/src/bin/c.rs
@@ -1,8 +1,20 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        s: [String; n]
     };
 
+    let mut map = HashMap::new();
+    for i in 0..n {
+        if map.contains_key(&s[i]) {
+            println!("{}({})", s[i], map[&s[i]]);
+        } else {
+            println!("{}", s[i]);
+        }
+        *map.entry(&s[i]).or_insert(0) += 1usize;
+    }
 }
