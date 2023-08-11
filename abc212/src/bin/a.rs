d05062c5fd052162ca7fc380d2dfb1c2b8506b63
aliyome <aliyome@gmail.com>
2023-08-11 21:01:53 +0900

    abc212 a

--- abc212/src/bin/a.rs
+++ abc212/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        b: usize,
     };
 
+    if 0 < a && b == 0 {
+        println!("Gold");
+    } else if a == 0 && 0 < b {
+        println!("Silver");
+    } else {
+        println!("Alloy");
+    }
 }
