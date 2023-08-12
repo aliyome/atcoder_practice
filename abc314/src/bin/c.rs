cead6381b6c546c98954cc556331ca6d54cbfd32
aliyome <aliyome@gmail.com>
2023-08-12 21:22:57 +0900

    abc314 c

--- abc314/src/bin/c.rs
+++ abc314/src/bin/c.rs
@@ -1,8 +1,27 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        s: Chars,
+        c: [usize; n]
     };
 
+    let mut list = vec![VecDeque::new(); m];
+    for i in 0..n {
+        list[c[i] - 1].push_back(i);
+    }
+
+    for i in 0..m {
+        if let Some(v) = list[i].pop_back() {
+            list[i].push_front(v);
+        }
+    }
+
+    for i in 0..n {
+        print!("{}", s[list[c[i] - 1].pop_front().unwrap()]);
+    }
 }
