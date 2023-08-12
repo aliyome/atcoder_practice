016be711447a95ec3fcba0e88750bae739975934
aliyome <aliyome@gmail.com>
2023-08-12 21:13:46 +0900

    abc314 b

--- abc314/src/bin/b.rs
+++ abc314/src/bin/b.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeMap, HashSet};
+
 use proconio::input;
 
 fn main() {
@@ -5,4 +7,39 @@ fn main() {
         n: usize,
     };
 
+    let mut a = vec![HashSet::new(); n];
+    for i in 0..n {
+        input! {
+            c: usize,
+            ai: [usize; c]
+        };
+        for j in 0..c {
+            a[i].insert(ai[j]);
+        }
+    }
+
+    input! {
+        x: usize
+    };
+
+    let mut map = BTreeMap::new();
+    for i in 0..n {
+        if a[i].contains(&x) {
+            map.entry(a[i].len()).or_insert(vec![]).push(i + 1);
+        }
+    }
+    if map.len() == 0 {
+        println!("0");
+        println!();
+    } else {
+        let (_, list) = map.iter().next().unwrap();
+        println!("{}", list.len());
+        println!(
+            "{}",
+            list.iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(" ")
+        );
+    }
 }
