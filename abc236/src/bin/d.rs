ffc7f8c1a1a96bfa79016011ddff57bf13416a49
aliyome <aliyome@gmail.com>
2023-08-12 17:10:36 +0900

    abc236 d DFS

--- abc236/src/bin/d.rs
+++ abc236/src/bin/d.rs
@@ -5,43 +5,52 @@ fn main() {
         n: usize, // <= 8
     };
 
-    let mut bit_point = vec![0; 1 << 2 * n];
+    // bit_point[bit] := 今まで選んだ人の集合がbitのときの楽しさ
+    let mut bit_point = vec![vec![0; 2 * n + 1]; 2 * n + 1];
     for i in 1..2 * n {
         input! {
-            a: [isize; 2*n - i]
+            a: [usize; 2*n - i]
         }
         for j in 0..a.len() {
-            bit_point[1 << (i - 1) | 1 << (i + j)] = a[j];
+            bit_point[i][i + j + 1] = a[j];
         }
     }
 
-    // DEBUG
-    // for i in 0..1 << 2 * n {
-    //     println!("{:04b} {}", i, bit_point[i]);
-    // }
+    let mut used = vec![false; 2 * n + 1];
+    let mut ans = vec![];
+    dfs(n, 0, &bit_point, &mut used, &mut ans);
 
-    // dp[i][bit] := i組目までを使って、bitの人を選んだときの最大値
-    let mut dp = vec![vec![0; 1 << 2 * n]; 2 * n + 1];
-    for i in 0..n {
-        // bit := 今までに選んだ人の集合
-        for from_bit in 0..(1 << (2 * n)) as usize {
-            // 今まで選ばれた人がi * 2人でなければスキップ (i組目までにはi * 2人選ばれている)
-            if from_bit.count_ones() != (i * 2) as u32 {
-                continue;
-            }
+    println!("{}", *ans.iter().max().unwrap());
+}
 
-            // 次に選ぶ人
-            // FIXME
-            for next_bit in 0..(1 << (2 * n)) as usize {
-                // 今まで選ばれた人がi * 2人でなければスキップ (i組目までにはi * 2人選ばれている)
-                let pair = from_bit ^ next_bit;
-                if pair.count_ones() != 2 {
-                    continue;
-                }
-                dp[i + 1][next_bit] = dp[i + 1][next_bit].max(dp[i][from_bit] + bit_point[pair]);
-            }
+fn dfs(
+    n: usize,
+    point: usize,
+    bit_point: &Vec<Vec<usize>>,
+    used: &mut Vec<bool>,
+    ans: &mut Vec<usize>,
+) {
+    let mut next_1 = 0;
+    for i in 1..=2 * n {
+        if !used[i] {
+            next_1 = i;
+            break;
         }
     }
 
-    println!("{}", dp[n][(1 << (2 * n)) - 1]);
+    if next_1 == 0 {
+        ans.push(point);
+        return;
+    }
+
+    used[next_1] = true;
+    for next_2 in next_1 + 1..=2 * n {
+        if used[next_2] {
+            continue;
+        }
+        used[next_2] = true;
+        dfs(n, point ^ bit_point[next_1][next_2], bit_point, used, ans);
+        used[next_2] = false;
+    }
+    used[next_1] = false;
 }
