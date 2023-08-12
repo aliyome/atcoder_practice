c74a70a3b83bdf8b7e395904e631ddfb5c25e1fd
aliyome <aliyome@gmail.com>
2023-08-13 07:37:04 +0900

    abc032 a

--- abc032/src/bin/a.rs
+++ abc032/src/bin/a.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
+        a: usize,
+        b: usize,
         n: usize,
     };
+    let l = lcm(a, b);
+    for i in 1.. {
+        if l * i >= n {
+            println!("{}", l * i);
+            return;
+        }
+    }
+}
 
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
 }
