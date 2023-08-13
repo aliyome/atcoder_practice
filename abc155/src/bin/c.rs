124e334cf79bcf47322c70fdc0f1f8271a2753b2
aliyome <aliyome@gmail.com>
2023-08-14 07:39:30 +0900

    abc155 c

--- abc155/src/bin/c.rs
+++ abc155/src/bin/c.rs
@@ -1,9 +1,23 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        s: [String; n]
     };
 
-    println!("{}", n);
+    let mut map = BTreeMap::new();
+    let mut max = 0;
+    for s in s {
+        *map.entry(s.clone()).or_insert(0) += 1usize;
+        max = max.max(map[&s]);
+    }
+
+    for (k, v) in map {
+        if v == max {
+            println!("{}", k);
+        }
+    }
 }
