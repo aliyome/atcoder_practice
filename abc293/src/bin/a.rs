1b31cce7f9248609080f5b46dffc74cda9408bc9
aliyome <aliyome@gmail.com>
2023-08-15 07:33:34 +0900

    abc293 a

--- abc293/src/bin/a.rs
+++ abc293/src/bin/a.rs
@@ -1,9 +1,14 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     };
 
-    println!("{}", n);
+    let mut t = s.clone();
+    for i in 1..=s.len() / 2 {
+        t.swap(2 * i - 1 - 1, 2 * i - 1);
+    }
+
+    println!("{}", t.iter().collect::<String>());
 }
