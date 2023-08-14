a59ba19b5719539bef7c5924ba0c70560e8c34cb
aliyome <aliyome@gmail.com>
2023-08-14 14:47:39 +0900

    typical90 052 数式変形

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
+const MOD: usize = 1000000007;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [[usize; 6]; n],
+    };
+
+    let mut sums = vec![0; n];
+    for i in 0..n {
+        for j in 0..6 {
+            sums[i] += a[i][j];
+            sums[i] %= MOD;
+        }
+    }
+
+    let mut ans = 1;
+    for i in 0..n {
+        ans *= sums[i];
+        ans %= MOD;
+    }
+
+    println!("{}", ans);
 }
