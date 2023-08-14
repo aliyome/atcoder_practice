afaa0913b702ed0a96d534dad17df1f035b34d05
aliyome <aliyome@gmail.com>
2023-08-14 21:32:58 +0900

    abc141 d

--- abc141/src/bin/d.rs
+++ abc141/src/bin/d.rs
@@ -1,9 +1,23 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        a: [usize; n]
     };
 
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    for &a in &a {
+        heap.push(a);
+    }
+
+    for _ in 0..m {
+        let max = heap.pop().unwrap();
+        heap.push(max / 2);
+    }
+
+    println!("{}", heap.iter().sum::<usize>());
 }
