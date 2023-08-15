e0bd3837434fb9fc99fe90a9d0da79be358dc205
aliyome <aliyome@gmail.com>
2023-08-16 07:31:55 +0900

    abc200 b

--- abc200/src/bin/b.rs
+++ abc200/src/bin/b.rs
@@ -2,8 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut n: usize,
+        k: usize,
     };
+
+    for _ in 0..k {
+        if n % 200 == 0 {
+            n /= 200;
+        } else {
+            n = n * 1000 + 200;
+        }
+    }
 
     println!("{}", n);
 }
