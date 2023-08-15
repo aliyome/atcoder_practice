e518e4b9b0acfc0380dd0dac3f59f6f46e36fc72
aliyome <aliyome@gmail.com>
2023-08-16 08:10:55 +0900

    abc172 c

--- abc172/src/bin/c.rs
+++ abc172/src/bin/c.rs
@@ -1,9 +1,33 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        k: usize,
+        a: [usize; n],
+        b: [usize; m],
     };
 
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    for i in 0..n {
+        heap.push(Reverse(a[i]));
+    }
+    for i in 0..m {
+        heap.push(Reverse(b[i]));
+    }
+
+    let mut time = 0;
+    let mut count = 0;
+    while let Some(Reverse(x)) = heap.pop() {
+        if time + x > k {
+            break;
+        }
+        time += x;
+        count += 1;
+    }
+
+    println!("{}", count);
 }
