c66a963e4fabfe1c2b99484a4e3a13ed5b3c6cf5
aliyome <aliyome@gmail.com>
2023-08-19 21:04:34 +0900

    abc315 b

--- abc315/src/bin/b.rs
+++ abc315/src/bin/b.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        m: usize,
+        d: [usize; m]
     };
 
+    let sum = d.iter().sum::<usize>();
+    let mut x = (sum + 1) / 2;
+    for i in 0..m {
+        if x <= d[i] {
+            println!("{} {}", i + 1, x);
+            return;
+        }
+        x -= d[i];
+    }
 }
