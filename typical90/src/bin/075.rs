8e02c7c249a0b399c6cdc4e2610aaa631b910965
aliyome <aliyome@gmail.com>
2023-08-19 14:43:55 +0900

    typical90 075

--- typical90/src/bin/075.rs
+++ typical90/src/bin/075.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    }
+
+    let primes = prime_factors(n);
+    println!("{}", (primes.len() as f64).log2().ceil() as usize);
+}
+
+fn prime_factors(mut n: usize) -> Vec<usize> {
+    let mut factors = Vec::new();
+    let mut div = 2;
+
+    while div * div <= n {
+        while (n % div) == 0 {
+            factors.push(div);
+            n /= div;
+        }
+        div += 1;
+    }
+    if n > 1 {
+        factors.push(n);
+    }
+    factors
 }
