6557107676bbad014100e02816e715e9ce6e4405
aliyome <aliyome@gmail.com>
2023-08-20 08:16:12 +0900

    abc141 e TLE

--- abc141/src/bin/e.rs
+++ abc141/src/bin/e.rs
@@ -1,9 +1,21 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
+        s: Chars
     };
 
-    println!("{}", n);
+    // 10^6
+    let mut ans = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            let t = s[i..j].iter().collect::<String>();
+            if s[j..].iter().collect::<String>().contains(&t) {
+                ans = ans.max(t.len());
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
