ca3e7bd810eae4bb3205e13dbe1cad1b103f0944
aliyome <aliyome@gmail.com>
2023-08-20 23:36:19 +0900

    abc315 e

--- abc315/src/bin/e.rs
+++ abc315/src/bin/e.rs
@@ -3,6 +3,35 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+    }
 
+    let mut books = vec![vec![]];
+    for _ in 0..n {
+        input! {
+            c: usize,
+            p: [usize; c]
+        };
+        books.push(p);
+    }
+
+    let mut routes = vec![];
+    dfs(&books, 1, &mut routes, &mut vec![false; n + 1]);
+
+    for &v in &routes {
+        print!("{} ", v);
+    }
+}
+
+fn dfs(books: &Vec<Vec<usize>>, i: usize, routes: &mut Vec<usize>, visited: &mut Vec<bool>) {
+    if visited[i] {
+        return;
+    }
+    visited[i] = true;
+    for &j in books[i].iter() {
+        if visited[j] {
+            continue;
+        }
+        dfs(books, j, routes, visited);
+        routes.push(j);
+    }
 }
