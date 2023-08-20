72d2758803e17ff2caf510499dfa4aab332f94aa
aliyome <aliyome@gmail.com>
2023-08-20 22:38:43 +0900

    abc315 d mapを使って高速化

--- abc315/src/bin/d.rs
+++ abc315/src/bin/d.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeSet, HashSet};
+use std::collections::{BTreeSet, HashMap, HashSet};
 
 use proconio::{input, marker::Chars};
 
@@ -17,41 +17,65 @@ fn main() {
     for j in 0..w {
         cols.insert(j);
     }
+
+    let mut row_map = vec![vec![0usize; 26]; h];
+    let mut col_map = vec![vec![0usize; 26]; w];
+    for i in 0..h {
+        for j in 0..w {
+            row_map[i][c[i][j] as usize - 'a' as usize] += 1;
+            col_map[j][c[i][j] as usize - 'a' as usize] += 1;
+        }
+    }
     loop {
         let mut updated = false;
 
-        let mut cols_to_remove = HashSet::new();
-        for &j in cols.iter() {
-            let mut set = HashSet::new();
-            let mut count = 0usize;
-            for &i in rows.iter() {
-                set.insert(c[i][j]);
-                count += 1;
-            }
-            if set.len() == 1 && count > 1 {
-                updated = true;
-                cols_to_remove.insert(j);
-            }
-        }
-        for &j in cols_to_remove.iter() {
-            cols.remove(&j);
-        }
-
         let mut rows_to_remove = HashSet::new();
         for &i in rows.iter() {
-            let mut set = HashSet::new();
+            let mut letters = 0;
             let mut count = 0usize;
-            for &j in cols.iter() {
-                set.insert(c[i][j]);
-                count += 1;
+            let mut letter = 0usize;
+            for k in 0..26 {
+                if row_map[i][k] > 0 {
+                    letters += 1;
+                    count += row_map[i][k];
+                    letter = k;
+                }
             }
-            if set.len() == 1 && count > 1 {
+            if letters == 1 && count > 1 {
                 updated = true;
-                rows_to_remove.insert(i);
+                rows_to_remove.insert((i, letter));
             }
         }
-        for &i in rows_to_remove.iter() {
+
+        let mut cols_to_remove = HashSet::new();
+        for &j in cols.iter() {
+            let mut letters = 0;
+            let mut count = 0usize;
+            let mut letter = 0usize;
+            for k in 0..26 {
+                if col_map[j][k] > 0 {
+                    letters += 1;
+                    count += col_map[j][k];
+                    letter = k;
+                }
+            }
+            if letters == 1 && count > 1 {
+                updated = true;
+                cols_to_remove.insert((j, letter));
+            }
+        }
+
+        for &(i, k) in rows_to_remove.iter() {
             rows.remove(&i);
+            for &j in cols.iter() {
+                col_map[j][k] -= 1;
+            }
+        }
+        for &(j, k) in cols_to_remove.iter() {
+            cols.remove(&j);
+            for &i in rows.iter() {
+                row_map[i][k] -= 1;
+            }
         }
 
         if !updated {
