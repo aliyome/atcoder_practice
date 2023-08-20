e34e050a9db1be858e7ad28927d62c66a72b56a0
aliyome <aliyome@gmail.com>
2023-08-21 07:40:17 +0900

    abc002 b

---
+++ abc002/src/bin/b.rs
@@ -0,0 +1,13 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        w: Chars,
+    };
+
+    for i in 0..w.len() {
+        if w[i] != 'a' && w[i] != 'i' && w[i] != 'u' && w[i] != 'e' && w[i] != 'o' {
+            print!("{}", w[i]);
+        }
+    }
+}
