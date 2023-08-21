fc4409932d21bde6d4ac9be1ded196673acd3c80
aliyome <aliyome@gmail.com>
2023-08-22 08:04:19 +0900

    arc039 a

--- arc039/src/bin/a.rs
+++ arc039/src/bin/a.rs
@@ -6,23 +6,46 @@ fn main() {
         b: Chars,
     };
 
-    let mut aa = a.clone();
-    let mut bb = b.clone();
-    let mut aaa = a.clone();
-    let mut bbb = b.clone();
+    let mut aa = vec![];
+    let mut bb = vec![];
+    for i in 0..3 {
+        let mut a = a.clone();
+        let mut b = b.clone();
+        let alen = a.len();
+        let blen = b.len();
+        if alen > i {
+            a[i] = '9';
+            aa.push(a.iter().collect::<String>().parse::<i32>().unwrap());
+            if i == 0 {
+                a[i] = '1';
+                aa.push(a.iter().collect::<String>().parse::<i32>().unwrap());
+            } else {
+                a[i] = '0';
+                aa.push(a.iter().collect::<String>().parse::<i32>().unwrap());
+            }
+        }
 
-    aa[0] = '9';
-    bb[0] = '9';
-    aaa[0] = '1';
-    bbb[0] = '1';
+        if blen > i {
+            b[i] = '9';
+            bb.push(b.iter().collect::<String>().parse::<i32>().unwrap());
+            if i == 0 {
+                b[i] = '1';
+                bb.push(b.iter().collect::<String>().parse::<i32>().unwrap());
+            } else {
+                b[i] = '0';
+                bb.push(b.iter().collect::<String>().parse::<i32>().unwrap());
+            }
+        }
+    }
 
     let a = a.iter().collect::<String>().parse::<i32>().unwrap();
-    let aa = aa.iter().collect::<String>().parse::<i32>().unwrap();
-    let aaa = aaa.iter().collect::<String>().parse::<i32>().unwrap();
     let b = b.iter().collect::<String>().parse::<i32>().unwrap();
-    let bb = bb.iter().collect::<String>().parse::<i32>().unwrap();
-    let bbb = bbb.iter().collect::<String>().parse::<i32>().unwrap();
-
-    let ans = (a - b).max(a - bb).max(a - bbb).max(aa - b).max(aaa - b);
+    let mut ans = -10000;
+    for b in bb {
+        ans = ans.max(a - b);
+    }
+    for a in aa {
+        ans = ans.max(a - b);
+    }
     println!("{}", ans);
 }
