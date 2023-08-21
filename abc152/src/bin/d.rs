6af61fa01006ce4fbd131787428170eb59bc7ffb
aliyome <aliyome@gmail.com>
2023-08-21 22:37:01 +0900

    abc152 d

--- abc152/src/bin/d.rs
+++ abc152/src/bin/d.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
@@ -5,5 +8,21 @@ fn main() {
         n: usize,
     };
 
-    println!("{}", n);
+    let mut map = HashMap::new();
+    for i in 1..=n {
+        let s = i.to_string().chars().collect_vec();
+        *map.entry((*s.first().unwrap(), *s.last().unwrap()))
+            .or_insert(0) += 1;
+    }
+
+    let mut ans = 0;
+    for (k, v) in map.iter() {
+        for (kk, vv) in map.iter() {
+            if k.0 == kk.1 && k.1 == kk.0 {
+                ans += v * vv;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
