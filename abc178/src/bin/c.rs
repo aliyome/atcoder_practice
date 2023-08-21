2e92aff05bdb3d27f3678520542f41ae0796fb47
aliyome <aliyome@gmail.com>
2023-08-21 22:27:10 +0900

    abc178 c

--- abc178/src/bin/c.rs
+++ abc178/src/bin/c.rs
@@ -1,9 +1,41 @@
 use proconio::input;
 
+const MOD: usize = 1000000007;
+
 fn main() {
     input! {
         n: usize,
     };
 
-    println!("{}", n);
+    if n == 1 {
+        println!("0");
+        return;
+    }
+
+    // 全事象 10^n
+    let mut total = 1;
+    for _ in 0..n {
+        total *= 10;
+        total %= MOD;
+    }
+
+    let mut x = 1;
+    for _ in 0..n {
+        x *= 8;
+        x %= MOD;
+    }
+
+    let mut y = 1;
+    for _ in 0..n {
+        y *= 9;
+        y %= MOD;
+    }
+    y *= 2;
+    y %= MOD;
+
+    let mut ans = total;
+    ans = (MOD + ans - y) % MOD;
+    ans = (MOD + ans + x) % MOD;
+
+    println!("{}", ans);
 }
