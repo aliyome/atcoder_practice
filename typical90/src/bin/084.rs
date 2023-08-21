da0336c80cd8bfa9ea0a38c6d76b70c7338f5b7f
aliyome <aliyome@gmail.com>
2023-08-21 14:26:43 +0900

    typical90 084 余事象の数え上げ

--- typical90/src/bin/084.rs
+++ typical90/src/bin/084.rs
@@ -1,3 +1,28 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars
+    }
+
+    let mut rl = vec![];
+    rl.push((s[0], 1));
+    for i in 1..n {
+        if s[i] == s[i - 1] {
+            rl.last_mut().unwrap().1 += 1;
+        } else {
+            rl.push((s[i], 1));
+        }
+    }
+
+    let total = n * (n + 1) / 2;
+
+    // 余事象の数え上げ
+    let mut x = 0;
+    for (_, l) in rl {
+        x += l * (l + 1) / 2;
+    }
+
+    println!("{}", total - x);
 }
