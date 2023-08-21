1e4b925344b40f95efeb61ba7f24f8e979ea311d
aliyome <aliyome@gmail.com>
2023-08-21 11:47:28 +0900

    typical90 079

--- typical90/src/bin/079.rs
+++ typical90/src/bin/079.rs
@@ -1,3 +1,33 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        mut a: [[isize; w]; h],
+        b: [[isize; w]; h]
+    }
+
+    let mut ans = 0;
+    for i in 0..h - 1 {
+        for j in 0..w - 1 {
+            let diff = b[i][j] - a[i][j];
+            if diff == 0 {
+                continue;
+            }
+            for ii in 0..2 {
+                for jj in 0..2 {
+                    a[i + ii][j + jj] += diff;
+                }
+            }
+            ans += diff.abs();
+        }
+    }
+
+    if a == b {
+        println!("Yes");
+        println!("{}", ans);
+    } else {
+        println!("No");
+    }
 }
