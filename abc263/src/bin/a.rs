414ae0bffa465bac0747620dabf5981a257c739d
aliyome <aliyome@gmail.com>
2023-08-23 07:36:54 +0900

    abc263 a

--- abc263/src/bin/a.rs
+++ abc263/src/bin/a.rs
@@ -1,9 +1,36 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
+        e: usize,
     };
 
-    println!("{}", n);
+    let mut map = HashMap::new();
+    *map.entry(a).or_insert(0) += 1;
+    *map.entry(b).or_insert(0) += 1;
+    *map.entry(c).or_insert(0) += 1;
+    *map.entry(d).or_insert(0) += 1;
+    *map.entry(e).or_insert(0) += 1;
+
+    let mut n2 = false;
+    let mut n3 = false;
+    for (_, &v) in map.iter() {
+        if v == 2 {
+            n2 = true;
+        }
+        if v == 3 {
+            n3 = true;
+        }
+    }
+    if n2 && n3 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
