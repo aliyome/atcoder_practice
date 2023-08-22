35875c2a560435cd35219254e22aed63603e4488
aliyome <aliyome@gmail.com>
2023-08-23 08:00:11 +0900

    panasonic2020 b fixed

--- panasonic2020/src/bin/b.rs
+++ panasonic2020/src/bin/b.rs
@@ -2,19 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        h: usize,
-        w: usize,
+        h: u128,
+        w: u128,
     };
+    if h == 1 || w == 1 {
+        println!("1");
+        return;
+    }
 
-    // 2n行の列数
-    let c = if w % 2 == 0 { w / 2 } else { w / 2 + 1 };
-    // 2n+1行の列数
-    let d = w / 2;
-
-    // 2nの行数
-    let a = if h % 2 == 0 { h / 2 } else { h / 2 + 1 };
-    // 2n+1の行数
-    let b = h / 2;
-
-    println!("{}", a * c + b * d);
+    if (h * w) % 2 != 0 {
+        println!("{}", (h * w) / 2 + 1);
+    } else {
+        println!("{}", (h * w) / 2);
+    }
 }
