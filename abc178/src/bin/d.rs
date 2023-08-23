08b2c80d16c6bb1382a85ac22e88755312f27a5f
aliyome <aliyome@gmail.com>
2023-08-24 08:30:02 +0900

    abc178 d

--- abc178/src/bin/d.rs
+++ abc178/src/bin/d.rs
@@ -1,24 +1,28 @@
 use proconio::input;
 
-const MOD: usize = 10usize.pow(9) + 7;
+const MOD: usize = 1000000007;
 
 fn main() {
     input! {
         s: usize,
     };
 
-    let mut dp = vec![0; s + 1];
-    dp[0] = 1;
-    for i in 1..=s {
-        if i < 3 {
-            continue;
+    // dp[i] := 各桁に 3 以上の数字をつかって総和が i になるような数列の個数
+    let mut dp = vec![0; 2001];
+    dp[0] = 0;
+
+    for i in 0..=s {
+        if i >= 3 {
+            dp[i] = 1;
         }
-        for j in 0..=(i - 3) {
-            dp[i] += dp[j];
+        for j in 3..=s {
+            if i < j {
+                break;
+            }
+            dp[i] += dp[i - j];
             dp[i] %= MOD;
         }
     }
 
-    println!("{:?}", dp);
     println!("{}", dp[s]);
 }
