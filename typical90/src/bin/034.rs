22e323daa883ebba0e5b8e64eed704a88623d286
aliyome <aliyome@gmail.com>
2023-08-23 11:49:42 +0900

    typical90 034

--- typical90/src/bin/034.rs
+++ typical90/src/bin/034.rs
@@ -1,5 +1,6 @@
+use std::collections::{HashMap, VecDeque};
+
 use proconio::input;
-use std::collections::HashMap;
 
 fn main() {
     input! {
@@ -8,28 +9,24 @@ fn main() {
         a: [usize; n]
     }
 
-    let mut count = HashMap::new();
+    let mut map = HashMap::new();
+    let mut q = VecDeque::new();
     let mut ans = 0;
-    let mut l = 0;
-    for r in 0..n {
-        *count.entry(a[r]).or_insert(0) += 1;
 
-        // k 種類以内の場合
-        if count.len() <= k {
-            ans = ans.max(r - l + 1);
-            continue;
-        }
-
-        // k 種類を超えた場合
-        // 一番古いものを削除して、文字種が変わるまで l を進める
-        while count.len() > k {
-            count.entry(a[l]).and_modify(|e| *e -= 1);
-            if count[&a[l]] == 0 {
-                count.remove(&a[l]);
+    for &a in &a {
+        *map.entry(a).or_insert(0) += 1;
+        q.push_back(a);
+        if map.len() <= k {
+            ans = ans.max(q.len());
+        } else {
+            while let Some(x) = q.pop_front() {
+                map.entry(x).and_modify(|f| *f -= 1);
+                if map[&x] == 0 {
+                    map.remove(&x);
+                    break;
+                }
             }
-            l += 1;
         }
-        ans = ans.max(r - l + 1);
     }
 
     println!("{}", ans);
