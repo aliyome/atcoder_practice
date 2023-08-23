d9281f62c57491daafba4247944ccdea99e3080d
aliyome <aliyome@gmail.com>
2023-08-24 07:55:59 +0900

    abc276 c

--- abc276/src/bin/c.rs
+++ abc276/src/bin/c.rs
@@ -1,45 +1,47 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        mut p: [isize; n]
+        p: [usize; n]
     };
 
-    p.insert(0, -1);
-    p.push(10isize.pow(9));
+    let mut max = p[n - 1];
+    let mut max_i = n - 1;
+    for i in (0..n - 1).rev() {
+        if p[i] > p[i + 1] {
+            max = p[i];
+            max_i = i;
+            break;
+        }
+    }
 
-    for i in (0..n).rev() {
-        if p[i] < p[i + 1] {
-            continue;
+    let mut c = 0;
+    for i in max_i + 1..n {
+        if p[i] < p[max_i] {
+            c = c.max(p[i]);
         }
-        let mut x = 0;
-        let mut pp = p[i..p.len() - 1].to_vec();
-        pp.sort();
-        pp.reverse();
-        for &pp in pp.iter() {
-            if pp < p[i] {
-                x = pp;
-                break;
-            }
-        }
-        let mut q = p[..i].to_vec();
-        q.push(x);
+    }
 
-        for j in 0..pp.len() {
-            if pp[j] == x {
-                continue;
-            }
-            q.push(pp[j]);
+    let mut front = vec![];
+    front.extend(p[..max_i].iter());
+    front.push(c);
+
+    let mut back = vec![];
+    back.push(max);
+    for &e in &p[max_i + 1..] {
+        if e != c {
+            back.push(e);
         }
-        println!(
-            "{}",
-            q[1..q.len()]
-                .iter()
-                .map(|&x| x.to_string())
-                .collect::<Vec<String>>()
-                .join(" ")
-        );
-        return;
+    }
+    back.sort();
+    back.reverse();
+
+    for &e in &front {
+        print!("{} ", e);
+    }
+    for &e in &back {
+        print!("{} ", e);
     }
 }
