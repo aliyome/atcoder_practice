afffd407c123e1579d92304950c73f0e0cdff1ac
aliyome <aliyome@gmail.com>
2023-08-23 22:58:24 +0900

    abc289 d DP

--- abc289/src/bin/d.rs
+++ abc289/src/bin/d.rs
@@ -1,9 +1,38 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        a: [usize; n],
+        m: usize,
+        b: [usize; m],
+        x: usize
     };
 
-    println!("{}", n);
+    let mut mochi = HashSet::new();
+    for &b in &b {
+        mochi.insert(b);
+    }
+
+    let mut dp = vec![false; x + 1];
+    dp[0] = true;
+
+    for i in 0..x {
+        for j in 0..n {
+            if !dp[i] {
+                continue;
+            }
+            if i + a[j] <= x && !mochi.contains(&(i + a[j])) {
+                dp[i + a[j]] = true;
+            }
+        }
+    }
+
+    if dp[x] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
