9dbd143a3014b6692550b9c1b48dfef6332cf045
aliyome <aliyome@gmail.com>
2023-08-23 20:08:25 +0900

    abc245 c DP

--- abc245/src/bin/c.rs
+++ abc245/src/bin/c.rs
@@ -8,19 +8,28 @@ fn main() {
         b: [isize; n]
     };
 
-    // 条件
-    let is_ok = |x0: isize, x1: isize| (x0 - x1).abs() <= k;
-
-    // dp[i][j] = i 番目にj(0:a, 1:b)を選んだときに条件を満たしているか
-    let mut dp = vec![vec![false; 2]; n + 1];
+    // dp[i][j] := i番目まで見た時に j が成立するか (j = 0:a, 1:b)
+    let mut dp = vec![vec![false; 2]; n];
     dp[0][0] = true;
     dp[0][1] = true;
 
     for i in 1..n {
-        dp[i][0] =
-            (dp[i - 1][0] && is_ok(a[i - 1], a[i])) || (dp[i - 1][1] && is_ok(b[i - 1], a[i]));
-        dp[i][1] =
-            (dp[i - 1][0] && is_ok(a[i - 1], b[i])) || (dp[i - 1][1] && is_ok(b[i - 1], b[i]));
+        if dp[i - 1][0] {
+            if (a[i] - a[i - 1]).abs() <= k {
+                dp[i][0] |= true;
+            }
+            if (b[i] - a[i - 1]).abs() <= k {
+                dp[i][1] |= true;
+            }
+        }
+        if dp[i - 1][1] {
+            if (a[i] - b[i - 1]).abs() <= k {
+                dp[i][0] |= true;
+            }
+            if (b[i] - b[i - 1]).abs() <= k {
+                dp[i][1] |= true;
+            }
+        }
     }
 
     if dp[n - 1][0] || dp[n - 1][1] {
