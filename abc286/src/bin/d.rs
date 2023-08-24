e71be53969174109d4139bfa48867914532e9f34
aliyome <aliyome@gmail.com>
2023-08-24 14:46:22 +0900

    abc286 d DP

--- abc286/src/bin/d.rs
+++ abc286/src/bin/d.rs
@@ -1,55 +1,46 @@
-use std::cmp::Reverse;
-
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
         x: usize,
-        mut ab: [(usize, usize); n],
+        ab: [(usize, usize); n],
     }
 
-    // コインを小さい順にソート
-    ab.sort_by_key(|&(_, b)| Reverse(b));
-
-    // コインを枚数分展開する
+    // 全コインを一つの配列にまとめる
     let mut coins = vec![];
-    for (a, b) in ab {
+    let mut all_coins = 0;
+    for &(a, b) in &ab {
         for _ in 0..b {
-            coins.push((a, b));
+            coins.push(a);
         }
+        all_coins += b;
     }
 
-    // DP
-    // dp[i][j]: i番目までのコインを使ってj円を作れるかどうか
-    let mut dp = vec![vec![false; x + 1]; coins.len() + 1];
+    // dp[i][j] := i枚のコインを使ってj円の支払いを支払うことができるか
+    let mut dp = vec![vec![false; x + 1]; all_coins + 1];
     dp[0][0] = true;
 
-    // i番目のコインを使うかどうか
-    for i in 0..coins.len() {
-        // j 円を作れるか
+    for i in 0..all_coins {
+        dp[i + 1] = dp[i].clone();
         for j in 0..=x {
-            // 一つ前の状態で作れない場合はi番目のコインを使っても作れない。
-            // ※i番目のコインはi番目より前のコインより金額が大きいため
             if !dp[i][j] {
                 continue;
             }
-            // i番目のコインを使わない場合はi-1番目までのコインを使ってj円を作れるかどうかと同じ結果
-            dp[i + 1][j] = dp[i][j];
-
-            // i番目のコインを使う場合
-            if j + coins[i].0 > x {
-                // X円を超えるなら使えない
+            if j + coins[i] > x {
                 continue;
             }
-            // j + coins[i].0 円を作れる
-            dp[i + 1][j + coins[i].0] = true;
+            dp[i + 1][j + coins[i]] = true;
         }
     }
 
-    if dp[coins.len()][x] {
-        println!("Yes");
-    } else {
-        println!("No");
+    for i in 0..=all_coins {
+        if dp[i][x] {
+            println!("Yes");
+            return;
+        }
+        // println!("{:?}", dp[i]);
     }
+
+    println!("No");
 }
