6c27dcc387bf10428d22ce0b28712b3c1af780f6
aliyome <aliyome@gmail.com>
2023-08-25 00:09:41 +0900

    abc129 c DP

--- abc129/src/bin/c.rs
+++ abc129/src/bin/c.rs
@@ -1,9 +1,34 @@
+use std::collections::HashSet;
+
 use proconio::input;
+
+const MOD: usize = 1_000_000_007;
 
 fn main() {
     input! {
         n: usize,
+        m: usize,
+        a: [usize; m]
     };
 
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for &a in &a {
+        set.insert(a);
+    }
+
+    let mut dp = vec![0; n + 5];
+    dp[0] = 1;
+
+    for i in 0..n {
+        if !set.contains(&(i + 1)) {
+            dp[i + 1] += dp[i];
+            dp[i + 1] %= MOD;
+        }
+        if !set.contains(&(i + 2)) {
+            dp[i + 2] += dp[i];
+            dp[i + 2] %= MOD;
+        }
+    }
+
+    println!("{}", dp[n]);
 }
