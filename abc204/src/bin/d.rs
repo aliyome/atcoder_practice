584a93afd8422f586218bad9960b787ad858dea1
aliyome <aliyome@gmail.com>
2023-08-25 00:13:53 +0900

    abc204 d 貪欲 WA

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -3,7 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        mut t: [usize; n]
     };
 
-    println!("{}", n);
+    t.sort();
+    t.reverse();
+
+    // 貪欲で解ける？
+    let mut dp = vec![0; 2];
+    for i in 0..n {
+        if dp[0] < dp[1] {
+            dp[0] += t[i];
+        } else {
+            dp[1] += t[i];
+        }
+    }
+
+    println!("{}", dp.iter().max().unwrap());
 }
