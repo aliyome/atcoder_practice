5b793cde1d9d5723c3222b66bbb00f7368ca7a45
aliyome <aliyome@gmail.com>
2023-08-25 08:52:10 +0900

    abc180 d

--- abc180/src/bin/d.rs
+++ abc180/src/bin/d.rs
@@ -2,8 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut x: u128,
+        y: u128,
+        a: u128,
+        b: u128,
     };
 
-    println!("{}", n);
+    let mut exp = 0;
+
+    loop {
+        let xx = x * a;
+        if xx > x + b {
+            break;
+        }
+        if xx >= y {
+            break;
+        }
+
+        exp += 1;
+        x = xx;
+    }
+
+    exp += (y - x - 1) / b;
+
+    println!("{}", exp);
 }
