484100646c0438d798a38b36d5663c8e39deae2e
aliyome <aliyome@gmail.com>
2023-08-26 08:04:23 +0900

    abc100 c

--- abc100/src/bin/c.rs
+++ abc100/src/bin/c.rs
@@ -1,9 +1,27 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        a: [usize; n]
     };
 
-    println!("{}", n);
+    let mut heap = BinaryHeap::new();
+    for &a in &a {
+        heap.push(a);
+    }
+
+    let mut ans = 0;
+    while let Some(x) = heap.pop() {
+        if x % 2 == 1 {
+            continue;
+        }
+
+        ans += 1;
+        heap.push(x / 2);
+    }
+
+    println!("{}", ans);
 }
