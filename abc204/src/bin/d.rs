651b0f90986c72db4d3585a50ccc6faeb17e3871
aliyome <aliyome@gmail.com>
2023-08-25 13:03:50 +0900

    abc204 d

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -3,21 +3,34 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut t: [usize; n]
+        t: [usize; n]
     };
 
-    t.sort();
-    t.reverse();
+    let total = t.iter().sum::<usize>();
 
-    // 貪欲で解ける？
-    let mut dp = vec![0; 2];
+    // dp[i][j] := i品目までを使ってj分で全部の料理を作れるか
+    let mut dp = vec![vec![0; 100_001]; n + 1];
+    dp[0][0] = 1;
     for i in 0..n {
-        if dp[0] < dp[1] {
-            dp[0] += t[i];
-        } else {
-            dp[1] += t[i];
+        dp[i + 1] = dp[i].clone();
+        let t = t[i];
+        for j in 0..100_000 {
+            if dp[i][j] == 0 {
+                continue;
+            }
+            // i品目までを何分で作れるか
+            dp[i + 1][j + t] = 1;
         }
     }
 
-    println!("{}", dp.iter().max().unwrap());
+    let mut ans = std::usize::MAX;
+    for a in 0..=100_000 {
+        if dp[n][a] == 1 {
+            let b = total - a;
+            let duration = a.max(b);
+            ans = ans.min(duration);
+        }
+    }
+
+    println!("{}", ans);
 }
