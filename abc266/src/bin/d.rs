b48d0ef112d49487951c97b942d4df376705457a
aliyome <aliyome@gmail.com>
2023-08-25 14:14:10 +0900

    abc266 d

--- abc266/src/bin/d.rs
+++ abc266/src/bin/d.rs
@@ -4,34 +4,35 @@ use std::collections::HashMap;
 fn main() {
     input! {
         n: usize,
-        txa: [(usize, usize, usize); n]
-    }
-    // t をキーとするハッシュにする
-    let mut data: HashMap<usize, (usize, usize)> = HashMap::new();
-    for i in 0..n {
-        data.insert(txa[i].0, (txa[i].1, txa[i].2));
+        txa: [(usize, usize, isize); n]
     }
 
-    let mut dp = vec![vec![0; 5]; 10usize.pow(5) + 10];
-    for t in (0..=10usize.pow(5)).rev() {
-        let mut snuke_x = 0;
-        let mut snuke_a = 0;
-        if data.contains_key(&t) {
-            snuke_x = data.get(&t).unwrap().0;
-            snuke_a = data.get(&t).unwrap().1;
+    let max_t = txa.iter().map(|&(t, _, _)| t).max().unwrap();
+
+    let mut map = HashMap::new();
+    for &(t, x, a) in &txa {
+        map.insert(t, (x, a));
+    }
+
+    // dp[t][j] := 時刻tにおいて、穴jに居るときの最大スコア
+    let mut dp = vec![vec![-10isize.pow(18); 5]; 100_001];
+    dp[0][0] = 0;
+
+    // もらうDP
+    for t in 1..=100_000 {
+        for j in 0..5 {
+            if j > 0 {
+                dp[t][j] = dp[t][j].max(dp[t - 1][j - 1]);
+            }
+            dp[t][j] = dp[t][j].max(dp[t - 1][j]);
+            if j < 4 {
+                dp[t][j] = dp[t][j].max(dp[t - 1][j + 1]);
+            }
         }
-        for x in 0..5 {
-            if x == 0 {
-                dp[t][x] = dp[t + 1][x].max(dp[t + 1][x + 1]);
-            } else if x == 4 {
-                dp[t][x] = dp[t + 1][x].max(dp[t + 1][x - 1]);
-            } else {
-                dp[t][x] = dp[t + 1][x].max(dp[t + 1][x - 1]).max(dp[t + 1][x + 1]);
-            }
-            if x == snuke_x {
-                dp[t][x] += snuke_a;
-            }
+        if let Some(&(x, a)) = map.get(&t) {
+            dp[t][x] += a;
         }
     }
-    println!("{}", dp[0][0]);
+
+    println!("{}", dp[100_000].iter().max().unwrap());
 }
