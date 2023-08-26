020a2356eae3263b9c5838490f8919866ac2af49
aliyome <aliyome@gmail.com>
2023-08-26 23:52:20 +0900

    abc317 a

--- abc317/src/bin/a.rs
+++ abc317/src/bin/a.rs
@@ -5,23 +5,13 @@ fn main() {
         n: usize,
         h: i32,
         x: i32,
-        potions: [i32; n]
+        p: [i32; n]
     }
 
-    let target = x - h;
-
-    // バイナリサーチで目標値以上のポーションを探す
-    let mut low = 0;
-    let mut high = n;
-    while low < high {
-        let mid = (low + high) / 2;
-        if potions[mid] < target {
-            low = mid + 1;
-        } else {
-            high = mid;
+    for i in 0..n {
+        if h + p[i] >= x {
+            println!("{}", i + 1);
+            return;
         }
     }
-
-    // 1-based indexなので+1して出力
-    println!("{}", low + 1);
 }
