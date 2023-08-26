61cacef78aa7d7c58f36a00d5f3ea1821b217aae
aliyome <aliyome@gmail.com>
2023-08-27 00:48:24 +0900

    abc317 c dfs

--- abc317/src/bin/c.rs
+++ abc317/src/bin/c.rs
@@ -1,52 +1,56 @@
-use proconio::input;
 use std::collections::HashMap;
 
-fn dfs(
-    current: usize,
-    visited: &mut Vec<bool>,
-    roads: &HashMap<usize, Vec<(usize, usize)>>,
-    current_distance: usize,
-) -> usize {
-    visited[current] = true;
-
-    let mut max_distance = current_distance;
-
-    if let Some(neighbors) = roads.get(&current) {
-        for &(next, distance) in neighbors {
-            if !visited[next] {
-                max_distance = std::cmp::max(
-                    max_distance,
-                    dfs(next, visited, roads, current_distance + distance),
-                );
-            }
-        }
-    }
-
-    visited[current] = false;
-
-    max_distance
-}
+use proconio::input;
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        roads: [(usize, usize, usize); m]
+        abc: [(usize, usize, usize); m]
     }
 
-    let mut road_map: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
-
-    for (a, b, c) in roads {
-        road_map.entry(a).or_insert(vec![]).push((b, c));
-        road_map.entry(b).or_insert(vec![]).push((a, c));
+    let mut edge = HashMap::new();
+    for &(a, b, c) in &abc {
+        edge.entry(a).or_insert(vec![]).push((b, c));
+        edge.entry(b).or_insert(vec![]).push((a, c));
     }
 
-    let mut visited = vec![false; n + 1];
-    let mut max_distance = 0;
-
+    let mut dist = vec![vec![0; n + 1]; n + 1];
     for i in 1..=n {
-        max_distance = std::cmp::max(max_distance, dfs(i, &mut visited, &road_map, 0));
+        let mut visited = vec![false; n + 1];
+        visited[0] = true;
+        visited[i] = true;
+        dfs(i, i, 0, &edge, &mut dist, &mut visited);
     }
 
-    println!("{}", max_distance);
+    let mut ans = 0;
+    for i in 1..=n {
+        for j in 1..=n {
+            ans = ans.max(dist[i][j]);
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn dfs(
+    start: usize,
+    i: usize,
+    sum: usize,
+    edge: &HashMap<usize, Vec<(usize, usize)>>,
+    dist: &mut Vec<Vec<usize>>,
+    visited: &mut Vec<bool>,
+) {
+    dist[start][i] = dist[start][i].max(sum);
+
+    if let Some(list) = edge.get(&i) {
+        for &(j, c) in list {
+            if visited[j] {
+                continue;
+            }
+            visited[j] = true;
+            dfs(start, j, sum + c, edge, dist, visited);
+            visited[j] = false;
+        }
+    }
 }
