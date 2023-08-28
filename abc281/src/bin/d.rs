323935f49afa2a72a82b84de415205a17ccbca00
aliyome <aliyome@gmail.com>
2023-08-28 14:18:30 +0900

    abc281 d

--- abc281/src/bin/d.rs
+++ abc281/src/bin/d.rs
@@ -5,34 +5,34 @@ fn main() {
         n: usize,
         k: usize,
         d: usize,
-        // a は非負の数
-        mut a: [isize; n]
+        a: [isize; n]
     };
-    // 1-indexed
-    a.insert(0, 0);
 
-    // dp[x][y][z] := x番目までの数をy個選んで、その和をdで割った余りがzであるような選び方をしたとき、その和の最大値
-    let mut dp = vec![vec![vec![-1isize; d]; k + 2]; n + 1];
+    // dp[i][j][x] := i番目まで見てj個選んだ時にdで割ったあまりがxであるような最大値
+    let mut dp = vec![vec![vec![std::isize::MIN; d]; k + 2]; n + 1];
     dp[0][0][0] = 0;
 
-    // 配るDP
-    for x in 0..n {
-        for y in 0..=k {
-            for z in 0..d {
-                if dp[x][y][z] == -1 {
-                    // 配り元が無い場合
-                    continue;
-                }
-                // a[x + 1] を選ばないとき
-                dp[x + 1][y][z] = dp[x + 1][y][z].max(dp[x][y][z]);
-                // a[x + 1] を選ぶとき
-                let z_new = (z + a[x + 1] as usize) % d;
-                dp[x + 1][y + 1][z_new] = dp[x + 1][y + 1][z_new].max(dp[x][y][z] + a[x + 1]);
+    // i まで見て
+    for i in 0..n {
+        let a = a[i];
+        // すでに j 個選んだ時に
+        for j in 0..=k {
+            // d で割ったあまりが x であるような
+            for x in 0..d {
+                // a[i] を選ばない場合
+                dp[i + 1][j][x] = dp[i + 1][j][x].max(dp[i][j][x]);
+                // a[i] を選ぶ場合
+                let sum = dp[i][j][x] + a;
+                let y = sum as usize % d;
+                dp[i + 1][j + 1][y] = dp[i + 1][j + 1][y].max(sum);
             }
         }
     }
 
-    let ans = dp[n][k][0];
+    if dp[n][k][0] < 0 {
+        println!("-1");
+        return;
+    }
 
-    println!("{}", ans);
+    println!("{}", dp[n][k][0]);
 }
