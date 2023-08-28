722d5ecb49b9c24125703c669053da85f6c911ea
aliyome <aliyome@gmail.com>
2023-08-29 07:47:33 +0900

    abc047 c

--- abc047/src/bin/c.rs
+++ abc047/src/bin/c.rs
@@ -1,9 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     };
 
-    println!("{}", n);
+    let mut rl = vec![s[0]];
+    let mut prev = s[0];
+    for i in 1..s.len() {
+        if prev != s[i] {
+            rl.push(s[i]);
+            prev = s[i];
+        }
+    }
+
+    println!("{}", rl.len() - 1);
 }
