6feb97efb16cf54201ea2a5af5886b9271e02772
aliyome <aliyome@gmail.com>
2023-08-30 08:25:55 +0900

    abc217 d

--- abc217/src/bin/d.rs
+++ abc217/src/bin/d.rs
@@ -1,8 +1,25 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        l: usize,
+        q: usize,
+        cx: [(usize, usize); q]
     };
 
+    let mut set = BTreeSet::new();
+    set.insert(0);
+    set.insert(l);
+
+    for &(c, x) in &cx {
+        if c == 1 {
+            set.insert(x);
+        } else {
+            let min = set.range(..x).next_back().unwrap();
+            let max = set.range(x..).next().unwrap();
+            println!("{}", max - min);
+        }
+    }
 }
