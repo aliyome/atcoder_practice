d75f36381a4bd98bc38ffc70ac0398b7c6979e72
aliyome <aliyome@gmail.com>
2023-08-31 07:34:44 +0900

    abc166 b

--- abc166/src/bin/b.rs
+++ abc166/src/bin/b.rs
@@ -1,9 +1,26 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        k: usize,
     };
 
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 1..=n {
+        set.insert(i);
+    }
+    for _ in 0..k {
+        input! {
+            d: usize,
+            a: [usize; d]
+        }
+
+        for &a in &a {
+            set.remove(&a);
+        }
+    }
+    println!("{}", set.len());
 }
