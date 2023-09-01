3a9cc3ca2d6c1d899ec6eb5875c650307339cc97
aliyome <aliyome@gmail.com>
2023-09-02 07:34:23 +0900

    abc152 b

--- abc152/src/bin/b.rs
+++ abc152/src/bin/b.rs
@@ -2,8 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        a: usize,
+        b: usize,
     };
 
-    println!("{}", n);
+    let mut aa = vec![];
+    let mut bb = vec![];
+    for _ in 0..b {
+        aa.push(a);
+    }
+    for _ in 0..a {
+        bb.push(b);
+    }
+
+    if aa < bb {
+        println!("{}", aa.iter().map(|x| x.to_string()).collect::<String>());
+    } else {
+        println!("{}", bb.iter().map(|x| x.to_string()).collect::<String>());
+    }
 }
