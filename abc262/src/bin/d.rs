92965c8a41c223b1b282817e0d597e0594ea52ec
aliyome <aliyome@gmail.com>
2023-09-05 13:31:29 +0900

    abc262 d TLE

--- abc262/src/bin/d.rs
+++ abc262/src/bin/d.rs
@@ -1,6 +1,6 @@
 use proconio::input;
 
-const MOD: usize = 998244353;
+const MOD: isize = 998244353;
 
 fn main() {
     input! {
@@ -8,24 +8,48 @@ fn main() {
         a: [usize; n]
     };
 
+    // dp[i][j][k] := ai まで見て、 j 個選んで、和が k になる場合の数 -> k が取りうる範囲は 10^9 x 100 なので出来ない
+    // ↓
+    // TODO: dp[i][j][k] := ai まで見て、 j 個選んで、 和を [1,n] で割った時のあまりの数が k となる場合の数 -> k が取りうる範囲は 100 なのでできる
+    let mut dp = vec![vec![vec![0; 30]; n + 1]; n + 1];
+    // 初期値
+    // 0個選んで和が0になる場合の数は1
+    dp[0][0][0] = 1;
+
+    // aiまで見て
+    for i in 0..n {
+        // aiを選ばない場合は dp[i+1] と dp[i] は同じ
+        dp[i + 1] = dp[i].clone();
+
+        // j個選んで
+        for j in 0..n {
+            // その和が k になる時
+            for k in 0..30 {
+                // 場合の数が 0 の場合はスキップ
+                if dp[i][j][k] == 0 {
+                    continue;
+                }
+
+                // aiを選ぶ場合
+                let nk = k + a[i];
+                dp[i + 1][j + 1][nk] += dp[i][j][k];
+                dp[i + 1][j + 1][nk] %= MOD;
+            }
+        }
+    }
+
     let mut ans = 0;
-    dfs(0, n, 0, 0, &a, &mut ans);
+    // an まで見たあと
+    // j個選んで
+    for j in 1..=n {
+        // 和が k のとき
+        for k in 1..30 {
+            // k を j で割り切れる場合の数を合計する
+            if k % j == 0 {
+                ans += dp[n][j][k];
+                ans %= MOD;
+            }
+        }
+    }
     println!("{}", ans);
-}
-
-fn dfs(i: usize, n: usize, sum: usize, chosen: usize, a: &[usize], ans: &mut usize) {
-    if i == n {
-        return;
-    }
-
-    // 1. 選ばない
-    dfs(i + 1, n, sum, chosen, a, ans);
-
-    // 2. 選ぶ
-    let sum = (sum + a[i]) % MOD;
-    let chosen = chosen + 1;
-    if sum % chosen == 0 {
-        *ans += 1;
-    }
-    dfs(i + 1, n, sum, chosen, a, ans);
 }
