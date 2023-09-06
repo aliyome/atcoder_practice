4db8ad4edce27630884d72d6a2034c120183df6b
aliyome <aliyome@gmail.com>
2023-09-06 14:19:49 +0900

    typical90 063 全探索

--- typical90/src/bin/063.rs
+++ typical90/src/bin/063.rs
@@ -1,3 +1,42 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize, w: usize,
+        p: [[usize; w]; h]
+    }
+
+    let mut ans = 0;
+    for bit in 1..1 << h {
+        let mut rows = vec![];
+        for i in 0..h {
+            if bit & 1 << i == 0 {
+                continue;
+            }
+            rows.push(i);
+        }
+
+        let mut map = HashMap::new();
+        for j in 0..w {
+            let mut col_map = HashMap::new();
+            for &i in &rows {
+                *col_map.entry(p[i][j]).or_insert(0) += 1;
+            }
+            if col_map.keys().len() > 1 {
+                continue;
+            }
+            *map.entry(p[rows[0]][j]).or_insert(0) += 1;
+        }
+
+        let mut max_cols = 0;
+        for (_, v) in map.iter() {
+            max_cols = max_cols.max(*v);
+        }
+
+        ans = ans.max(rows.len() * max_cols);
+    }
+
+    println!("{}", ans);
 }
