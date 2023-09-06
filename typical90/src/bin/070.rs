1d63c3d5f27095c09b5a23dafdd02fb3732c4947
aliyome <aliyome@gmail.com>
2023-09-06 14:47:17 +0900

    typical90 070

--- typical90/src/bin/070.rs
+++ typical90/src/bin/070.rs
@@ -1,3 +1,19 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xy: [(isize, isize); n]
+    }
+
+    let xa = xy.iter().map(|&(x, _)| x).sorted().collect_vec()[n / 2];
+    let ya = xy.iter().map(|&(_, y)| y).sorted().collect_vec()[n / 2];
+
+    let mut ans = 0;
+    for &(x, y) in &xy {
+        ans += (x - xa).abs() + (y - ya).abs();
+    }
+
+    println!("{}", ans);
 }
