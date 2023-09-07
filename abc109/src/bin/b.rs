9af17f031284bbd5569fbc041931aa53bcf91504
aliyome <aliyome@gmail.com>
2023-09-08 07:35:13 +0900

    abc109 b

--- abc109/src/bin/b.rs
+++ abc109/src/bin/b.rs
@@ -1,9 +1,26 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         n: usize,
+        w: [Chars; n]
     };
 
-    println!("{}", n);
+    let mut set = HashSet::new();
+    for i in 0..n {
+        if i > 0 && w[i][0] != w[i - 1][w[i - 1].len() - 1] {
+            println!("No");
+            return;
+        }
+        if set.contains(&w[i].iter().collect::<String>()) {
+            println!("No");
+            return;
+        }
+
+        set.insert(w[i].iter().collect::<String>());
+    }
+
+    println!("Yes");
 }
