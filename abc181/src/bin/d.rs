91e2d1436d430e26e142686ec1449413cca3c865
aliyome <aliyome@gmail.com>
2023-09-08 07:48:28 +0900

    abc181 d

--- abc181/src/bin/d.rs
+++ abc181/src/bin/d.rs
@@ -1,8 +1,50 @@
-use proconio::input;
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        mut s: Chars,
     };
 
+    let mut hachi = vec![];
+    for i in (8..1000).step_by(8) {
+        let chars = format!("{:03}", i).chars().collect_vec();
+        let mut map = HashMap::new();
+        for c in chars {
+            *map.entry(c).or_insert(0) += 1;
+        }
+        hachi.push(map);
+    }
+
+    if s.len() < 4 {
+        s = format!(
+            "{:03}",
+            s.iter().collect::<String>().parse::<i32>().unwrap()
+        )
+        .chars()
+        .collect_vec();
+    }
+
+    let mut map = HashMap::new();
+    for &c in &s {
+        *map.entry(c).or_insert(0) += 1;
+    }
+
+    for h in hachi {
+        let mut ok = true;
+        for (k, v) in h {
+            if !map.contains_key(&k) || map[&k] < v {
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
