a191d75193ff637d025e5f9a1a595127a3721169
aliyome <aliyome@gmail.com>
2023-09-08 08:00:18 +0900

    abc181 d

--- abc181/src/bin/d.rs
+++ abc181/src/bin/d.rs
@@ -8,23 +8,27 @@ fn main() {
         mut s: Chars,
     };
 
+    if s.len() < 3 {
+        let l = s.len();
+        if s.into_iter()
+            .permutations(l)
+            .any(|x| x.iter().collect::<String>().parse::<i32>().unwrap() % 8 == 0)
+        {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+        return;
+    }
+
     let mut hachi = vec![];
     for i in (8..1000).step_by(8) {
         let chars = format!("{:03}", i).chars().collect_vec();
         let mut map = HashMap::new();
         for c in chars {
-            *map.entry(c).or_insert(0) += 1;
+            *map.entry(c).or_insert(0usize) += 1;
         }
         hachi.push(map);
-    }
-
-    if s.len() < 4 {
-        s = format!(
-            "{:03}",
-            s.iter().collect::<String>().parse::<i32>().unwrap()
-        )
-        .chars()
-        .collect_vec();
     }
 
     let mut map = HashMap::new();
