9000e9856f9dd497c6e4bd456cea53e91a8aa6e2
aliyome <aliyome@gmail.com>
2023-09-09 22:37:39 +0900

    abc319 e 愚直 TLE

--- abc319/src/bin/e.rs
+++ abc319/src/bin/e.rs
@@ -3,6 +3,34 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        x: u64,
+        y: u64,
+        bus_data: [(u64, u64); n - 1],
+        q: usize,
+        queries: [u64; q],
+    }
 
+    for query in queries {
+        // Takahashi's initial time
+        let mut cur_time = query;
+
+        // Walk to the first bus stop
+        cur_time += x;
+
+        // Process each bus stop
+        for &(p, t) in &bus_data {
+            // Wait for the bus
+            let wait_time = if cur_time % p == 0 {
+                0
+            } else {
+                p - cur_time % p
+            };
+            cur_time += wait_time + t;
+        }
+
+        // Walk to Aoki's house
+        cur_time += y;
+
+        println!("{}", cur_time);
+    }
 }
