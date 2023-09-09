d8e9dfd63d1566f6e32343fcefb52e54743041f0
aliyome <aliyome@gmail.com>
2023-09-09 22:04:28 +0900

    abc319 d binary_search

--- abc319/src/bin/d.rs
+++ abc319/src/bin/d.rs
@@ -1,8 +1,42 @@
 use proconio::input;
 
+fn check_width(width: i64, word_lengths: &Vec<i64>, m: usize) -> bool {
+    let mut lines = 1;
+    let mut current_width = 0;
+
+    for &length in word_lengths {
+        if length > width {
+            return false; // 単語がウィンドウ幅を超える場合
+        }
+        if current_width + length + if current_width > 0 { 1 } else { 0 } > width {
+            lines += 1;
+            current_width = 0;
+        }
+
+        current_width += length + if current_width > 0 { 1 } else { 0 };
+    }
+
+    lines <= m
+}
+
 fn main() {
     input! {
         n: usize,
-    };
+        m: usize,
+        l: [i64; n]
+    }
 
+    let mut left = *l.iter().max().unwrap(); // 最大の単語の長さ
+    let mut right = l.iter().sum::<i64>() + (n as i64) - 1; // すべての単語とスペースの合計
+
+    while left < right {
+        let mid = (left + right) / 2;
+        if check_width(mid, &l, m) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    println!("{}", left);
 }
