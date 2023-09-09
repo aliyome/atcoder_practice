619de799c13f47b5d66e155c3ba04d4813b08eea
aliyome <aliyome@gmail.com>
2023-09-09 21:16:36 +0900

    abc319 b

--- abc319/src/bin/b.rs
+++ abc319/src/bin/b.rs
@@ -2,7 +2,37 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-    };
+        n: usize
+    }
 
+    let mut divisors = vec![];
+
+    // Nの1から9までの約数を見つける
+    for i in 1..=9 {
+        if n % i == 0 {
+            divisors.push(i);
+        }
+    }
+
+    let mut answer = String::new();
+
+    // 各iに対してsiを計算する
+    for i in 0..=n {
+        let mut min_divisor = 10; // 10は使われない値。siが最小の約数で上書きされる。
+
+        for &d in &divisors {
+            if i % (n / d) == 0 {
+                min_divisor = min_divisor.min(d);
+            }
+        }
+
+        // 約数が見つかったかどうかを確認する
+        if min_divisor == 10 {
+            answer.push('-');
+        } else {
+            answer.push_str(&min_divisor.to_string());
+        }
+    }
+
+    println!("{}", answer);
 }
