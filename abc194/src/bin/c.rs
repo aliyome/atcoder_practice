1002bc2bef96eafb63634f5a923d8860a4b7893e
aliyome <aliyome@gmail.com>
2023-09-13 08:02:00 +0900

    abc194 c

--- abc194/src/bin/c.rs
+++ abc194/src/bin/c.rs
@@ -1,26 +1,29 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        mut a: [i64; n]
+        n: usize, // 1 <= n <= 3 x 10^5
+        a: [i64; n] // -200 <= a_i <= 200
     };
 
-    // sums[i][j] := i番目以降にjが出現する回数 (jはa[i]に200の下駄を履かせたもの)
-    let mut sums = vec![vec![0i64; 401]; n + 1];
-    for i in 0..n {
-        let idx = n - i - 1;
-        sums[idx] = sums[idx + 1].clone();
-        sums[idx][(a[idx] + 200) as usize] += 1;
+    // a を自然数に変換
+    let a = a.iter().map(|x| (x + 200) as usize).collect_vec();
+
+    // cnt[i][j] := i 番目までに含まれる j の数
+    let mut cnt = vec![vec![0; 401]; n + 1];
+    cnt[0][a[0]] = 1;
+    for i in 1..n {
+        cnt[i] = cnt[i - 1].clone();
+        cnt[i][a[i]] += 1;
     }
 
-    let mut ans = 0i64;
-    for i in 0..n {
+    let mut ans = 0;
+
+    // Σ_{i=1}^{n} Σ_{j=0}^{i-1} (a[i] - a[j])^2
+    for i in 1..n {
         for j in 0..=400 {
-            if sums[i][j] == 0 {
-                continue;
-            }
-            ans += ((a[i] + 200) - j as i64).pow(2) * sums[i][j];
+            ans += (a[i] - j).pow(2) * cnt[i - 1][j];
         }
     }
 
