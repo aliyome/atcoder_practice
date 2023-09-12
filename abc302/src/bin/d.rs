f614cd9cbb1d38192b614229b5366437f5ac5afd
aliyome <aliyome@gmail.com>
2023-09-13 08:25:45 +0900

    abc302 d

--- abc302/src/bin/d.rs
+++ abc302/src/bin/d.rs
@@ -14,7 +14,7 @@ fn main() {
 
     let mut ans = -1;
     for &a in &a {
-        let i = b.upper_bound(a + d);
+        let i = b.partition_point(|&x| x <= a + d);
         if i >= 1 {
             if (a - b[i - 1]).abs() <= d {
                 ans = ans.max(a + b[i - 1]);
@@ -34,72 +34,3 @@ fn main() {
 
     println!("{}", ans);
 }
-
-pub trait BinarySearch<T> {
-    fn lower_bound(&self, key: T) -> usize;
-    fn upper_bound(&self, key: T) -> usize;
-}
-
-impl<T> BinarySearch<T> for [T]
-where
-    T: Ord,
-{
-    fn lower_bound(&self, key: T) -> usize {
-        let mut ng = -1 as isize;
-        let mut ok = self.len() as isize;
-        while ok - ng > 1 {
-            let mid = (ok + ng) / 2;
-            if key <= self[mid as usize] {
-                ok = mid;
-            } else {
-                ng = mid;
-            }
-        }
-        ok as usize
-    }
-
-    fn upper_bound(&self, key: T) -> usize {
-        let mut ng = -1 as isize;
-        let mut ok = self.len() as isize;
-        while ok - ng > 1 {
-            let mid = (ok + ng) / 2;
-            if key < self[mid as usize] {
-                ok = mid;
-            } else {
-                ng = mid;
-            }
-        }
-        ok as usize
-    }
-}
-
-// use proconio::input;
-
-// fn main() {
-//     input! {
-//         n: usize,
-//         m: usize,
-//         d: i64,
-//         mut a: [i64; n],
-//         mut b: [i64; m],
-//     }
-
-//     a.sort();
-//     b.sort();
-
-//     let mut ans = -1;
-//     for &a_val in &a {
-//         let idx = match b.binary_search(&(a_val + d + 1)) {
-//             Ok(i) => i,
-//             Err(i) => i,
-//         };
-//         if idx > 0 {
-//             let b_val = b[idx - 1];
-//             if (a_val - b_val).abs() <= d {
-//                 ans = ans.max(a_val + b_val);
-//             }
-//         }
-//     }
-
-//     println!("{}", ans);
-// }
