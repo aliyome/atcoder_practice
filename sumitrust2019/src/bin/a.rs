7490e9176f907380f3b988355f86304083a28cf8
aliyome <aliyome@gmail.com>
2023-09-14 13:09:05 +0900

    sumitrust2019 a

--- sumitrust2019/src/bin/a.rs
+++ sumitrust2019/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        m1: usize,
+        d1: usize,
+        m2: usize,
+        d2: usize,
     };
 
+    if m1 != m2 {
+        println!("1");
+    } else {
+        println!("0");
+    }
 }
