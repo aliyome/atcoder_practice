cd804ad0960bfd8e30ba1730b9491824a52b2a21
aliyome <aliyome@gmail.com>
2023-09-15 10:46:36 +0900

    abc046 c binary search

--- abc046/src/bin/c.rs
+++ abc046/src/bin/c.rs
@@ -3,19 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        ta: [(usize, usize); n]
+        ta: [(u128, u128); n]
     };
 
-    let (mut c_t, mut c_a) = (0usize, 0usize);
+    let (mut c_t, mut c_a) = (1u128, 1u128);
     for &(t, a) in &ta {
-        for i in 1.. {
-            if t * i < c_t || a * i < c_a {
-                continue;
+        // 二分探索
+        let mut ok = 1e19 as u128;
+        let mut ng = 0u128;
+        while ok - ng > 1 {
+            let mid = (ok + ng) / 2;
+            let next_t = t * mid;
+            let next_a = a * mid;
+            if c_t <= next_t && c_a <= next_a {
+                ok = mid;
+            } else {
+                ng = mid;
             }
-            c_t = t * i;
-            c_a = a * i;
-            break;
         }
+        c_t = t * ok;
+        c_a = a * ok;
     }
     println!("{}", c_t + c_a);
 }
