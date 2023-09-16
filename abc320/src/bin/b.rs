606a4306a1a04c18afe2625f7d182f6dabf10425
aliyome <aliyome@gmail.com>
2023-09-16 21:05:26 +0900

    abc320 b

--- abc320/src/bin/b.rs
+++ abc320/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
+        s: Chars,
     };
 
+    let mut ans = 0;
+    for i in 0..s.len() {
+        for j in i + 1..=s.len() {
+            let a = s[i..j].iter().collect::<String>();
+            let b = a.chars().rev().collect::<String>();
+            if a == b {
+                ans = ans.max(j - i);
+            }
+        }
+    }
+    println!("{}", ans);
 }
