73a2d5ed8e23c27c6104bb27da579fe6dc18f60b
aliyome <aliyome@gmail.com>
2023-09-20 13:21:26 +0900

    practice2 j segtree

--- practice2/src/bin/j.rs
+++ practice2/src/bin/j.rs
@@ -1,8 +1,34 @@
+use ac_library::{Max, Segtree};
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
+        q: usize,
+        a: [usize; n],
     };
 
+    let mut seg_max = Segtree::<Max<_>>::new(n + 1);
+    for i in 0..n {
+        seg_max.set(i + 1, a[i]);
+    }
+
+    for _ in 0..q {
+        input! { t: usize };
+        match t {
+            1 => {
+                input! { x: usize, v: usize };
+                seg_max.set(x, v);
+            }
+            2 => {
+                input! { l: usize, r: usize };
+                println!("{}", seg_max.prod(l..=r));
+            }
+            3 => {
+                input! { x: usize, v: usize };
+                println!("{}", seg_max.max_right(x, |&a| a < v));
+            }
+            _ => unreachable!(),
+        }
+    }
 }
