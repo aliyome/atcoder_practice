55c647246a196c1f287ff5c5c973f6ed4258c9ff
aliyome <aliyome@gmail.com>
2023-09-23 21:13:54 +0900

    abc321 b

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -3,6 +3,27 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        x: usize,
+        a: [usize; n - 1],
+    }
 
+    // 0~100のスコアを全探索
+    let mut ans = std::usize::MAX;
+    for last in 0..=100 {
+        let mut all_scores = a.clone();
+        all_scores.push(last);
+        all_scores.sort_unstable();
+
+        let sum = all_scores[1..n - 1].iter().sum::<usize>();
+
+        if sum >= x {
+            ans = ans.min(last);
+        }
+    }
+
+    if ans == std::usize::MAX {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }
