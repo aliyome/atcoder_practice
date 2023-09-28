f917ace5ab5ad22d9d5466f9c6a68ebae7a0ff1f
aliyome <aliyome@gmail.com>
2023-09-29 08:07:03 +0900

    abc130 c

--- abc130/src/bin/c.rs
+++ abc130/src/bin/c.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        w: f64,
+        h: f64,
+        x: f64,
+        y: f64,
     };
 
+    if x == w / 2.0 && y == h / 2.0 {
+        println!("{} {}", w * h / 2.0, 1);
+    } else {
+        println!("{} {}", w * h / 2.0, 0);
+    }
 }
