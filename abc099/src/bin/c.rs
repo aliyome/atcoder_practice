28795bc369ee3c61694d95c769fac9377c2b8bee
aliyome <aliyome@gmail.com>
2023-09-29 08:14:20 +0900

    abc099 c

--- abc099/src/bin/c.rs
+++ abc099/src/bin/c.rs
@@ -5,5 +5,29 @@ fn main() {
         n: usize,
     };
 
-    println!("{}", n);
+    let mut dp = vec![std::usize::MAX; 100011];
+    dp[1] = 1;
+    dp[6] = 1;
+    dp[9] = 1;
+
+    for i in 1..=100000 {
+        if dp[i] == std::usize::MAX {
+            continue;
+        }
+        dp[i + 1] = dp[i + 1].min(dp[i] + 1);
+
+        let mut j = 6;
+        while i + j <= 100000 {
+            dp[i + j] = dp[i + j].min(dp[i] + 1);
+            j *= 6;
+        }
+
+        let mut j = 9;
+        while i + j <= 100000 {
+            dp[i + j] = dp[i + j].min(dp[i] + 1);
+            j *= 9;
+        }
+    }
+
+    println!("{}", dp[n]);
 }
