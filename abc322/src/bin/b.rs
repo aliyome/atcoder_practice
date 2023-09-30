d4eefeb2312e5fef63f904bcc84da48abe317f49
aliyome <aliyome@gmail.com>
2023-09-30 21:05:46 +0900

    abc322 b

--- abc322/src/bin/b.rs
+++ abc322/src/bin/b.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::input; // proconioクレートを使います。
 
 fn main() {
     input! {
         n: usize,
-    };
+        m: usize,
+        s: String,
+        t: String,
+    }
 
+    // SがTのprefixかどうかをチェックします。
+    let is_prefix = &t[0..n] == s;
+    // SがTのsuffixかどうかをチェックします。
+    let is_suffix = &t[(m - n)..] == s;
+
+    // 条件に基づいて結果を出力します。
+    if is_prefix && is_suffix {
+        println!("0");
+    } else if is_prefix {
+        println!("1");
+    } else if is_suffix {
+        println!("2");
+    } else {
+        println!("3");
+    }
 }
