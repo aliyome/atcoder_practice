2b19ed8d3ea10659cdd87841cb7421d664201315
aliyome <aliyome@gmail.com>
2023-09-30 22:27:23 +0900

    abc322 d

--- abc322/src/bin/d.rs
+++ abc322/src/bin/d.rs
@@ -20,40 +20,32 @@ fn main() {
         return;
     }
 
-    // 最終的な形
-    let mut reference = HashSet::new();
-    for i in 0..4 {
-        for j in 0..4 {
-            reference.insert((i as isize, j as isize));
-        }
-    }
-
     // 各ポリオミノの座標をHashSet<(isize, isize)>で表現し、正規化します。
     let mut shapes = Vec::new();
     for p in 0..3 {
         let mut shape = HashSet::new();
-        let mut min_row = 4;
-        let mut min_col = 4;
         for i in 0..4 {
             for j in 0..4 {
                 if polyominoes[i + 4 * p][j] == '#' {
-                    min_row = min_row.min(i);
-                    min_col = min_col.min(j);
                     shape.insert((i as isize, j as isize));
                 }
             }
         }
-        let normalized_shape: HashSet<(isize, isize)> = shape
-            .iter()
-            .map(|&(r, c)| (r - min_row as isize, c - min_col as isize))
-            .collect();
-        shapes.push(normalized_shape);
+        shapes.push(shape);
     }
+
+    // 最終的な形
+    let reference = (0..4)
+        .flat_map(|i| (0..4).map(move |j| (i as isize, j as isize)))
+        .collect::<HashSet<_>>();
 
     // すべてのポリオミノの配置と回転を試します。
     for &rotation1 in &[0, 1, 2, 3] {
+        let shape1 = normalize(&shapes[0], rotation1);
         for &rotation2 in &[0, 1, 2, 3] {
+            let shape2 = normalize(&shapes[1], rotation2);
             for &rotation3 in &[0, 1, 2, 3] {
+                let shape3 = normalize(&shapes[2], rotation3);
                 for row1 in 0..4 {
                     for col1 in 0..4 {
                         for row2 in 0..4 {
@@ -61,40 +53,16 @@ fn main() {
                                 for row3 in 0..4 {
                                     for col3 in 0..4 {
                                         let mut grid = HashSet::new();
-                                        let mut valid = true;
+                                        let shape1 = translate(&shape1, row1, col1);
+                                        grid.extend(shape1);
 
-                                        // ポリオミノ1を配置します。
-                                        for &(r, c) in
-                                            &rotate_and_translate(&shapes[0], rotation1, row1, col1)
-                                        {
-                                            if !grid.insert((r, c)) {
-                                                valid = false;
-                                                break;
-                                            }
-                                        }
+                                        let shape2 = translate(&shape2, row2, col2);
+                                        grid.extend(shape2);
 
-                                        // ポリオミノ2を配置します。
-                                        for &(r, c) in
-                                            &rotate_and_translate(&shapes[1], rotation2, row2, col2)
-                                        {
-                                            if !grid.insert((r, c)) {
-                                                valid = false;
-                                                break;
-                                            }
-                                        }
+                                        let shape3 = translate(&shape3, row3, col3);
+                                        grid.extend(shape3);
 
-                                        // ポリオミノ3を配置します。
-                                        for &(r, c) in
-                                            &rotate_and_translate(&shapes[2], rotation3, row3, col3)
-                                        {
-                                            if !grid.insert((r, c)) {
-                                                valid = false;
-                                                break;
-                                            }
-                                        }
-
-                                        // グリッドが全て埋まっているか確認します。
-                                        if valid && grid == reference {
+                                        if grid == reference {
                                             println!("Yes");
                                             return;
                                         }
@@ -113,21 +81,36 @@ fn main() {
 }
 
 // 指定された回転と移動を適用してポリオミノの座標を計算します。
-fn rotate_and_translate(
-    shape: &HashSet<(isize, isize)>,
-    rotation: usize,
-    row: isize,
-    col: isize,
-) -> HashSet<(isize, isize)> {
+fn translate(shape: &HashSet<(isize, isize)>, row: isize, col: isize) -> HashSet<(isize, isize)> {
+    let mut ret = HashSet::new();
+    for &(r, c) in shape {
+        ret.insert((r + row, c + col));
+    }
+    ret
+}
+
+fn rotation(shape: &HashSet<(isize, isize)>) -> HashSet<(isize, isize)> {
     let mut rotated = HashSet::new();
     for &(r, c) in shape {
-        let (nr, nc) = match rotation {
-            1 => (c, 3 - r),
-            2 => (3 - r, 3 - c),
-            3 => (3 - c, r),
-            _ => (r, c),
-        };
-        rotated.insert((nr + row, nc + col));
+        rotated.insert((c, -r));
     }
     rotated
+}
+
+fn n_rotation(shape: &HashSet<(isize, isize)>, n: usize) -> HashSet<(isize, isize)> {
+    let mut rotated = shape.clone();
+    for _ in 0..n {
+        rotated = rotation(&rotated);
+    }
+    rotated
+}
+
+fn normalize(shape: &HashSet<(isize, isize)>, rot: i32) -> HashSet<(isize, isize)> {
+    let shape = n_rotation(shape, rot as usize);
+    let min_row = shape.iter().map(|&(r, _)| r).min().unwrap_or(0);
+    let min_col = shape.iter().map(|&(_, c)| c).min().unwrap_or(0);
+    shape
+        .iter()
+        .map(|&(r, c)| (r - min_row, c - min_col))
+        .collect()
 }
