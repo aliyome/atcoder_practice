72203e69b72363e9dde4a832f4b77c00513a27f7
aliyome <aliyome@gmail.com>
2023-10-08 15:13:33 +0900

    abc323 d AC

--- abc323/src/bin/d.rs
+++ abc323/src/bin/d.rs
@@ -1,38 +1,27 @@
+use std::collections::BTreeMap;
+
 use proconio::input;
-use std::collections::HashMap;
 
 fn main() {
     input! {
         n: usize,
-        slimes: [(u64, u64); n],
+        sc: [(usize, usize); n],
     }
 
-    let mut slimes_map = HashMap::new();
-
-    for (s, c) in slimes {
-        *slimes_map.entry(s).or_insert(0) += c;
+    let mut map = BTreeMap::new();
+    for &(s, c) in &sc {
+        map.insert(s, c);
     }
 
-    let mut keys: Vec<_> = slimes_map.keys().cloned().collect();
-    keys.sort_unstable();
-
-    let mut idx = 0;
-    while idx < keys.len() {
-        let size = keys[idx];
-        if let Some(&count) = slimes_map.get(&size) {
-            if count >= 2 {
-                let combined = count / 2;
-                *slimes_map.entry(size * 2).or_insert_with(|| {
-                    keys.push(size * 2);
-                    0
-                }) += combined;
-                *slimes_map.entry(size).or_insert(0) -= combined * 2;
-            }
+    let mut count = 0;
+    while let Some((s, c)) = map.pop_first() {
+        let rest = c % 2;
+        let combined = c / 2;
+        count += rest;
+        if combined > 0 {
+            *map.entry(s * 2).or_insert(0) += combined;
         }
-        idx += 1;
     }
 
-    let result: u64 = slimes_map.values().sum();
-
-    println!("{}", result);
+    println!("{}", count);
 }
