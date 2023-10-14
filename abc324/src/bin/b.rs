dd24c1b6e78e5dff76245225cae51d90484fa447
aliyome <aliyome@gmail.com>
2023-10-14 21:07:01 +0900

    abc324 b

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -2,7 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-    };
+        n: i64,
+    }
 
+    let mut n = n;
+
+    while n % 2 == 0 {
+        n /= 2;
+    }
+
+    while n % 3 == 0 {
+        n /= 3;
+    }
+
+    if n == 1 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
