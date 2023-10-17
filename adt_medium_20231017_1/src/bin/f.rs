2d991c9853d4c9e8c64d979a2123409c5a41114d
aliyome <aliyome@gmail.com>
2023-10-17 16:23:53 +0900

    adt_medium_20231017_1 f

--- adt_medium_20231017_1/src/bin/f.rs
+++ adt_medium_20231017_1/src/bin/f.rs
@@ -2,7 +2,38 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-    };
+        _n: usize,
+        s: String,
+    }
 
+    // 現在の位置を (x, y) とする
+    let mut x = 0;
+    let mut y = 0;
+
+    // すでに訪れた座標を保持するためのHashSetを使用する
+    use std::collections::HashSet;
+    let mut visited = HashSet::new();
+
+    // 初期位置を訪れた座標として登録
+    visited.insert((x, y));
+
+    for ch in s.chars() {
+        match ch {
+            'R' => x += 1,
+            'L' => x -= 1,
+            'U' => y += 1,
+            'D' => y -= 1,
+            _ => {}
+        }
+
+        // すでにその座標を訪れていれば "Yes" を出力して終了
+        if visited.contains(&(x, y)) {
+            println!("Yes");
+            return;
+        }
+        visited.insert((x, y));
+    }
+
+    // すべての移動が終わっても同じ座標に戻らない場合 "No" を出力
+    println!("No");
 }
