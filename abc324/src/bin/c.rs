74695855a291b203653b525d6b11bf27207ee959
aliyome <aliyome@gmail.com>
2023-10-17 14:29:46 +0900

    abc324 c

--- abc324/src/bin/c.rs
+++ abc324/src/bin/c.rs
@@ -7,60 +7,54 @@ fn main() {
         s: [String; n],
     }
 
-    let mut ans = vec![];
+    let ok = |a: &str, b: &str| {
+        if a == b {
+            return true;
+        }
 
-    for (idx, si) in s.iter().enumerate() {
-        if check_possible(&t, si) {
-            ans.push(idx + 1);
+        if a.len() + 1 == b.len() {
+            for i in 0..b.len() {
+                if a[..i] == b[..i] && a[i..] == b[i + 1..] {
+                    return true;
+                }
+            }
+        }
+
+        if a.len() == b.len() + 1 {
+            for i in 0..a.len() {
+                if a[..i] == b[..i] && a[i + 1..] == b[i..] {
+                    return true;
+                }
+            }
+        }
+
+        if a.len() == b.len() {
+            let mut diff = 0usize;
+            for (x, y) in a.chars().zip(b.chars()) {
+                if x != y {
+                    diff += 1;
+                }
+            }
+            if diff == 1 {
+                return true;
+            }
+        }
+
+        false
+    };
+
+    let mut ans = vec![];
+    for i in 0..n {
+        if ok(&t, &s[i]) {
+            ans.push(i + 1);
         }
     }
-
     println!("{}", ans.len());
     println!(
         "{}",
         ans.iter()
             .map(|x| x.to_string())
-            .collect::<Vec<String>>()
+            .collect::<Vec<_>>()
             .join(" ")
     );
-}
-
-fn check_possible(t: &String, s: &String) -> bool {
-    // 条件1
-    if t == s {
-        return true;
-    }
-
-    // 条件2
-    if t.len() + 1 == s.len() {
-        for i in 0..=t.len() {
-            if &t[0..i] == &s[0..i] && &t[i..] == &s[i + 1..] {
-                return true;
-            }
-        }
-    }
-
-    // 条件3
-    if t.len() == s.len() + 1 {
-        for i in 0..=s.len() {
-            if &t[0..i] == &s[0..i] && &t[i + 1..] == &s[i..] {
-                return true;
-            }
-        }
-    }
-
-    // 条件4
-    if t.len() == s.len() {
-        let mut diff_count = 0usize;
-        for (c_t, c_s) in t.chars().zip(s.chars()) {
-            if c_t != c_s {
-                diff_count += 1;
-            }
-        }
-        if diff_count == 1 {
-            return true;
-        }
-    }
-
-    false
 }
