abd9e8fd8d6659e11a36e319eab45f1906f7cb7a
aliyome <aliyome@gmail.com>
2023-10-24 22:39:21 +0900

    abc325 e

--- abc325/src/bin/e.rs
+++ abc325/src/bin/e.rs
@@ -1,25 +1,6 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct State {
-    cost: i64,
-    position: usize,
-    by_car: bool,
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.cost.cmp(&self.cost)
-    }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 fn main() {
     input! {
@@ -27,59 +8,48 @@ fn main() {
         a: i64,
         b: i64,
         c: i64,
-        distances: [[i64; n]; n]
+        d: [[i64; n]; n]
     }
 
-    let mut costs = vec![vec![std::i64::MAX; 2]; n];
+    // ダイクストラで 0 から n-1 までの社用車の最短経路を求める
     let mut heap = BinaryHeap::new();
-
-    costs[0][0] = 0; // 0: by car, 1: by train
-    heap.push(State {
-        cost: 0,
-        position: 0,
-        by_car: true,
-    });
-
-    while let Some(current_state) = heap.pop() {
-        if current_state.cost > costs[current_state.position][current_state.by_car as usize] {
+    let mut dist_car = vec![std::i64::MAX; n];
+    dist_car[0] = 0;
+    heap.push((Reverse(0), 0));
+    while let Some((Reverse(cost), v)) = heap.pop() {
+        if dist_car[v] < cost {
             continue;
         }
-
-        for next in 0..n {
-            if next == current_state.position {
-                continue;
-            }
-            let next_cost = if current_state.by_car {
-                current_state.cost + distances[current_state.position][next] * a
-            } else {
-                current_state.cost + distances[current_state.position][next] * b + c
-            };
-
-            let next_state = State {
-                cost: next_cost,
-                position: next,
-                by_car: current_state.by_car,
-            };
-
-            if next_cost < costs[next][current_state.by_car as usize] {
-                costs[next][current_state.by_car as usize] = next_cost;
-                heap.push(next_state);
-            }
-
-            if current_state.by_car {
-                let train_cost =
-                    current_state.cost + distances[current_state.position][next] * b + c;
-                if train_cost < costs[next][1] {
-                    costs[next][1] = train_cost;
-                    heap.push(State {
-                        cost: train_cost,
-                        position: next,
-                        by_car: false,
-                    });
-                }
+        for (u, &d) in d[v].iter().enumerate() {
+            if dist_car[u] > dist_car[v] + d * a {
+                dist_car[u] = dist_car[v] + d * a;
+                heap.push((Reverse(dist_car[u]), u));
             }
         }
     }
 
-    println!("{}", std::cmp::min(costs[n - 1][0], costs[n - 1][1]));
+    // ダイクストラで n-1 から 0 までの電車の最短経路を求める
+    let mut heap = BinaryHeap::new();
+    let mut dist_train = vec![std::i64::MAX; n];
+    dist_train[n - 1] = 0;
+    heap.push((Reverse(0), n - 1));
+    while let Some((Reverse(cost), v)) = heap.pop() {
+        if dist_train[v] < cost {
+            continue;
+        }
+        for (u, &d) in d[v].iter().enumerate() {
+            if dist_train[u] > dist_train[v] + d * b + c {
+                dist_train[u] = dist_train[v] + d * b + c;
+                heap.push((Reverse(dist_train[u]), u));
+            }
+        }
+    }
+
+    // 0 から n-1 までの最短経路を求める
+    let mut ans = std::i64::MAX;
+    for i in 0..n {
+        ans = ans.min(dist_car[i] + dist_train[i]);
+    }
+
+    println!("{}", ans);
 }
