283572857d88d5a1af696318699cbdab2ab25a6b
aliyome <aliyome@gmail.com>
2023-11-04 21:15:56 +0900

    abc327 b

--- abc327/src/bin/b.rs
+++ abc327/src/bin/b.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-    };
+        b: u128,
+    }
 
+    for a in 1u128.. {
+        let x: u128 = a.pow(a as u32);
+        if x > 10u128.pow(18) {
+            break;
+        }
+        if x == b {
+            println!("{}", a);
+            return;
+        }
+    }
+
+    println!("-1");
 }
