1966ba9ad4cb12118b8b6d96236617f9ef03af63
aliyome <aliyome@gmail.com>
2023-11-11 21:27:03 +0900

    abc328 d

--- abc328/src/bin/d.rs
+++ abc328/src/bin/d.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: usize,
-    };
+        s: Chars,
+    }
 
+    let mut stack = Vec::new();
+
+    for &c in &s {
+        match c {
+            'C' => {
+                if stack.ends_with(&['A', 'B']) {
+                    stack.pop();
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            _ => stack.push(c),
+        }
+    }
+
+    let result: String = stack.into_iter().collect();
+    println!("{}", result);
 }
