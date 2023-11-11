f9a9c2db20ef43ed05f94ee8a34ec9a219df71b6
aliyome <aliyome@gmail.com>
2023-11-11 21:06:48 +0900

    abc328 b

--- abc328/src/bin/b.rs
+++ abc328/src/bin/b.rs
@@ -3,6 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        d: [usize; n],
+    }
 
+    let mut cnt = 0;
+    for i in 0..n {
+        let d = d[i];
+        let i = i + 1;
+        for j in 1..=d {
+            let s = format!("{}{}", i, j);
+            let first = s.chars().nth(0).unwrap();
+            if s.chars().all(|c| c == first) {
+                cnt += 1;
+            }
+        }
+    }
+    println!("{}", cnt);
 }
