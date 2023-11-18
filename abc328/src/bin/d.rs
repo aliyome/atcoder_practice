a2bfc2e68448011b59414cd101ceec7d841dc877
aliyome <aliyome@gmail.com>
2023-11-18 20:48:22 +0900

    abc328 d

--- abc328/src/bin/d.rs
+++ abc328/src/bin/d.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use proconio::{input, marker::Chars};
 
 fn main() {
@@ -5,22 +6,25 @@ fn main() {
         s: Chars,
     }
 
-    let mut stack = Vec::new();
-
-    for &c in &s {
+    let mut stack = vec![vec![]];
+    for c in s {
         match c {
+            'A' => stack.push(vec![c]),
+            'B' => stack.last_mut().unwrap().push(c),
             'C' => {
-                if stack.ends_with(&['A', 'B']) {
+                stack.last_mut().unwrap().push(c);
+                if stack.last().unwrap() == &['A', 'B', 'C'] {
                     stack.pop();
-                    stack.pop();
-                } else {
-                    stack.push(c);
                 }
             }
-            _ => stack.push(c),
+            _ => unreachable!(),
         }
     }
 
-    let result: String = stack.into_iter().collect();
-    println!("{}", result);
+    let ans = stack.iter().flatten().collect::<String>();
+    if ans != "" {
+        println!("{}", ans);
+    }
+
+    // eprintln!("{:?}", stack);
 }
