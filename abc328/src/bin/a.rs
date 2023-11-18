ac42bd6e9941b292bc061d51fe606ae200b64d7c
aliyome <aliyome@gmail.com>
2023-11-18 20:28:34 +0900

    abc328 a re

--- abc328/src/bin/a.rs
+++ abc328/src/bin/a.rs
@@ -3,16 +3,9 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        x: i32,
-        scores: [i32; n],
+        x: usize,
+        s: [usize; n],
     }
 
-    let mut total_score = 0;
-    for score in scores {
-        if score <= x {
-            total_score += score;
-        }
-    }
-
-    println!("{}", total_score);
+    println!("{}", s.iter().filter(|&&s| s <= x).sum::<usize>());
 }
