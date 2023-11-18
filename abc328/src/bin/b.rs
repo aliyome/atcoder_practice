504f660948cec325514356c8ff4c34a99d1d2b3a
aliyome <aliyome@gmail.com>
2023-11-18 20:32:24 +0900

    abc328 b re

--- abc328/src/bin/b.rs
+++ abc328/src/bin/b.rs
@@ -8,10 +8,9 @@ fn main() {
 
     let mut cnt = 0;
     for i in 0..n {
-        let d = d[i];
-        let i = i + 1;
-        for j in 1..=d {
-            let s = format!("{}{}", i, j);
+        let m = i + 1;
+        for d in 1..=d[i] {
+            let s = format!("{}{}", m, d);
             let first = s.chars().nth(0).unwrap();
             if s.chars().all(|c| c == first) {
                 cnt += 1;
