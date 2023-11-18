c34d367ba0ebcf4d8475e988e3317c68d7f762c6
aliyome <aliyome@gmail.com>
2023-11-18 20:41:02 +0900

    abc328 c re

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -8,17 +8,13 @@ fn main() {
         lr: [(usize, usize); q],
     }
 
-    // 累積和を計算するための配列を準備
-    let mut cum_sum = vec![0; n + 1];
-
-    // 文字列を一回走査し、隣接する同じ文字の数をカウント
-    for i in 0..n - 1 {
-        cum_sum[i + 1] = cum_sum[i] + if s[i] == s[i + 1] { 1 } else { 0 };
+    // 累積和
+    let mut sum = vec![0; n + 1];
+    for i in 1..n {
+        sum[i + 1] = sum[i] + if s[i - 1] == s[i] { 1 } else { 0 };
     }
 
-    // 累積和を用いてクエリの答えを計算
     for (l, r) in lr {
-        let ans = cum_sum[r - 1] - cum_sum[l - 1];
-        println!("{}", ans);
+        println!("{}", sum[r] - sum[l]);
     }
 }
