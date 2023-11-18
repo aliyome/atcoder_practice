4701460c801b99316cf6aecd9446c22889c7ae3f
aliyome <aliyome@gmail.com>
2023-11-18 21:03:37 +0900

    abc329 b

--- abc329/src/bin/b.rs
+++ abc329/src/bin/b.rs
@@ -3,6 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        a: [i32; n],
+    }
 
+    let max_value = *a.iter().max().unwrap();
+
+    let second_max = a.iter().filter(|&&x| x != max_value).max().unwrap();
+
+    println!("{}", second_max);
 }
