29b79bd8e52f72c1a1779a8d883e726fbaed8e60
aliyome <aliyome@gmail.com>
2023-11-25 21:03:04 +0900

    abc330 b

--- abc330/src/bin/b.rs
+++ abc330/src/bin/b.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        l: i32,
+        r: i32,
+        a: [i32; n],
+    }
 
+    for &ai in &a {
+        let xi = if ai < l {
+            l
+        } else if ai > r {
+            r
+        } else {
+            ai
+        };
+
+        print!("{} ", xi);
+    }
+    println!();
 }
