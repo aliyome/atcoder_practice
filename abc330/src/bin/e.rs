06a13c83642d62682a5de843b85896ea08c2f084
aliyome <aliyome@gmail.com>
2023-11-25 21:44:21 +0900

    abc330 e fixed-range

--- abc330/src/bin/e.rs
+++ abc330/src/bin/e.rs
@@ -1,5 +1,4 @@
 use proconio::input;
-use std::collections::BTreeSet;
 
 fn main() {
     input! {
@@ -9,36 +8,43 @@ fn main() {
         queries: [(usize, usize); q],
     }
 
-    let mut counts = vec![0; n + 2];
-    let mut set: BTreeSet<usize> = (0..=n + 1).collect();
+    let mut freq = vec![0; n + 1];
+    let mut mex = 0;
 
-    for &ai in &a {
-        if counts[ai] == 0 {
-            set.remove(&ai);
+    // 初期配列の頻度を計算
+    for &value in &a {
+        if value <= n {
+            freq[value] += 1;
         }
-        counts[ai] += 1;
     }
 
+    // 初期のmexを計算
+    while mex <= n && freq[mex] > 0 {
+        mex += 1;
+    }
+
+    // クエリを処理
     for (i, x) in queries {
-        let index = i - 1; // 0-indexed
-
-        // 元の値を減らす
-        counts[a[index]] -= 1;
-        if counts[a[index]] == 0 {
-            set.insert(a[index]);
-        }
-
-        // 新しい値を増やす
-        if counts[x] == 0 {
-            set.remove(&x);
-        }
-        counts[x] += 1;
-
-        // mexを見つける
-        let mex = set.range(..).next().unwrap();
-        println!("{}", mex);
-
-        // 配列を更新
+        let index = i - 1; // 0-indexedに変換
+        let old_value = a[index];
         a[index] = x;
+
+        // 古い値の頻度を減らす
+        if old_value <= n {
+            freq[old_value] -= 1;
+            if freq[old_value] == 0 && old_value < mex {
+                mex = old_value;
+            }
+        }
+
+        // 新しい値の頻度を増やす
+        if x <= n {
+            freq[x] += 1;
+            while mex <= n && freq[mex] > 0 {
+                mex += 1;
+            }
+        }
+
+        println!("{}", mex);
     }
 }
