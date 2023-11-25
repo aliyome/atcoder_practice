61c59c1fbcafd7c8cdeaeac2101e8e6b71470e94
aliyome <aliyome@gmail.com>
2023-11-25 21:05:10 +0900

    abc330 c

--- abc330/src/bin/c.rs
+++ abc330/src/bin/c.rs
@@ -1,8 +1,22 @@
 use proconio::input;
+use std::cmp;
 
 fn main() {
     input! {
-        n: usize,
-    };
+        d: i64,
+    }
 
+    let mut result = d;
+    let mut x = 0;
+
+    while x * x <= d {
+        let y = ((d - x * x) as f64).sqrt() as i64;
+        // |x^2 + y^2 - D| の値を計算し、最小値を更新
+        result = cmp::min(result, (x * x + y * y - d).abs());
+        // y+1 の場合もチェック
+        result = cmp::min(result, (x * x + (y + 1) * (y + 1) - d).abs());
+        x += 1;
+    }
+
+    println!("{}", result);
 }
