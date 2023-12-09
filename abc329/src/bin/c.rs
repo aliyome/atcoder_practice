2d585680c473b4110bc07d1de26db489c42b7880
aliyome <aliyome@gmail.com>
2023-12-09 19:53:05 +0900

    abc329 c re

--- abc329/src/bin/c.rs
+++ abc329/src/bin/c.rs
@@ -8,27 +8,30 @@ fn main() {
         mut s: Chars,
     }
 
-    s.push('\0'); // 番兵 (sentinel
+    // 文字種類ごとの最大の長さを求める
+    let mut map: HashMap<char, usize> = HashMap::new();
 
-    let mut count = 0;
-    let mut total = 0;
-    let mut prev_char = '\0'; // 初期値として無効な文字を設定
-    let mut map = HashMap::new();
-
-    for c in s {
-        if c == prev_char {
-            // 前の文字と同じ場合、カウンターを増やす
-            count += 1;
-        } else {
-            // 異なる文字が現れたら、これまでのカウントに基づいて部分文字列の数を計算
-            if prev_char != '\0' {
-                let current = *map.get(&prev_char).unwrap_or(&0);
-                map.insert(prev_char, current.max(count));
+    let mut c = ' ';
+    let mut cnt = 1;
+    for i in 0..n {
+        if c == s[i] {
+            cnt += 1;
+            if map.get(&c).unwrap_or(&0) < &cnt {
+                map.insert(c, cnt);
             }
-            prev_char = c;
-            count = 1; // 新しい文字のカウントを開始
+        } else {
+            c = s[i];
+            cnt = 1;
+            if map.get(&c).unwrap_or(&0) < &cnt {
+                map.insert(c, cnt);
+            }
         }
     }
 
-    println!("{}", map.values().sum::<usize>());
+    let mut ans = 0;
+    for (_, &c) in map.iter() {
+        ans += c;
+    }
+
+    println!("{}", ans);
 }
