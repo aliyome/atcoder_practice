52530e32891b71f3f7e7630bb114f2de8957b50c
aliyome <aliyome@gmail.com>
2023-12-09 20:12:43 +0900

    abc329 d re

--- abc329/src/bin/d.rs
+++ abc329/src/bin/d.rs
@@ -1,31 +1,31 @@
+use std::{
+    cmp::Reverse,
+    collections::{BTreeSet, HashMap},
+};
+
 use proconio::input;
 
 fn main() {
     input! {
-        n: usize, // 候補者の数
-        m: usize, // 投票の数
-        votes: [usize; m], // 各投票
+        _n: usize,
+        m: usize,
+        a: [usize; m],
     }
 
-    let mut counts = vec![0; n + 1]; // 各候補者の得票数を格納する配列
-    let mut current_max = 0; // 現在の最大得票数
-    let mut current_winner = 0; // 現在の勝者
+    // 候補者ごとの票数
+    let mut map: HashMap<usize, usize> = HashMap::new();
+    // 現在の順位 [(票数, 候補者)]
+    let mut set: BTreeSet<(Reverse<usize>, usize)> = BTreeSet::new();
 
-    for i in 0..m {
-        let vote = votes[i];
-        counts[vote] += 1; // 対応する候補者の得票数を増やす
-
-        // 新しい得票数が現在の最大得票数以上の場合、勝者を更新
-        if counts[vote] > current_max {
-            current_max = counts[vote];
-            current_winner = vote;
-        } else if counts[vote] == current_max {
-            // 最大得票数が同数の場合、番号が小さい候補者を選ぶ
-            if vote < current_winner {
-                current_winner = vote;
-            }
+    for a in a {
+        *map.entry(a).or_insert(0) += 1;
+        let current = *map.get(&a).unwrap();
+        if current == 1 {
+            set.insert((Reverse(current), a));
+        } else {
+            set.remove(&(Reverse(current - 1), a));
+            set.insert((Reverse(current), a));
         }
-
-        println!("{}", current_winner);
+        println!("{}", set.iter().next().unwrap().1);
     }
 }
