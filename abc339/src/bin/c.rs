faac3b7b6270c9f7e363601925ee4ac734586919
aliyome <aliyome@gmail.com>
2024-02-03 21:06:15 +0900

    abc339 c

--- abc339/src/bin/c.rs
+++ abc339/src/bin/c.rs
@@ -3,6 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-    };
+        a: [i64; n],
+    }
 
+    let mut min = 0;
+    let mut current = 0;
+
+    for &change in &a {
+        current += change;
+        if current < min {
+            min = current;
+        }
+    }
+
+    let initial = if min < 0 { -min } else { 0 };
+
+    let result = initial + current;
+    println!("{}", result);
 }
